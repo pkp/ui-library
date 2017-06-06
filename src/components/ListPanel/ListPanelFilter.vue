2b123ef292d938c646d6b0bf35854d5b8c356b66
Nate Wright <natew@notthisway.com>
2017-06-06 13:38:46 +0100

    Move ListPanelFilter styles into that component

--- src/components/ListPanel/ListPanelFilter.vue
+++ src/components/ListPanel/ListPanelFilter.vue
@@ -51,3 +51,133 @@ export default {
 	}
 }
 </script>
+
+<style lang="less">
+@import '../../styles/_config';
+
+.pkpListPanel__filter {
+	position: absolute;
+	left: -9999px;
+	width: 0;
+}
+
+.pkpListPanel__filter:before {
+	content: '';
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	right: 0;
+	left: 0;
+	border-right: @grid-border;
+}
+
+.pkpListPanel__filter,
+.pkpListPanel__content {
+	transition: width 0.2s;
+}
+
+.pkpListPanel__content {
+	position: relative;
+}
+
+.pkpListPanel__filter + .pkpListPanel__content {
+	float: right;
+	width: 100%;
+}
+
+.pkpListPanel__filterHeader {
+	margin: @base 0;
+	padding: 0 @base;
+	font-size: @font-sml;
+	line-height: @line-sml;
+	font-weight: @bold;
+
+	&:focus {
+		outline: 0;
+	}
+
+	.fa {
+		margin-right: 0.25em;
+	}
+}
+
+.pkpListPanel__filterHeader,
+.pkpListPanel__filterOptions {
+	position: absolute;
+	left: -9999px;
+	opacity: 0;
+	// This is an arbitrarily large width that is used to smooth the transition
+	// when the filter panel is shown. Without a large width, the filter options
+	// get squished as the parent element grows from a small to a large size.
+	// This then causes the content to have additional line breaks, so that the
+	// panel starts tall and shortens as the width expands. By forcing a big
+	// width here, we prevent it from "jumping" to a large height.
+	width: 1000px;
+}
+
+.pkpListPanel__filterSet {
+	margin: @base 0;
+	font-size: @font-sml;
+	line-height: @line-sml;
+
+	ul {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+	}
+
+	li {
+		position: relative;
+	}
+}
+
+.pkpListPanel__filterSetLabel {
+	padding-left: @base;
+	padding-right: @base;
+	font-weight: @bold;
+	font-size: @font-tiny;
+	color: @text-light;
+}
+
+.pkpListPanel__filterLabel {
+	display: block;
+	padding: (@half / 2) @base;
+	background: transparent;
+	border: none;
+	color: @primary;
+	text-decoration: none;
+
+	&:focus {
+		border-left: 2px solid @primary;
+		padding-left: @base - 2;
+		outline: 0;
+	}
+
+	&.--isActive {
+		font-weight: @bold;
+	}
+}
+
+.pkpListPanel__filterRemove {
+	position: absolute;
+	top: 50%;
+	right: 0;
+	padding: 0;
+	margin: 0;
+	width: @double;
+	border: none;
+	transform: translateY(-50%);
+	background: transparent;
+	text-align: center;
+	color: @text-light;
+
+	&:hover,
+	&:focus {
+		color: @offset;
+	}
+}
+
+.pkpListPanel__filterRemoveLabel {
+	.pkp_screen_reader();
+}
+</style>
