dc5d7e3f7e3cda59525f3f0553ffb6b923997be3
Nate Wright <natew@notthisway.com>
2017-06-22 15:24:47 +0100

    Implement first unit test on ListPanel

---
+++ test/unit/specs/ListPanel.spec.js
@@ -0,0 +1,41 @@
+import Vue from 'vue';
+import ListPanel from '@/components/ListPanel/ListPanel.vue';
+
+// Test data
+ListPanel.data = function () {
+	return {
+		id: 'ExampleListPanel',
+		collection: {
+			items: [
+				{title: 'Item one'},
+				{title: 'Item two'},
+				{title: 'Item three'},
+			],
+			maxItems: 10,
+		},
+		filterParams: {},
+		searchPhrase: '',
+		isLoading: false,
+		isSearching: false,
+		isOrdering: false,
+		isFilterVisible: false,
+		count: 20,
+		offset: 0,
+		apiPath: '',
+		getParams: {},
+		i18n: {
+			title: 'Example List Panel',
+		},
+		lazyLoad: false,
+		_lastGetRequest: null,
+	};
+};
+
+describe('ListPanel.vue', () => {
+	it('should render correct contents', () => {
+		const Constructor = Vue.extend(ListPanel);
+		const vm = new Constructor().$mount();
+		expect(vm.$el.querySelector('.pkpListPanel__title').textContent)
+			.to.equal('Example List Panel');
+	});
+});
