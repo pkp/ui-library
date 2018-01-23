9cccb09f77c24d903fe345797cc9c9433c41ffaa
Nate Wright <natew@notthisway.com>
2018-01-23 16:37:46 +0000

    pkp/pkp-lib#2894 Convert icon elements to components

---
+++ src/components/Icon/Icon.vue
@@ -0,0 +1,44 @@
+<template>
+	<span class="fa" :class="classes"	aria-hidden="true"></span>
+</template>
+
+<script>
+export default {
+	name: 'Icon',
+	props: {
+		icon: String,
+		inline: Boolean,
+	},
+	computed: {
+		classes: function () {
+			let classes = ['fa-' + this.icon];
+			if (this.inline) {
+				classes.push('pkpIcon--inline');
+			}
+			return classes;
+		},
+	},
+};
+</script>
+
+<style lang="less">
+@import '../../styles/_import';
+
+.pkpIcon--inline {
+	margin-right: 0.25em;
+}
+
+/**
+ * Help icon
+ */
+.fa-info-circle {
+	color: @yes;
+}
+
+/**
+ * Warning icon
+ */
+.fa-exclamation-triangle {
+	color: @no;
+}
+</style>
---
+++ src/docs/examples/Icon/WarningIcon.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<icon :icon="icon" />
+	</div>
+</template>
+
+<script>
+import Icon from '@/components/Icon/Icon.vue';
+
+export default {
+	name: 'WarningIcon',
+	components: {
+		Icon,
+	},
+	data: function () {
+		return {
+			icon: 'exclamation-triangle',
+		};
+	},
+};
+</script>
