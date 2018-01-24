1d8dea9757744263aba7c5e07582611ddad19438
Nate Wright <natew@notthisway.com>
2018-01-24 16:35:07 +0000

    pkp/pkp-lib#2894 Initial work on SelectReviewerListPanel

--- src/docs/examples/SelectListPanel/data.js
+++ src/docs/examples/SelectListPanel/data.js
@@ -4,6 +4,8 @@ import WithRadio from './WithRadio.vue';
 import WithSelectAll from './WithSelectAll.vue';
 import SelectSubmissionsListPanel from './implementations/ExampleSelectSubmissionsListPanel.vue';
 import SelectSubmissionsListPanelRaw from '!!raw-loader!@/components/SelectListPanel/submissions/SelectSubmissionsListPanel.vue';
+import SelectReviewerListPanel from './implementations/ExampleSelectReviewerListPanel.vue';
+import SelectReviewerListPanelRaw from '!!raw-loader!@/components/SelectListPanel/users/SelectReviewerListPanel.vue';
 
 export default {
 	baseData: function () {
@@ -43,6 +45,11 @@ export default {
 			label: 'SelectSubmissionsListPanel',
 			component: SelectSubmissionsListPanel,
 			componentRaw: SelectSubmissionsListPanelRaw,
+		},
+		'SelectReviewerListPanel': {
+			label: 'SelectReviewerListPanel',
+			component: SelectReviewerListPanel,
+			componentRaw: SelectReviewerListPanelRaw,
 		},
 	},
 };
---
+++ src/docs/examples/SelectListPanel/implementations/helpers/BaseReviewerObject.js
@@ -0,0 +1,55 @@
+export default {
+	id: 1,
+	_href: 'http://ojs.org/api/users/1',
+	userName: 'exampleuser',
+	fullName: 'Jane Doe',
+	affiliation: {
+		en_US: 'University of Nowhere',
+	},
+	groups: [
+		{
+			id: 1,
+			name: {
+				en_US: 'Reviewer',
+			},
+			abbrev: {
+				en_US: 'RV',
+			},
+			roleId: 4096, // reviewer
+			showTitle: true,
+			permitSelfRegistration: true,
+			recommendOnly: true,
+		},
+	],
+	interests: [
+		{
+			id: 123,
+			interest: 'cat',
+		},
+		{
+			id: 124,
+			interest: 'dog',
+		},
+		{
+			id: 125,
+			interest: 'fish',
+		},
+		{
+			id: 126,
+			interest: 'bear',
+		},
+		{
+			id: 127,
+			interest: 'alien',
+		},
+	],
+	gossip: {
+		en_US: '<p>This is a note about the user which has been entered by someone at some point in the past. It might reflect on past performance or include a reminder about what kind of material the reviewer prefers to review.',
+	},
+	reviewsActive: 2,
+	reviewsCompleted: 1,
+	averageReviewCompletionDays: 21,
+	dateLastReviewAssignment: '2017-02-03 11:22:42',
+	reviewerRating: 4,
+	disabled: false,
+};
