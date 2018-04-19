a095a4dc0204fade429ec60f034aab67c132613f
Nate Wright <natew@notthisway.com>
2018-04-19 12:18:06 +0100

    pkp/pkp-lib#2860 Remove duplicated code from extended ListPanelFilter

--- src/components/ListPanel/submissions/SubmissionsListFilter.vue
+++ src/components/ListPanel/submissions/SubmissionsListFilter.vue
@@ -1,49 +1,9 @@
-<template>
-	<div class="pkpListPanel__filter pkpListPanel__filter--submissions" :class="{'-isVisible': isVisible}" :aria-hidden="!isVisible">
-		<div class="pkpListPanel__filterHeader pkpListPanel__filterHeader--submissions" tabindex="0">
-			<icon icon="filter" :inline="true" />
-			{{ i18n.filter }}
-		</div>
-		<div class="pkpListPanel__filterOptions pkpListPanel__filterOptions--submissions">
-			<div v-for="filter in filters" class="pkpListPanel__filterSet">
-				<div v-if="filter.heading" class="pkpListPanel__filterSetLabel">
-					{{ filter.heading }}
-				</div>
-				<ul>
-					<li v-for="filterItem in filter.filters">
-						<a href="#"
-							@click.prevent.stop="filterBy(filterItem.param, filterItem.val)"
-							class="pkpListPanel__filterLabel"
-							:class="{'-isActive': isFilterActive(filterItem.param, filterItem.val)}"
-							:tabindex="tabIndex"
-						>{{ filterItem.title }}</a>
-						<button
-							v-if="isFilterActive(filterItem.param, filterItem.val)"
-							href="#"
-							class="pkpListPanel__filterRemove"
-							@click.prevent.stop="clearFilter(filterItem.param, filterItem.val)"
-						>
-							<icon icon="times-circle-o" />
-							<span class="-screenReader">{{ __('filterRemove', {filterTitle: filterItem.title}) }}</span>
-						</button>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
-</template>
-
 <script>
 import ListPanelFilter from '@/components/ListPanel/ListPanelFilter.vue';
-import Icon from '@/components/Icon/Icon.vue';
 
 export default {
 	extends: ListPanelFilter,
 	name: 'SubmissionsListFilter',
-	components: {
-		Icon,
-	},
-	props: ['isVisible', 'filters', 'i18n'],
 	methods: {
 		/**
 		 * Override ListPanelFilter.filterBy to handle special
