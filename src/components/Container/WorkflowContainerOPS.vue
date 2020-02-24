34c2ee81270c42aa3020d026367e374194f191b9
ajnyga <ajnyga@gmail.com>
2020-02-24 12:06:55 +0200

    Rename css class

--- src/components/Container/WorkflowContainerOPS.vue
+++ src/components/Container/WorkflowContainerOPS.vue
@@ -57,18 +57,18 @@ export default {
 
 <style lang="less">
 @import '../../styles/_import';
-.relateForm .pkpFormGroup{
+.pkpWorkflow__relateForm .pkpFormGroup{
 	padding: 0.5rem;
 }
 .pkpWorkflow__relation .pkpDropdown__content{
 	min-width: 25em;
 	max-width: 25em;
 }
-.relateForm .pkpFormPage__footer{
+.pkpWorkflow__relateForm .pkpFormPage__footer{
 	border-top: none;
 	padding: 0.5rem;
 }
-.relateForm .pkpFormPage__buttons > * {
+.pkpWorkflow__relateForm .pkpFormPage__buttons > * {
 	margin-right: 2px;
 }
 </style>
