0ba82ea3a7565c6dd071738dc002728226190ffe
thinkbulecount2 <38994650+thinkbulecount2@users.noreply.github.com>
2020-07-06 11:14:12 -0400

    pkp/pkp-lib#6054 Added issue filter

--- src/components/Filter/FilterAutosuggest.vue
+++ src/components/Filter/FilterAutosuggest.vue
@@ -1,7 +1,7 @@
 <template>
-	<div class="pkpFilter--autosuggest" :class="classes">
-		<component :is="component" v-bind="autosuggestProps" @change="toggle" />
-	</div>
+    <div class="pkpFilter--autosuggest" :class="classes">
+        <component :is="component" v-bind="autosuggestProps" @change="toggle" />
+    </div>
 </template>
 
 <script>
@@ -9,47 +9,47 @@ import Filter from './Filter.vue';
 import FieldSelectUsers from '@/components/Form/fields/FieldSelectUsers.vue';
 import FieldSelectIssues from '@/components/Form/fields/FieldSelectIssues.vue';
 export default {
-	extends: Filter,
-	components: {
-		FieldSelectUsers,
-		FieldSelectIssues
-	},
-	props: {
-		component: {
-			type: String,
-			required: true
-		},
-		autosuggestProps: {
-			type: Object,
-			required: true
-		}
-	},
-	methods: {
-		toggle(fieldName, fieldProp, newVal) {
-			if (newVal.length) {
-				this.$emit('add-filter', this.param, newVal);
-			} else {
-				this.remove();
-			}
-		},
-		remove() {
-			this.$emit('remove-filter', this.param);
-		}
-	}
+    extends: Filter,
+    components: {
+        FieldSelectUsers,
+        FieldSelectIssues
+    },
+    props: {
+        component: {
+            type: String,
+            required: true
+        },
+        autosuggestProps: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        toggle(fieldName, fieldProp, newVal) {
+            if (newVal.length) {
+                this.$emit('add-filter', this.param, newVal);
+            } else {
+                this.remove();
+            }
+        },
+        remove() {
+            this.$emit('remove-filter', this.param);
+        }
+    }
 };
 </script>
 
 <style lang="less">
 @import '../../styles/_import';
 .pkpFilter--autosuggest {
-	position: relative;
-	padding-left: 1rem;
-	padding-right: 1rem;
-	padding-bottom: 1rem;
+    position: relative;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    padding-bottom: 1rem;
 }
 
 .pkpFormField--autosuggest__values .pkpBadge {
-	margin-right: 0;
-	display: block;
+    margin-right: 0;
+    display: block;
 }
 </style>
--- src/docs/components/Filter/previews/PreviewFilterAutosuggest.vue
+++ src/docs/components/Filter/previews/PreviewFilterAutosuggest.vue
@@ -1,24 +1,24 @@
 <template>
-	<div class="previewFilters">
-		<filter-autosuggest
-			:autosuggestProps="users"
-			component="field-select-users"
-			param="assignedTo"
-			:value="[]"
-			title="Assigned to Editors"
-			@add-filter="addFilter"
-			@remove-filter="removeFilter"
-		/>
-		<filter-autosuggest
-			:autosuggestProps="issues"
-			component="field-select-issues"
-			param="issueIds"
-			:value="[]"
-			title="Assigned to Issues"
-			@add-filter="addFilter"
-			@remove-filter="removeFilter"
-		/>
-	</div>
+    <div class="previewFilters">
+        <filter-autosuggest
+            :autosuggestProps="users"
+            component="field-select-users"
+            param="assignedTo"
+            :value="[]"
+            title="Assigned to Editors"
+            @add-filter="addFilter"
+            @remove-filter="removeFilter"
+        />
+        <filter-autosuggest
+            :autosuggestProps="issues"
+            component="field-select-issues"
+            param="issueIds"
+            :value="[]"
+            title="Assigned to Issues"
+            @add-filter="addFilter"
+            @remove-filter="removeFilter"
+        />
+    </div>
 </template>
 
 <script>
@@ -27,40 +27,40 @@ import fieldBase from '../../Form/helpers/field-base';
 import fieldBaseAutosuggest from '../../Form/helpers/field-autosuggest';
 
 export default {
-	components: {
-		FilterAutosuggest
-	},
-	data() {
-		return {
-			users: {
-				...fieldBase,
-				...fieldBaseAutosuggest,
-				name: 'assignedTo',
-				label: 'Assigned to Editors',
-				selectedLabel: 'Assigned',
-				apiUrl: '/usernames.json'
-			},
-			issues: {
-				...fieldBase,
-				...fieldBaseAutosuggest,
-				name: 'issueIds',
-				label: 'Assigned to Issues',
-				selectedLabel: 'Assigned',
-				apiUrl: '/issues.json'
-			},
-			activeFilters: {
-				assignedTo: [],
-				issueIds: []
-			}
-		};
-	},
-	methods: {
-		addFilter(param, newVal) {
-			this.activeFilters[param] = newVal;
-		},
-		removeFilter(param) {
-			this.activeFilters[param] = [];
-		}
-	}
+    components: {
+        FilterAutosuggest
+    },
+    data() {
+        return {
+            users: {
+                ...fieldBase,
+                ...fieldBaseAutosuggest,
+                name: 'assignedTo',
+                label: 'Assigned to Editors',
+                selectedLabel: 'Assigned',
+                apiUrl: '/usernames.json'
+            },
+            issues: {
+                ...fieldBase,
+                ...fieldBaseAutosuggest,
+                name: 'issueIds',
+                label: 'Assigned to Issues',
+                selectedLabel: 'Assigned',
+                apiUrl: '/issues.json'
+            },
+            activeFilters: {
+                assignedTo: [],
+                issueIds: []
+            }
+        };
+    },
+    methods: {
+        addFilter(param, newVal) {
+            this.activeFilters[param] = newVal;
+        },
+        removeFilter(param) {
+            this.activeFilters[param] = [];
+        }
+    }
 };
 </script>
