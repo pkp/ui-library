c730d3b83c63f60fd303c299ab9c87f95b49e51e
thinkbulecount2 <38994650+thinkbulecount2@users.noreply.github.com>
2020-07-06 11:24:19 -0400

    pkp/pkp-lib#6054 Edited field-autosuggest form helper

---
+++ src/docs/components/Form/helpers/field-autosuggest.js
@@ -0,0 +1,10 @@
+export default {
+	name: 'autosuggest',
+	component: 'field-autosuggest',
+	label: '',
+	value: [],
+	deselectLabel: 'Remove {$item}',
+	initialPosition: 'inline',
+	selectedLabel: 'Selected',
+	apiUrl: ''
+};
