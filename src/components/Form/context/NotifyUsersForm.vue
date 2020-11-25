eda42e56265399f875ff64353a9a159bee6630a1
Nate Wright <natew@notthisway.com>
2020-11-25 13:22:48 +0000

    pkp/pkp-lib#4017 Add components to send bulk emails

---
+++ src/components/Form/context/NotifyUsersForm.vue
@@ -0,0 +1,49 @@
+<script>
+import PkpForm from '../Form.vue';
+
+export default {
+	name: 'NotifyUsersForm',
+	extends: PkpForm,
+	props: {
+		confirmLabel: {
+			type: String,
+			required: true
+		},
+		userGroupCounts: {
+			type: Object,
+			required: true
+		},
+		sendLabel: {
+			type: String,
+			required: true
+		}
+	},
+	methods: {
+		/**
+		 * Require user confirmation before submitting the form
+		 */
+		nextPage: function(pageId) {
+			let totalUserCount = 0;
+			if (this.submitValues.userGroupIds) {
+				totalUserCount = this.submitValues.userGroupIds.reduce(
+					(total, userGroupId) => {
+						return total + this.userGroupCounts[userGroupId];
+					},
+					0
+				);
+			}
+			this.openDialog({
+				cancelLabel: this.__('common.cancel'),
+				confirmLabel: this.sendLabel,
+				message: this.confirmLabel.replace('{$total}', totalUserCount),
+				modalName: 'confirmNotify',
+				title: this.sendLabel,
+				callback: () => {
+					this.submit();
+					this.$modal.hide('confirmNotify');
+				}
+			});
+		}
+	}
+};
+</script>
