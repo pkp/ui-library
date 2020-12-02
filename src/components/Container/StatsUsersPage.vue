a50418fcff344a44346a9ede51c3e695ff43efc7
Nate Wright <natew@notthisway.com>
2020-12-02 10:30:39 +0000

    pkp/pkp-lib#5642 Fix user csv export by creating handler to access global window

---
+++ src/components/Container/StatsUsersPage.vue
@@ -0,0 +1,19 @@
+<script>
+import StatsPage from './StatsPage.vue';
+
+export default {
+	name: 'StatsUsersPage',
+	extends: StatsPage,
+	methods: {
+		/**
+		 * Load the user export
+		 *
+		 * @param {String} url
+		 */
+		loadExport(url) {
+			window.location = url;
+			this.$modal.hide('export');
+		}
+	}
+};
+</script>
