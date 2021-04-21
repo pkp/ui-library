36d914c8c9169bc3a8622a2dd060309c1bc342ad
Nate Wright <natew@notthisway.com>
2021-04-21 16:57:27 +0100

    pkp/pkp-lib#6969 Fix email template description field shoudln't have value

--- src/components/ListPanel/emailTemplates/EmailTemplatesListPanel.vue
+++ src/components/ListPanel/emailTemplates/EmailTemplatesListPanel.vue
@@ -365,7 +365,10 @@ export default {
 			}
 
 			activeForm.fields = activeForm.fields.map(field => {
-				if (Object.keys(emailTemplate).includes(field.name)) {
+				if (
+					Object.keys(emailTemplate).includes(field.name) &&
+					field.name !== 'description'
+				) {
 					field.value = emailTemplate[field.name];
 				}
 				return field;
