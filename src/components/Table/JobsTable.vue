975f8a84ef4bdb1e22a495867b5970f3db6eece8
Henrique Ramos <henrique@henriqueramos.eti.br>
2021-12-16 22:31:54 -0300

    pkp/pkp-lib#7171 Creating custom components for queued jobs GUI

---
+++ src/components/Table/JobsTable.vue
@@ -0,0 +1,19 @@
+<template>
+	<pkp-table
+		:columns="table_data.columns"
+		:rows="table_data.rows"
+		:description="table_data.description"
+		:label="table_data.label"
+	/>
+</template>
+
+<script>
+import PkpTable from '@/components/Table/Table.vue';
+
+export default {
+	components: {
+		PkpTable
+	},
+	props: ['table_data']
+};
+</script>
