695ff5473ae05317ed760237d2e16398ba38e8ae
Nate Wright <natew@notthisway.com>
2022-03-17 16:25:01 +0000

    pkp/pkp-lib#7772 Remove unused prop

--- src/docs/components/Form/helpers/field-controlled-vocab-keywords.js
+++ src/docs/components/Form/helpers/field-controlled-vocab-keywords.js
@@ -6,7 +6,6 @@ export default {
 		'Keywords are typically one- to three-word phrases that are used to indicate the main topics of a submission.',
 	value: [],
 	deselectLabel: 'Remove {$item}',
-	initialPosition: 'inline',
 	noneLabel: 'None',
 	selectedLabel: 'Selected',
 	apiUrl: ''
