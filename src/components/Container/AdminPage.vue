cec434def46c50aec70b7b5898964c079fea5964
Nate Wright <natew@notthisway.com>
2022-03-31 12:12:39 +0100

    pkp/pkp-lib#7799 Use ActionPanel on admin page

--- src/components/Container/AdminPage.vue
+++ src/components/Container/AdminPage.vue
@@ -1,11 +1,13 @@
 <script type="text/javascript">
 import Page from '@/components/Container/Page.vue';
 import ThemeForm from '@/components/Form/context/ThemeForm.vue';
+import ActionPanel from '../ActionPanel/ActionPanel.vue';
 
 export default {
 	extends: Page,
 	name: 'AdminPage',
 	components: {
+		ActionPanel,
 		ThemeForm
 	}
 };
@@ -17,6 +19,19 @@ export default {
 .pkp_page_admin {
 	.pkpNotification {
 		margin-bottom: 2rem;
+	}
+
+	.actionPanel {
+		margin-bottom: 1.5rem;
+		padding: 1rem;
+		border: @bg-border-light;
+		border-radius: @radius;
+		background: @lift;
+	}
+
+	// Account for <form> wrappers around some actions
+	.actionPanel__actions form + form {
+		margin-top: 0.25rem;
 	}
 
 	.app__contentPanel {
@@ -31,24 +46,6 @@ export default {
 
 		h2:first-child {
 			margin-top: 0;
-		}
-
-		h3 {
-			margin-bottom: 0.5rem;
-		}
-
-		ul {
-			margin: 0;
-			padding: 0;
-			list-style: none;
-		}
-
-		li {
-			padding: 0.25em 0;
-		}
-
-		ul + h2 {
-			margin-top: 1rem;
 		}
 
 		.app--admin__systemInfoGroup {
