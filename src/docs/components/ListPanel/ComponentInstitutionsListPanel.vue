c4f9940436f25be78397efc9e3106b924c12bbbb
Bozana Bokan <bozana.bokan@posteo.net>
2022-05-10 12:57:01 +0200

    add institutions list panel to docs

---
+++ src/docs/components/ListPanel/ComponentInstitutionsListPanel.vue
@@ -0,0 +1,21 @@
+<script>
+import Component from '@/docs/Component.vue';
+import PreviewInstitutionsListPanel from './previews/PreviewInstitutionsListPanel.vue';
+import PreviewInstitutionsListPanelTemplate from '!raw-loader!./previews/PreviewInstitutionsListPanel.vue';
+
+export default {
+	extends: Component,
+	data() {
+		return {
+			name: 'InstitutionsListPanel',
+			examples: [
+				{
+					component: PreviewInstitutionsListPanel,
+					name: 'Base',
+					template: this.extractTemplate(PreviewInstitutionsListPanelTemplate)
+				}
+			]
+		};
+	}
+};
+</script>
---
+++ src/docs/components/ListPanel/previews/PreviewInstitutionsListPanel.vue
@@ -0,0 +1,69 @@
+<template>
+	<institutions-list-panel
+		addInstitutionLabel="Add Institution"
+		apiUrl=""
+		confirmDeleteMessage="Are you sure you want to delete this institution?"
+		deleteInstitutionLabel="Delete Institution"
+		editInstitutionLabel="Edit Institution"
+		:form="form"
+		id="previewInstitutionsListPanel"
+		:items="items"
+		:itemsMax="itemsMax"
+		title="Institutions"
+		urlBase="https://example.com/institution/view/__id__"
+	/>
+</template>
+<script>
+import InstitutionsListPanel from '@/components/ListPanel/institutions/InstitutionsListPanel.vue';
+import form from '@/docs/components/Form/helpers/form-institution';
+
+export default {
+	components: {
+		InstitutionsListPanel
+	},
+	data() {
+		const institutions = [
+			{
+				id: 1,
+				name: {
+					en_US: 'Institution 1 Name EN',
+					fr_CA: 'Institution 1 Name FR',
+					ar_AR: 'Institution 1 Name AR'
+				},
+				ipRanges: ['142.60.*.*']
+			},
+			{
+				id: 2,
+				name: {
+					en_US: 'Institution 2 Name EN',
+					fr_CA: 'Institution 2 Name FR',
+					ar_AR: 'Institution 2 Name AR'
+				},
+				ipRanges: ['142.58.103.1 - 142.58.103.4', '142.59.*.*']
+			},
+			{
+				id: 3,
+				name: {
+					en_US: 'Institution 3 Name EN',
+					fr_CA: 'Institution 3 Name FR',
+					ar_AR: 'Institution 3 Name AR'
+				}
+			},
+			{
+				id: 4,
+				name: {
+					en_US: 'Institution 4 Name EN',
+					fr_CA: 'Institution 4 Name FR',
+					ar_AR: 'Institution 4 Name AR'
+				}
+			}
+		];
+
+		return {
+			form: {...form},
+			items: [...institutions],
+			itemsMax: institutions.length
+		};
+	}
+};
+</script>
