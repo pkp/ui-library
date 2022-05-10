d2e8efb37df7312f06ba3175060ea4751ac54eb9
Bozana Bokan <bozana.bokan@posteo.net>
2022-05-10 12:56:17 +0200

    fixes after code review

--- src/components/Container/SettingsPage.vue
+++ src/components/Container/SettingsPage.vue
@@ -43,9 +43,7 @@ export default {
 					let menu = {};
 					Object.keys(this.menu).forEach(key => {
 						if (key === 'settings') {
-							if (!this.menu['institutions']) {
-								menu.institutions = this.institutionsNavLink;
-							}
+							menu.institutions = this.institutionsNavLink;
 							menu.payments = this.paymentsNavLink;
 						}
 						menu[key] = this.menu[key];
@@ -56,7 +54,11 @@ export default {
 
 			// Add or remove institutions nav link
 			if (formId === pkp.const.FORM_CONTEXT_STATISTICS) {
-				if (!context.enableInstitutionUsageStats && !this.menu['payments'] && !!this.menu['institutions']) {
+				if (
+					!context.enableInstitutionUsageStats &&
+					!this.menu['payments'] &&
+					!!this.menu['institutions']
+				) {
 					let menu = {...this.menu};
 					delete menu.institutions;
 					this.menu = menu;
@@ -87,7 +89,11 @@ export default {
 				) {
 					let menu = {};
 					Object.keys(this.menu).forEach(key => {
-						if (key === 'settings' || key === 'payments' || key === 'institutions') {
+						if (
+							key === 'settings' ||
+							key === 'payments' ||
+							key === 'institutions'
+						) {
 							menu.announcements = this.announcementsNavLink;
 						}
 						menu[key] = this.menu[key];
