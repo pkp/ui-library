b76f139cc81ed76d4c472161d9a7e4754c85fc68
Bozana Bokan <bozana.bokan@posteo.net>
2022-10-25 19:17:45 +0200

    pkp/pkp-lib#7318 stats issues page changes, after second code review

--- src/components/Container/StatsIssuesPage.vue
+++ src/components/Container/StatsIssuesPage.vue
@@ -1,54 +1,17 @@
 <script>
 import StatsPublicationsPage from '@/components/Container/StatsPublicationsPage.vue';
-import Tooltip from '@/components/Tooltip/Tooltip.vue';
 
 export default {
 	name: 'StatsIssuesPage',
 	extends: StatsPublicationsPage,
-	components: {
-		Tooltip
-	},
 	methods: {
 		/**
-		 * Download the CSV report based on the current params
+		 * Get report file name part by type
+		 *
+		 * @return string
 		 */
-		downloadReport() {
-			this.isDownloadingReport = true;
-
-			let downloadFileName =
-				[
-					'stats',
-					'issues',
-					this.getDateRangeDescription()
-						.replaceAll(' ', '')
-						.replace('to', '_'),
-					this.searchPhrase ?? ''
-				]
-					.filter(i => i) // removes empty values. it is the same as function(i) => { return $i ? $i : false}
-					.join('_') + '.csv';
-
-			$.ajax({
-				url: this.apiUrl,
-				type: 'GET',
-				context: this,
-				headers: {
-					Accept: 'text/csv; charset=utf-8',
-					'Content-Type': 'text/csv;·charset_utf-8'
-				},
-				data: this.getReportParams(),
-				error: this.ajaxErrorCallback,
-				success(r) {
-					var blob = new Blob([r]);
-					var link = document.createElement('a');
-					link.href = window.URL.createObjectURL(blob);
-					link.download = downloadFileName;
-					link.click();
-				},
-				complete(r) {
-					this.isDownloadingReport = false;
-					this.$modal.hide('export');
-				}
-			});
+		getReportFileNamePart(type) {
+			return 'issues';
 		}
 	}
 };
