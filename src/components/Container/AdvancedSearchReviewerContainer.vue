9c3039721e686cd87d14541cd0f8743b2ad651af
Rob Arnold <rob.arnold@ubiquitypress.com>
2023-08-01 10:31:57 +0100

    vue changes for author list action text changes

--- src/components/Container/AdvancedSearchReviewerContainer.vue
+++ src/components/Container/AdvancedSearchReviewerContainer.vue
@@ -2,32 +2,28 @@
 	<div>
 		<div class="section">
 			<h3>{{ components.selectReviewer.submissionAuthorList }}</h3>
-			<div :class="authorContainerclasses">
-				<div
-					class="author_row"
-					v-for="(affiliations, author) in components.selectReviewer.authors"
-					:key="author"
-				>
-					<span>{{ author }}</span>
-					<span v-if="affiliations"> - </span>
-					<span>{{ affiliations }}</span>
+			<div class="list_outline">
+				<ol :class="authorContainerclasses">
+					<li
+						class="author_row"
+						v-for="(affiliations, author) in components.selectReviewer.authors"
+						:key="author"
+					>
+						<strong>{{ author }}</strong>
+						<span v-if="affiliations"> - </span>
+						<span>{{ affiliations }}</span>
+					</li>
+				</ol>
+				<div class="action_container" v-if="authorCount > 4">
+					<span class="show_authors_action" v-if="!showAllAuthors" @click="toggleShowAllAuthors">
+						{{
+							`${components.selectReviewer.showAll} ${authorCount} ${components.selectReviewer.authorsLabel}`
+						}}
+					</span>
+					<span class="show_authors_action" v-if="showAllAuthors" @click="toggleShowAllAuthors">
+						{{ components.selectReviewer.showLess }}
+					</span>
 				</div>
-			</div>
-			<div v-if="Object.keys(components.selectReviewer.authors).length > 4">
-				<button
-					v-if="!showAllAuthors"
-					@click="toggleShowAllAuthors"
-					class="pkpButton pkp_helpers_align_right"
-				>
-					{{ components.selectReviewer.showMore }}
-				</button>
-				<button
-					v-if="showAllAuthors"
-					@click="toggleShowAllAuthors"
-					class="pkpButton pkp_helpers_align_right"
-				>
-					{{ components.selectReviewer.showLess }}
-				</button>
 			</div>
 		</div>
 		<select-reviewer-list-panel v-bind="components.selectReviewer" />
@@ -54,9 +50,39 @@ export default {
 	},
 	computed: {
 		authorContainerclasses() {
-			let statics = 'pkp_list_box pkpFormField__description';
+			let statics = 'list_box pkpFormField__description';
 			return this.showAllAuthors ? statics : `${statics} expandable`;
-		}
-	}
+		},
+		authorCount() {
+			return Object.keys(this.components.selectReviewer.authors).length;
+		},
+	},
 };
 </script>
+<style lang="less">
+@import '../../styles/_import';
+
+.action_container {
+	padding: 0.9rem 1.2rem;
+}
+
+.list_box {
+	margin-bottom: 0;
+	&.expandable {
+		max-height: 6rem;
+		overflow-y: scroll;
+	}
+}
+
+.list_outline {
+	border: 1px solid @bg-border-color-light;
+	border-radius: 2px;
+	margin-bottom: 1.3rem;
+}
+
+.show_authors_action {
+	color: @primary;
+	text-decoration: none;
+	cursor: pointer;
+}
+</style>
