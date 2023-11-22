88f2950e853429b7541c9544e560eb1f2c8cd501
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2023-11-22 10:12:17 +0100

    pkp/pkp-lib#9527 Initial storybook setup (#295)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
 		'plugin:vue/vue3-essential',
 		'eslint:recommended',
 		'@vue/eslint-config-prettier/skip-formatting',
+		'plugin:storybook/recommended',
 	],
 	globals: {
 		$: true,
