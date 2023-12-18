1ff15e58cefcb692ea37b3ad896ba614cdd6a450
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2023-12-18 12:04:47 +0100

    pkp/pkp-lib#9527 Add rtl snapshoting

--- .storybook/modes.js
+++ .storybook/modes.js
@@ -2,6 +2,10 @@ export const allModes = {
 	desktop: {
 		viewport: 'large',
 	},
+	'desktop rtl': {
+		viewport: 'large',
+		direction: 'rtl',
+	},
 	// for snapshotting scrollable areas with all content, like modals
 	desktopLargeHeight: {
 		viewport: 'largeHeight',
