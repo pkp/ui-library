794cc6ed4714fa34da2111038486fd1367c53da4
Nicolas Boulay <nibou230@ulaval.ca>
2024-02-01 15:33:42 -0500

    pkp/pkp-lib#9453 Refactor the round history UI to follow the proper design

--- src/pages/reviewerSubmission/ReviewerSubmissionPage.vue
+++ src/pages/reviewerSubmission/ReviewerSubmissionPage.vue
@@ -1,15 +1,24 @@
 <template>
-	<div class="mb-4 space-x-2">
-		<span class="bg-medium text-lg-normal">
-			{{ t('reviewer.submission.reviewRound.info') }}
-		</span>
-		<PkpButton
+	<div class="panel p-8 mb-4">
+		<h2>{{ t('reviewer.submission.reviewRound.info') }}</h2>
+		<p
 			v-for="review in store.reviewRoundHistories"
 			:key="review.reviewRoundId"
-			@click="store.openRoundHistoryModal(review)"
 		>
-			{{ t('common.reviewRoundNumber', {round: review.reviewRoundNumber}) }}
-		</PkpButton>
+			<span class="text-lg-normal">
+				{{ t(
+					'reviewer.submission.reviewRound.info.submittedOn',
+					{round: review.reviewRoundNumber, submittedOn: formatShortDate(review.submittedOn)}
+				) }}
+			</span>
+			<PkpButton
+				is-link="true"
+				class="ml-4"
+				@click="store.openRoundHistoryModal(review)"
+			>
+				{{ t('reviewer.submission.reviewRound.info.read', {round: review.reviewRoundNumber}) }}
+			</PkpButton>
+		</p>
 	</div>
 	<SideModal
 		:open="store.isRoundHistoryModalOpened"
@@ -23,13 +32,15 @@
 
 <script setup>
 import {defineProps} from 'vue';
-
 import SideModal from '@/components/Modal/SideModal.vue';
 import RoundHistoryModal from './RoundHistoryModal.vue';
-
 import {useTranslation} from '@/composables/useTranslation';
-
 import {useReviewerSubmissionPageStore} from './reviewerSubmissionPageStore';
+import moment from 'moment';
+
+function formatShortDate(dateString) {
+	return moment(dateString).format('DD-MM-YYYY');
+}
 
 const {t} = useTranslation();
 
--- src/pages/reviewerSubmission/RoundHistoryModal.vue
+++ src/pages/reviewerSubmission/RoundHistoryModal.vue
@@ -1,18 +1,18 @@
 <template>
 	<SideModalBody v-if="store.reviewRoundHistory">
+		<template v-if="store.reviewRoundHistory.reviewAssignment" #pre-title>
+			{{ store.reviewRoundHistory.reviewAssignment.submissionId }}
+		</template>
 		<template #title>
-			<h1>{{ t('reviewer.submission.reviewRound.info.history') }}</h1>
+			<h1 class="underline">
+				{{ t('reviewer.submission.reviewRound.info.modal.title', {round: store.reviewRoundNumber}) }}
+			</h1>
 		</template>
 		<template v-if="store.reviewRoundHistory.publicationTitle" #description>
-			<h2>
-				{{ t(
-					'reviewer.submission.reviewRound.info.modal.title',
-					{reviewRoundNumber: store.reviewRoundNumber, submissionTitle: store.reviewRoundHistory.publicationTitle}
-				) }}
-			</h2>
+			{{ store.reviewRoundHistory.publicationTitle }}
 		</template>
-		<div class="p-4">
-			<div class="bg-lightest p-5">
+		<SideModalLayout2Columns>
+			<template #left>
 				<div v-if="store.reviewRoundHistory.reviewAssignment.declined">
 					<Panel v-bind="panelOptions" class="mb-4">
 						<PanelSection>
@@ -45,72 +45,97 @@
 						</PanelSection>
 					</Panel>
 
-					<Panel v-if="store.reviewRoundHistory.comments.length || store.reviewRoundHistory.privateComments.length" v-bind="panelOptions" class="mb-4">
-						<PanelSection>
-							<template #header>
-								<h2>{{ t('reviewer.submission.reviewRound.comments.review') }}</h2>
-							</template>
-							<PanelSection v-if="store.reviewRoundHistory.comments.length">
-								<template #header>
-									<h3>{{ t('reviewer.submission.reviewRound.comments.authorAndEditor') }}</h3>
-								</template>
-								<p v-for="reviewComment in store.reviewRoundHistory.comments" v-html="reviewComment"></p>
-							</PanelSection>
-							<PanelSection v-if="store.reviewRoundHistory.privateComments.length">
-								<template #header>
-									<h3>{{ t('reviewer.submission.reviewRound.comments.editorOnly') }}</h3>
-								</template>
-								<p v-for="reviewComment in store.reviewRoundHistory.privateComments" v-html="reviewComment"></p>
-							</PanelSection>
-						</PanelSection>
-					</Panel>
-
-					<Panel v-bind="panelOptions" class="mb-4">
-						<PanelSection>
-							<template #header>
-								<h2>{{ t('manager.setup.generalInformation') }}</h2>
-							</template>
-							<List>
-								<ListItem>
-									<p class="inline pr-2 font-bold">{{ t('reviewer.submission.reviewRequestDate') }}</p>{{ formatShortDate(store.reviewRoundHistory.reviewAssignment.dateNotified) }}
-								</ListItem>
-								<ListItem>
-									<p class="inline pr-2 font-bold">{{ t('reviewer.submission.responseDueDate') }}</p>{{ formatShortDate(store.reviewRoundHistory.reviewAssignment.dateResponseDue) }}
-								</ListItem>
-								<ListItem>
-									<p class="inline pr-2 font-bold">{{ t('reviewer.submission.reviewDueDate') }}</p>{{ formatShortDate(store.reviewRoundHistory.reviewAssignment.dateDue) }}
-								</ListItem>
-								<ListItem>
-									<p class="inline pr-2 font-bold">{{ t('common.dateCompleted') }}</p>{{ formatShortDate(store.reviewRoundHistory.reviewAssignment.dateCompleted) }}
-								</ListItem>
-							</List>
-						</PanelSection>
-					</Panel>
-
-					<div class="mb-4">
-						<p>TODO: display attachments grid</p>
+					<div v-if="store.reviewRoundHistory.comments.length || store.reviewRoundHistory.privateComments.length" class="border border-light mb-4">
+						<div class="border-b border-light p-4">
+							<h2 class="text-2xl-bold">{{ t('reviewer.submission.reviewRound.comments') }}</h2>
+						</div>
+						<div v-if="store.reviewRoundHistory.comments.length" class="border-b border-light p-4">
+							<h2 class="text-xl-bold text-light mb-4">{{ t('reviewer.submission.reviewRound.comments.authorAndEditor') }}</h2>
+							<p v-for="(reviewComment, index) in store.reviewRoundHistory.comments">
+								<span class="text-xl-bold">{{ t('reviewer.submission.reviewRound.comments.prefix', {index: index + 1}) }}</span>
+								<span v-html="reviewComment"></span>
+							</p>
+						</div>
+						<div v-if="store.reviewRoundHistory.privateComments.length" class="p-4">
+							<h2 class="text-xl-bold text-light mb-4">{{ t('reviewer.submission.reviewRound.comments.editorOnly') }}</h2>
+							<p v-for="(reviewComment, index) in store.reviewRoundHistory.privateComments">
+								<span class="text-xl-bold">{{ t('reviewer.submission.reviewRound.comments.prefix', {index: index + 1}) }}</span>
+								<span v-html="reviewComment"></span>
+							</p>
+						</div>
 					</div>
 
-					<div v-if="store.reviewRoundHistory.displayFiles" class="mb-4">
-						<p>TODO: display files grid</p>
-					</div>
+					<ListingFilesListPanel
+						v-if="store.reviewRoundHistory.attachments.length"
+						class="mb-4"
+						:title="t('reviewer.submission.reviewRound.attachments')"
+						:description="t('reviewer.submission.reviewRound.attachments.description')"
+						:files="store.reviewRoundHistory.attachments"
+					></ListingFilesListPanel>
+
+					<ListingFilesListPanel
+						v-if="store.reviewRoundHistory.files.length"
+						:title="t('reviewer.submission.reviewRound.files')"
+						:description="t('reviewer.submission.reviewRound.files.description')"
+						:files="store.reviewRoundHistory.files"
+					></ListingFilesListPanel>
 				</div>
-			</div>
-		</div>
+			</template>
+			<template #right1>
+				<div class="text-xl-bold">{{ t('reviewer.submission.reviewRound.metadata') }}</div>
+				<div v-if="store.reviewRoundHistory.publicationType" class="mt-4">
+					<div class="text-lg-bold">{{ t('reviewer.submission.reviewRound.metadata.type') }}:</div>
+					<div class="text-base-normal mt-1">{{ store.reviewRoundHistory.publicationType }}</div>
+				</div>
+				<div v-if="store.reviewRoundHistory.publicationAbstract" class="mt-4">
+					<div class="text-lg-bold">{{ t('reviewer.submission.reviewRound.metadata.abstract') }}:</div>
+					<div v-html="store.reviewRoundHistory.publicationAbstract" class="text-base-normal mt-1"></div>
+				</div>
+				<div v-if="store.reviewRoundHistory.publicationKeywords" class="mt-4">
+					<div class="text-lg-bold">{{ t('reviewer.submission.reviewRound.metadata.keywords') }}:</div>
+					<div class="text-base-normal mt-1">{{ store.reviewRoundHistory.publicationKeywords }}</div>
+				</div>
+			</template>
+			<template v-if="store.reviewRoundHistory.reviewAssignment" #right2>
+				<div class="text-xl-bold">
+					{{ t('reviewer.submission.reviewRound.general') }}
+				</div>
+				<div v-if="store.reviewRoundHistory.reviewAssignment.dateNotified" class="mt-4">
+					<div class="text-lg-bold">{{ t('reviewer.submission.reviewRequestDate') }}:</div>
+					<div class="text-base-normal mt-1">{{ formatShortDate(store.reviewRoundHistory.reviewAssignment.dateNotified) }}</div>
+				</div>
+				<div v-if="store.reviewRoundHistory.reviewAssignment.dateResponseDue" class="mt-4">
+					<div class="text-lg-bold">{{ t('reviewer.submission.responseDueDate') }}:</div>
+					<div class="text-base-normal mt-1">{{ formatShortDate(store.reviewRoundHistory.reviewAssignment.dateResponseDue) }}</div>
+				</div>
+				<div v-if="store.reviewRoundHistory.reviewAssignment.dateConfirmed" class="mt-4">
+					<div class="text-lg-bold">{{ t('reviewer.submission.acceptedOn') }}:</div>
+					<div class="text-base-normal mt-1">{{ formatShortDate(store.reviewRoundHistory.reviewAssignment.dateConfirmed) }}</div>
+				</div>
+				<div v-if="store.reviewRoundHistory.reviewAssignment.dateDue" class="mt-4">
+					<div class="text-lg-bold">{{ t('reviewer.submission.reviewDueDate') }}:</div>
+					<div class="text-base-normal mt-1">{{ formatShortDate(store.reviewRoundHistory.reviewAssignment.dateDue) }}</div>
+				</div>
+				<div v-if="store.reviewRoundHistory.reviewAssignment.dateCompleted" class="mt-4">
+					<div class="text-lg-bold">{{ t('reviewer.submission.submittedOn') }}:</div>
+					<div class="text-base-normal mt-1">{{ formatShortDate(store.reviewRoundHistory.reviewAssignment.dateCompleted) }}</div>
+				</div>
+			</template>
+		</SideModalLayout2Columns>
 	</SideModalBody>
 </template>
 
 <script setup>
 import {defineProps} from 'vue';
 import SideModalBody from '@/components/Modal/SideModalBody.vue';
+import SideModalLayout2Columns from "@/components/Modal/SideModalLayout2Columns.vue";
+import ListingFilesListPanel from "@/components/ListPanel/listingFiles/ListingFilesListPanel.vue";
 import {useTranslation} from '@/composables/useTranslation';
 import {useRoundHistoryModalStore} from './roundHistoryModalStore';
 import moment from 'moment';
-import List from "@/components/List/List.vue";
-import ListItem from "@/components/List/ListItem.vue";
 
 function formatShortDate(dateString) {
-	return moment(dateString).format('YYYY-MM-DD');
+	return moment(dateString).format('DD-MM-YYYY');
 }
 
 const panelOptions = { stack: true };
