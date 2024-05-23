2575193a32b4ad10ab5340cf506a04c103b8dee2
Dimitris Efstathiou <defstat@gmail.com>
2024-05-23 14:16:53 +0300

    pkp/pkp-lib#9840 Add mdx and stories for new component

---
+++ src/components/CodeHighlighter/CodeHighlighter.stories.js
@@ -0,0 +1,37 @@
+import CodeHighlighter from './CodeHighlighter.vue';
+
+export default {
+	title: 'Components/CodeHighlighter',
+	component: CodeHighlighter,
+	render: (args) => ({
+		components: {CodeHighlighter},
+		setup() {
+			return {args};
+		},
+		template: '<code-highlighter :code=args.code :language=args.language />',
+	}),
+};
+
+const sampleXML = `<?xml version="1.0" encoding="UTF-8"?>
+<article>
+<title>Open Access in Research</title>
+<author>
+    <name>Jane Doe</name>
+    <affiliation>University of Knowledge</affiliation>
+</author>
+<journal>
+    <name>Journal of Open Science</name>
+    <year>2023</year>
+</journal>
+<doi>10.1234/jos.2023.001</doi>
+<license type="CC-BY">https://creativecommons.org/licenses/by/4.0/</license>
+<abstract>
+    This paper explores the significance of open access in scientific research.
+</abstract>`;
+
+export const XML = {
+	args: {
+		code: sampleXML,
+		language: 'xml',
+	},
+};
--- src/components/CodeHighlighter/CodeHighlighter.vue
+++ src/components/CodeHighlighter/CodeHighlighter.vue
@@ -1,13 +1,9 @@
 <template>
-    <highlightjs
-        :language="language"
-        :code="code"
-    />
+	<highlightjs :language="language" :code="code" />
 </template>
 
 <script>
-// import hljs from 'highlight.js/lib/core';
-import hljs from 'highlight.js/lib/common';
+import hljs from 'highlight.js/lib/core';
 import hljsVuePlugin from '@highlightjs/vue-plugin';
 
 // Importing the language modules
@@ -16,51 +12,53 @@ import xml from 'highlight.js/lib/languages/xml';
 
 // A map to store language modules
 const languageMap = {
-    xml,
-    // Add more language modules imported above
+	xml,
+	// Add more language modules imported above
 };
 
 const supportedLanguages = Object.keys(languageMap);
 
 export default {
-    components: {
-        highlightjs: hljsVuePlugin.component
-    },
-    props: {
-        code: {
-            type: String,
-            required: true
-        },
+	components: {
+		highlightjs: hljsVuePlugin.component,
+	},
+	props: {
+		code: {
+			type: String,
+			required: true,
+		},
 
-        language: {
-            type: String,
-            required: true,
-            validator(value) {
-                if (!supportedLanguages.includes(value)) {
-                    console.error(`Unsupported language: ${value}. Supported languages are: ${supportedLanguages.join(', ')}`);
-                    return false;
-                }
+		language: {
+			type: String,
+			required: true,
+			validator(value) {
+				if (!supportedLanguages.includes(value)) {
+					console.error(
+						`Unsupported language: ${value}. Supported languages are: ${supportedLanguages.join(', ')}`,
+					);
+					return false;
+				}
 
-                return true;
-            }
-        }
-    },
-    created() {
-        this.loadLanguageModule(this.language);
-    },
-    methods: {
-        loadLanguageModule(language) {
-            const languageModule = languageMap[language];
-            if (languageModule) {
-                hljs.registerLanguage(language, languageModule);
-            } else {
-                console.error(`Language module for ${language} is not available.`);
-            }
-        }
-    }
+				return true;
+			},
+		},
+	},
+	created() {
+		this.loadLanguageModule(this.language);
+	},
+	methods: {
+		loadLanguageModule(language) {
+			const languageModule = languageMap[language];
+			if (languageModule) {
+				hljs.registerLanguage(language, languageModule);
+			} else {
+				console.error(`Language module for ${language} is not available.`);
+			}
+		},
+	},
 };
 </script>
 
 <style>
-    @import 'highlight.js/styles/default.css';
+@import 'highlight.js/styles/default.css';
 </style>
