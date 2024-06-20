0e49e788988d1eca3a787d76685e8c4f6c018efc
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2024-06-20 13:07:54 +0200

    pkp/pkp-lib#9992 users stats export modal migrated to side modal

---
+++ src/pages/statsUsers/UserExportModal.vue
@@ -0,0 +1,26 @@
+<template>
+	<SideModalBody>
+		<template #title>
+			{{ title }}
+		</template>
+		<SideModalLayoutBasic>
+			<PkpForm
+				v-bind="usersReportForm"
+				@set="(...args) => emit('set', ...args)"
+				@success="(...args) => emit('loadExport', ...args)"
+			/>
+		</SideModalLayoutBasic>
+	</SideModalBody>
+</template>
+
+<script setup>
+import SideModalBody from '@/components/Modal/SideModalBody.vue';
+import SideModalLayoutBasic from '@/components/Modal/SideModalLayoutBasic.vue';
+import PkpForm from '@/components/Form/Form.vue';
+
+defineProps({
+	title: {type: String, required: true},
+	usersReportForm: {type: Object, required: true},
+});
+const emit = defineEmits(['set', 'loadExport']);
+</script>
