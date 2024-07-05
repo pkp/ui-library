0e91bc1338e4f306cf355d05716de45d5878e123
Jonas Raoni Soares da Silva <github@raoni.org>
2024-07-05 21:00:02 +0300

    pkp/pkp-lib#10170 Fixed synchronization of the reviewer container

--- src/components/Container/AdvancedSearchReviewerContainer.vue
+++ src/components/Container/AdvancedSearchReviewerContainer.vue
@@ -5,34 +5,37 @@
 			<div class="list_outline">
 				<ol class="list_box">
 					<li
-							class="author_row"
-							v-for="(affiliations, author) in displayedItems"
-							:key="author"
+						class="author_row"
+						v-for="(affiliations, author) in displayedItems"
+						:key="author"
 					>
 						<strong>{{ author }}</strong>
-						<span v-if="affiliations"> - </span>
+						<span v-if="affiliations">-</span>
 						<span>{{ affiliations }}</span>
 					</li>
 				</ol>
 				<div class="action_container" v-if="authorCount > 4">
 					<span
-				  class="show_authors_action"
-				  v-if="!showAllAuthors"
-				  @click="toggleShowAllAuthors"
-		  >
+						class="show_authors_action"
+						v-if="!showAllAuthors"
+						@click="toggleShowAllAuthors"
+					>
 						{{ `${labels.showAll} ${authorCount} ${labels.authorsLabel}` }}
 					</span>
 					<span
-							class="show_authors_action"
-							v-if="showAllAuthors"
-							@click="toggleShowAllAuthors"
+						class="show_authors_action"
+						v-if="showAllAuthors"
+						@click="toggleShowAllAuthors"
 					>
 						{{ labels.showLess }}
 					</span>
 				</div>
 			</div>
 		</div>
-		<select-reviewer-list-panel v-bind="components.selectReviewer" />
+		<select-reviewer-list-panel
+			v-bind="components.selectReviewer"
+			@set="updateReviewerList"
+		/>
 	</div>
 </template>
 
@@ -51,6 +54,12 @@ export default {
 	methods: {
 		toggleShowAllAuthors() {
 			this.showAllAuthors = !this.showAllAuthors;
+		},
+		updateReviewerList(panelId, reviewersData) {
+			this.components.selectReviewer = {
+				...this.components.selectReviewer,
+				...reviewersData,
+			};
 		},
 	},
 	computed: {
@@ -75,24 +84,24 @@ export default {
 <style lang="less">
 @import '../../styles/_import';
 .pkpAdvancedSearchReviewerContainer {
-  .action_container {
-	padding-left: 1rem;
-	padding-bottom: 0.9rem;
-  }
-  .list_outline {
-	border: 1px solid @grid-border-color;
-	border-radius: 2px;
-	margin-bottom: 1.3rem;
-  }
-  .list_box {
-	padding-left: 1.9rem;
-	overflow-y: scroll;
-	max-height: 17em;
-  }
-  .show_authors_action {
-	color: @primary;
-	text-decoration: none;
-	cursor: pointer;
-  }
+	.action_container {
+		padding-left: 1rem;
+		padding-bottom: 0.9rem;
+	}
+	.list_outline {
+		border: 1px solid @grid-border-color;
+		border-radius: 2px;
+		margin-bottom: 1.3rem;
+	}
+	.list_box {
+		padding-left: 1.9rem;
+		overflow-y: scroll;
+		max-height: 17em;
+	}
+	.show_authors_action {
+		color: @primary;
+		text-decoration: none;
+		cursor: pointer;
+	}
 }
 </style>
