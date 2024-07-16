ef08c042f7682368562f551422e5969166dd3394
Bozana Bokan <bozana.bokan@posteo.net>
2024-07-16 13:52:48 +0200

    pkp/pkp-lib#9468 disable DOI editing for chapters without landing page and DOI

--- src/components/ListPanel/doi/DoiListPanelOMP.vue
+++ src/components/ListPanel/doi/DoiListPanelOMP.vue
@@ -61,6 +61,7 @@ export default {
 								isCurrentVersion,
 								versionNumber,
 								updateWithNewDoiEndpoint,
+								disabled: !chapter.isPageEnabled && !doiObject,
 							})
 						);
 					});
