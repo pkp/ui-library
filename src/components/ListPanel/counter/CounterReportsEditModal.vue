ae86c641b1613c3ddda9fe9f4b879f6051ba92b3
Bozana Bokan <bozana.bokan@posteo.net>
2024-07-17 15:31:36 +0200

    fix counter list panel

---
+++ src/components/ListPanel/counter/CounterReportsEditModal.vue
@@ -0,0 +1,26 @@
+<template>
+	<SideModalBody>
+		<template #title>
+			{{ title }}
+		</template>
+		<SideModalLayoutBasic>
+			<CounterReportForm
+				v-bind="activeForm"
+				@set="(...args) => emit('updateForm', ...args)"
+				@success="(...args) => emit('formSuccess', ...args)"
+			/>
+		</SideModalLayoutBasic>
+	</SideModalBody>
+</template>
+
+<script setup>
+import SideModalBody from '@/components/Modal/SideModalBody.vue';
+import SideModalLayoutBasic from '@/components/Modal/SideModalLayoutBasic.vue';
+import CounterReportForm from '@/components/Form/counter/CounterReportForm.vue';
+
+defineProps({
+	title: {type: String, required: true},
+	activeForm: {type: Object, required: true},
+});
+const emit = defineEmits(['updateForm', 'formSuccess']);
+</script>
