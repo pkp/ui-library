b201a8091839f7a4cc31dd2da3473aef8f9735ec
Jonas Raoni Soares da Silva <github@raoni.org>
2024-07-17 11:41:56 +0300

    Updated AdvancedSearchReviewerContainer to extend the container

--- src/components/Container/AdvancedSearchReviewerContainer.vue
+++ src/components/Container/AdvancedSearchReviewerContainer.vue
@@ -32,17 +32,17 @@
 				</div>
 			</div>
 		</div>
-		<select-reviewer-list-panel
-			v-bind="components.selectReviewer"
-			@set="updateReviewerList"
-		/>
+		<select-reviewer-list-panel v-bind="components.selectReviewer" @set="set" />
 	</div>
 </template>
 
 <script>
+import Container from '@/components/Container/Container.vue';
 import SelectReviewerListPanel from '@/components/ListPanel/users/SelectReviewerListPanel.vue';
+
 export default {
 	name: 'AdvancedSearchReviewerContainer',
+	extends: Container,
 	components: {
 		SelectReviewerListPanel,
 	},
@@ -54,12 +54,6 @@ export default {
 	methods: {
 		toggleShowAllAuthors() {
 			this.showAllAuthors = !this.showAllAuthors;
-		},
-		updateReviewerList(panelId, reviewersData) {
-			this.components.selectReviewer = {
-				...this.components.selectReviewer,
-				...reviewersData,
-			};
 		},
 	},
 	computed: {
