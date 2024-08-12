b6745f5de4d0af5efc1e5224600136889f302880
Blesilda Ramirez <ramirezblesilda@gmail.com>
2024-08-12 12:59:32 +0800

    pkp/pkp-lib#9890 Add SideMenu component

---
+++ src/components/SideMenu/SideMenu.stories.js
@@ -0,0 +1,362 @@
+import SideMenu from './SideMenu.vue';
+
+export default {
+	title: 'Components/SideMenu',
+	component: SideMenu,
+	render: (args) => ({
+		components: {SideMenu},
+		setup() {
+			function startNewSubmission() {
+				console.log('startNewSubmission clicked');
+			}
+
+			function otherAction() {
+				console.log('otherAction clicked');
+			}
+
+			function handleActions(action) {
+				switch (action) {
+					case 'startNewSubmission':
+						startNewSubmission();
+						break;
+					case 'otherAction':
+						otherAction();
+						break;
+					default:
+						console.error(`No handler for action: ${action}`);
+				}
+			}
+			return {args, handleActions};
+		},
+		template: '<SideMenu v-bind="args" @action="handleActions" />',
+	}),
+};
+
+export const Default = {
+	args: {
+		items: [
+			{
+				label: 'Editorial Dashboard',
+				icon: 'Dashboard',
+				isCurrent: false,
+				items: [
+					{
+						label: 'Action required by me',
+						link: '#',
+						isCurrent: true,
+						badge: {
+							slot: 20,
+							isWarnable: true,
+						},
+					},
+					{
+						label: 'Assigned to me',
+						link: '#',
+						isCurrent: false,
+						badge: {
+							slot: 45,
+						},
+					},
+					{
+						label: 'Active submissions',
+						link: '#',
+						isCurrent: false,
+						badge: {
+							slot: 100,
+						},
+					},
+					{
+						label: 'All in submission stage',
+						link: '#',
+						isCurrent: false,
+						badge: {
+							slot: 35,
+						},
+					},
+					{
+						label: 'Needs reviewers',
+						link: '#',
+						isCurrent: false,
+						badge: {
+							slot: 5,
+						},
+					},
+					{
+						label: 'Awaiting reviews',
+						link: '#',
+						isCurrent: false,
+						badge: {
+							slot: 6,
+						},
+					},
+					{
+						label: 'Reviews submitted',
+						link: '#',
+						isCurrent: false,
+						badge: {
+							slot: 4,
+						},
+					},
+					{
+						label: 'Reviews overdue',
+						link: '#',
+						isCurrent: false,
+						badge: {
+							slot: 10,
+							isWarnable: true,
+						},
+					},
+					{
+						label: 'Author revisions submitted',
+						link: '#',
+						isCurrent: false,
+						badge: {
+							slot: 5,
+						},
+					},
+					{
+						label: 'All in review stage',
+						link: '#',
+						isCurrent: false,
+						badge: {
+							slot: 10,
+						},
+					},
+					{
+						label: 'All in copyediting stage',
+						link: '#',
+						isCurrent: false,
+						badge: {
+							slot: 10,
+						},
+					},
+					{
+						label: 'All in production stage',
+						link: '#',
+						isCurrent: false,
+						badge: {
+							slot: 12,
+						},
+					},
+					{
+						label: 'Scheduled for publishing',
+						link: '#',
+						isCurrent: false,
+						badge: {
+							slot: 16,
+						},
+					},
+					{
+						label: 'Published',
+						link: '#',
+						isCurrent: false,
+						badge: {
+							slot: 75,
+						},
+					},
+					{
+						label: 'Declined',
+						link: '#',
+						isCurrent: false,
+						badge: {
+							slot: 10,
+						},
+					},
+					{
+						label: 'Start a New Submission',
+						link: '#',
+						isCurrent: false,
+						action: 'startNewSubmission',
+					},
+				],
+			},
+			{
+				label: 'My Assignments as Reviewer',
+				icon: 'ReviewAssignments',
+				isCurrent: false,
+				link: '#',
+			},
+			{
+				label: 'My Submissions as Author',
+				icon: 'MySubmissions',
+				isCurrent: false,
+				link: '#',
+			},
+			{
+				label: 'Issues',
+				icon: 'Issues',
+				isCurrent: false,
+				link: '#',
+			},
+			{
+				label: 'Announcements',
+				icon: 'Announcements',
+				isCurrent: false,
+				link: '#',
+			},
+			{
+				label: 'DOIs',
+				icon: 'NavDoi',
+				isCurrent: false,
+				link: '#',
+			},
+			{
+				label: 'Institutes',
+				icon: 'Institutes',
+				isCurrent: false,
+				link: '#',
+			},
+		],
+	},
+};
+
+export const WithColorStripe = {
+	args: {
+		items: [
+			{
+				label: 'Submission Stages',
+				isCurrent: true,
+				items: [
+					{
+						label: 'Desk Review',
+						link: '#',
+						isCurrent: false,
+						colorStripe: 'border-stage-desk-review',
+					},
+					{
+						label: 'Review',
+						link: '#',
+						isCurrent: false,
+						colorStripe: 'border-stage-in-review',
+					},
+					{
+						label: 'Copyediting',
+						link: '#',
+						isCurrent: false,
+						colorStripe: 'border-stage-copyediting',
+					},
+					{
+						label: 'Production',
+						link: '#',
+						isCurrent: false,
+						colorStripe: 'border-stage-production',
+					},
+					{
+						label: 'Scheduled',
+						link: '#',
+						isCurrent: false,
+						colorStripe: 'border-stage-scheduled-for-publishing',
+					},
+					{
+						label: 'Incomoplete Submission',
+						link: '#',
+						isCurrent: false,
+						colorStripe: 'border-stage-incomplete-submission',
+					},
+					{
+						label: 'Published',
+						link: '#',
+						isCurrent: false,
+						colorStripe: 'border-stage-published',
+					},
+					{
+						label: 'Declined',
+						link: '#',
+						isCurrent: false,
+						colorStripe: 'border-stage-declined',
+					},
+				],
+			},
+		],
+	},
+};
+
+export const ExpandedMenu = {
+	args: {
+		items: [
+			{
+				label: 'Workflow',
+				icon: 'thumbs-up',
+				isCurrent: false,
+				items: [
+					{
+						label: 'Submission',
+						link: '#',
+						isCurrent: false,
+						colorStripe: 'border-stage-desk-review',
+					},
+					{
+						label: 'Review',
+						link: '#',
+						isCurrent: false,
+						items: [
+							{
+								label: 'Review Round 1',
+								link: '#',
+								isCurrent: true,
+							},
+							{
+								label: 'New Review Round',
+								link: '#',
+								isCurrent: false,
+							},
+						],
+					},
+					{
+						label: 'Copyediting',
+						link: '#',
+						isCurrent: false,
+					},
+					{
+						label: 'Production',
+						link: '#',
+						isCurrent: false,
+					},
+				],
+			},
+			{
+				label: 'Publication',
+				icon: 'MySubmissions',
+				isCurrent: false,
+				isOpen: true,
+				items: [
+					{
+						label: 'Title & Abstract',
+						link: '#',
+						isCurrent: false,
+					},
+					{
+						label: 'Contributors',
+						link: '#',
+						isCurrent: false,
+					},
+					{
+						label: 'Metadata',
+						link: '#',
+						isCurrent: false,
+					},
+					{
+						label: 'References',
+						link: '#',
+						isCurrent: false,
+					},
+					{
+						label: 'Galleys',
+						link: '#',
+						isCurrent: false,
+					},
+					{
+						label: 'Permissions & Disclosure',
+						link: '#',
+						isCurrent: false,
+					},
+					{
+						label: 'Issue',
+						link: '#',
+						isCurrent: false,
+					},
+				],
+			},
+		],
+	},
+};
---
+++ src/components/SideMenu/SideMenu.vue
@@ -0,0 +1,202 @@
+<template>
+	<PanelMenu
+		v-model:expandedKeys="expandedKeys"
+		:model="items"
+		:pt="navigationStyling"
+		class="w-max min-w-60 overflow-y-auto border-e border-s border-light bg-secondary"
+	>
+		<template #item="{item, active, hasSubmenu}">
+			<a
+				:class="getButtonStyles(item)"
+				:href="item.link"
+				tabindex="-1"
+				@click="handleClick(item)"
+			>
+				<Badge
+					v-if="item.badge?.slot"
+					:color-variant="
+						isActive(item) && !item.badge.isWarnable ? 'default-on-dark' : null
+					"
+					v-bind="item.badge"
+					class="me-1"
+				>
+					{{ item.badge.slot }}
+				</Badge>
+				<Icon v-if="item.icon" class="h-5 w-5" :icon="item.icon" />
+				{{ item.label }}
+				<Icon
+					v-if="hasSubmenu"
+					class="h-4 w-4 ltr:ml-auto rtl:mr-auto"
+					:icon="active ? 'caret-up' : 'Dropdown'"
+				/>
+			</a>
+		</template>
+	</PanelMenu>
+</template>
+
+<script setup>
+import PanelMenu from 'primevue/panelmenu';
+import Icon from '../Icon/Icon.vue';
+import Badge from '../Badge/Badge.vue';
+import {ref, reactive} from 'vue';
+
+const props = defineProps({
+	/**
+	 * An array of item objects for the SideMenu.
+	 * Each object should contain:
+	 * - `label` (string): The label of the menu item.
+	 * - `link` (string, optional): The URL to navigate to when the item is clicked.
+	 * - `action` (function, optional): A function to be executed when the item is clicked.
+	 * - `isCurrent` (boolean, optional): Marks the item as the current selection.
+	 * - `isOpen` (boolean, optional): Identifies if the menu should be expanded.
+	 * - `badge` (object, optional): Contains `slot` (string) and other props for `<Badge>` customization.
+	 * - `colorStripe` (string, optional): A border class to add a color stripe to the item.
+	 * - `items` (array, optional): An array of child items for nested menus.
+	 */
+	items: {
+		type: Array,
+		required: true,
+		validator: (items) => {
+			return items.every((item) => {
+				const hasLabel =
+					typeof item.label === 'string' && item.label.trim() !== '';
+				const validItem = item.link || item.action || item.items;
+				return hasLabel && validItem;
+			});
+		},
+	},
+});
+
+const emit = defineEmits([
+	/** When a panel menu item's "action" is clicked */
+	'action',
+]);
+
+const activeItemKey = ref('');
+
+// Maps the key and level attributes which are necessary to render the nested menu
+function mapItems(_items, level = 1, _key) {
+	const result = [];
+
+	_items.forEach((_item, index) => {
+		const key = _key ? `${_key}_${index}` : `item_${index}`;
+		const item = {
+			..._item,
+			key,
+			level,
+		};
+
+		if (_item.items) {
+			item.items = mapItems(_item.items, level + 1, key);
+		}
+
+		if (item.isCurrent) {
+			activeItemKey.value = key;
+		}
+
+		result.push(item);
+	});
+
+	return result;
+}
+
+const items = reactive(mapItems(props.items));
+
+// Defines the list of expanded keys, so it renders correctly when the component loads
+function getExpandedKeys(items) {
+	const _expandedKeys = {};
+
+	function markExpanded(items) {
+		let found = false;
+		for (const item of items) {
+			if (item.isCurrent || item.isOpen) {
+				_expandedKeys[item.key] = true;
+				found = true;
+			}
+
+			const isMarkExpanded = item.items && markExpanded(item.items);
+
+			if (item.items && isMarkExpanded) {
+				_expandedKeys[item.key] = true;
+				found = true;
+			}
+		}
+		return found;
+	}
+
+	markExpanded(items);
+	return _expandedKeys;
+}
+
+const expandedKeys = ref(getExpandedKeys(items));
+
+const navigationStyling = {
+	headerContent: () => {
+		return {
+			class: [
+				// Transition
+				'transition duration-200 ease-in-out',
+				'transition-shadow duration-200',
+			],
+		};
+	},
+	itemContent: {
+		class: ['transition-shadow duration-200'],
+	},
+	transition: {
+		enterFromClass: 'max-h-0',
+		enterActiveClass:
+			'overflow-hidden transition-[max-height] duration-1000 ease-[cubic-bezier(0.42,0,0.58,1)]',
+		enterToClass: 'max-h-[1000px]',
+		leaveFromClass: 'max-h-[1000px]',
+		leaveActiveClass:
+			'overflow-hidden transition-[max-height] duration-[450ms] ease-[cubic-bezier(0,1,0,1)]',
+		leaveToClass: 'max-h-0',
+	},
+};
+
+function handleClick(item) {
+	// set the current active item key
+	activeItemKey.value = item.key;
+
+	if (item.action) {
+		emit('action', item.action);
+	}
+}
+
+function isActive(item) {
+	return item?.key === activeItemKey.value;
+}
+
+function getButtonStyles(item) {
+	const isActiveItem = isActive(item);
+
+	const style = {
+		// Base
+		'inline-flex relative items-center gap-x-1 text-lg-semibold py-2 px-3 w-full border-b border-b-light': true,
+		// Default button styling
+		'text-primary border-light  hover:text-hover disabled:text-disabled bg-secondary':
+			!isActiveItem,
+		// Active
+		'text-on-dark bg-selection-dark': isActiveItem,
+		'border-transparent': isActiveItem && !item.colorStripe,
+		// Indentions for child menus
+		'!px-7': item.level === 2 && item.colorStripe,
+		'!px-9': item.level === 2 && !item.colorStripe,
+		'!px-10': item.level === 3 && item.colorStripe,
+		'!px-12': item.level === 3 && !item.colorStripe,
+		'!px-14': item.level === 4 && item.colorStripe,
+		'!px-16': item.level === 4 && !item.colorStripe,
+		// Additional border styling
+		'border-t border-t-light': item.key === 'item_0',
+		'border-s-8': item.colorStripe,
+	};
+
+	// set the additional class if the button should include a color stripe
+	if (item.colorStripe) {
+		style[item.colorStripe] = true;
+	}
+
+	return style;
+}
+</script>
