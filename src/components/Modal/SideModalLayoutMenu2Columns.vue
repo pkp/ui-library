5c4845d3859c3a6ef232c0f194d5fe8b01118a17
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2024-08-15 11:00:27 +0200

    pkp/pkp-lib#7495 Summary with navigation in progress

---
+++ src/components/Modal/SideModalLayoutMenu2Columns.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="p-4">
+		<div class="flex w-full bg-secondary">
+			<div class=""><slot name="menu"></slot></div>
+			<div class="w-full border-y border-e border-light">
+				<div class="border-b border-light p-4">
+					<slot name="heading"></slot>
+				</div>
+				<div class="flex">
+					<div class="flex-grow"><slot name="primary"></slot></div>
+					<div class="w-96 border-s border-light">
+						<div class="border-b border-light"><slot name="actions" /></div>
+						<div><slot name="secondary"></slot></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
--- src/pages/dashboard/SubmissionSummaryModal/SubmissionSummaryModal.vue
+++ src/pages/dashboard/SubmissionSummaryModal/SubmissionSummaryModal.vue
@@ -28,38 +28,46 @@
 				{{ t('dashboard.summary.viewSubmissionInDetail') }}
 			</PkpButton>
 		</template>
-		<SideModalLayout2Columns>
-			<template #left>
+		<SideModalLayoutMenu2Columns>
+			<template #menu>
+				<SideMenu
+					v-if="summaryStore.menuItems.length"
+					:items="summaryStore.menuItems"
+					@action="summaryStore.selectMenuItem"
+				></SideMenu>
+			</template>
+			<template #heading>Hello</template>
+			<template #primary>
 				<div class="flex flex-col gap-y-5 bg-secondary p-5">
 					<component
 						:is="Components[item.component] || item.component"
 						v-bind="item.props"
-						v-for="(item, index) in primaryItems"
-						:key="index"
+						v-for="(item, index) in summaryStore.primaryItems"
+						:key="`${index} - ${item.props?.configName}`"
 					/>
 				</div>
 			</template>
-			<template #right1>
+			<template #actions>
 				<div class="flex flex-col items-start space-y-4 p-4">
 					<component
 						:is="Components[item.component] || item.component"
-						v-for="(item, index) in actionItems"
+						v-for="(item, index) in summaryStore.actionItems"
 						v-bind="item.props"
 						:key="index"
 					></component>
 				</div>
 			</template>
-			<template #right2>
+			<template #secondary>
 				<div class="flex flex-col space-y-4 p-4">
 					<component
 						:is="Components[item.component] || item.component"
-						v-for="(item, index) in metaItems"
+						v-for="(item, index) in summaryStore.secondaryItems"
 						v-bind="item.props"
 						:key="index"
 					></component>
 				</div>
 			</template>
-		</SideModalLayout2Columns>
+		</SideModalLayoutMenu2Columns>
 	</SideModalBody>
 </template>
 
@@ -67,6 +75,7 @@
 import {computed, provide} from 'vue';
 import {storeToRefs} from 'pinia';
 import PkpButton from '@/components/Button/Button.vue';
+import SideMenu from '@/components/SideMenu/SideMenu.vue';
 import SideModalBody from '@/components/Modal/SideModalBody.vue';
 import StageBubble from '@/components/StageBubble/StageBubble.vue';
 import FileManager from '@/managers/FileManager/FileManager.vue';
@@ -84,11 +93,10 @@ import BasicMetadata from './metaItems/BasicMetadata.vue';
 
 import IssueAssigned from './metaItems/IssueAssigned.vue';
 import {useSubmissionSummaryStore} from './submissionSummaryStore';
-import SideModalLayout2Columns from '@/components/Modal/SideModalLayout2Columns.vue';
+import SideModalLayoutMenu2Columns from '@/components/Modal/SideModalLayoutMenu2Columns.vue';
 
 import {useLocalize} from '@/composables/useLocalize';
 import {useSubmission} from '@/composables/useSubmission';
-
 const {t, localizeSubmission} = useLocalize();
 
 const Components = {
@@ -121,54 +129,6 @@ const extendedStage = computed(
 const stageLabel = computed(
 	() => submission.value && getExtendedStageLabel(submission.value),
 );
-
-const primaryItems = computed(() => {
-	if (!summaryStore.submission || !summaryStore.currentPublication) {
-		return [];
-	}
-
-	return summaryStore.filterItemsBasedOnContext(
-		summaryStore.getPrimaryItems(
-			summaryStore.submission,
-			summaryStore.currentPublication,
-		),
-		summaryStore.dashboardPage,
-		summaryStore.submission,
-		summaryStore.selectedReviewAssignment,
-	);
-});
-
-const actionItems = computed(() => {
-	if (!summaryStore.submission || !summaryStore.currentPublication) {
-		return [];
-	}
-
-	return summaryStore.filterItemsBasedOnContext(
-		summaryStore.getActionItems(
-			summaryStore.submission,
-			summaryStore.currentPublication,
-		),
-		summaryStore.dashboardPage,
-		summaryStore.submission,
-		summaryStore.selectedReviewAssignment,
-	);
-});
-
-const metaItems = computed(() => {
-	if (!summaryStore.submission || !summaryStore.currentPublication) {
-		return [];
-	}
-
-	return summaryStore.filterItemsBasedOnContext(
-		summaryStore.getMetaItems(
-			summaryStore.submission,
-			summaryStore.currentPublication,
-		),
-		summaryStore.dashboardPage,
-		summaryStore.submission,
-		summaryStore.selectedReviewAssignment,
-	);
-});
 
 const {submission, currentPublication} = storeToRefs(summaryStore);
 
