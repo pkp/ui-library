8db17fbc21dc34a01a9e5815f8b9a2a8c3771aa7
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2024-08-28 22:41:44 +0200

    Support for message in empty table body

--- src/components/TableNext/TableBody.vue
+++ src/components/TableNext/TableBody.vue
@@ -1,3 +1,37 @@
 <template>
-	<tbody class=""><slot></slot></tbody>
+	<tbody class="">
+		<slot></slot>
+		<tr v-if="noContent" class="">
+			<td
+				:colspan="tableContext.columnsCount.value"
+				class="border-x border-b border-light p-4 text-center text-base-normal"
+			>
+				{{ emptyText }}
+			</td>
+		</tr>
+	</tbody>
 </template>
+
+<script setup>
+import {useSlots, computed, inject} from 'vue';
+import {useLocalize} from '@/composables/useLocalize';
+
+const props = defineProps({
+	emptyText: {type: String, default: ''},
+});
+const slots = useSlots();
+
+const {t} = useLocalize();
+
+const tableContext = inject('tableContext');
+
+const noContent = computed(() => {
+	const defaultSlot = slots.default();
+
+	return !defaultSlot?.[0]?.children?.length;
+});
+
+const emptyText = computed(() => {
+	return props.emptyText || t('grid.noItems');
+});
+</script>
