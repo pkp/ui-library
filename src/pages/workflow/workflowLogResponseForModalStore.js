8e4ed59c27782adf0f781247c33988c621864792
RobA <rob.arnold@ubiquitypress.com>
2024-09-10 14:15:56 -0300

    using useForm composable which contains updateForm instead of writing one

--- src/pages/workflow/workflowLogResponseForModalStore.js
+++ src/pages/workflow/workflowLogResponseForModalStore.js
@@ -1,18 +1,15 @@
 import {inject} from 'vue';
 import {defineComponentStore} from '@/utils/defineComponentStore';
+import {useForm} from '@/composables/useForm';
 
 export const useWorkflowLogResponseForModalStore = defineComponentStore(
 	'workflowLogResponseForModal',
 	(props) => {
 		const closeModal = inject('closeModal');
-		const form = props.logResponseForm;
+		const {set: updateForm, form} = useForm(props.logResponseForm);
 
 		function formSuccess() {
 			closeModal();
-		}
-
-		function updateForm(formId, data) {
-			Object.keys(data).forEach((key) => (form[key] = data[key]));
 		}
 
 		return {form, formSuccess, updateForm};
