fb85937226a3c2dd9ca55cb2d1556738cde4b5b9
GaziYucel <GaziYucel@users.noreply.github.com>
2024-09-27 15:51:18 +0200

    Author Affiliations (work in progress)

---
+++ src/components/Form/mocks/field-author-affiliations.js
@@ -0,0 +1,24 @@
+export default {
+	name: 'author-affiliations',
+	component: 'author-affiliations',
+	items: [
+		{
+			id: 1,
+			authorId: 1,
+			ror: 'https://ror.org/0213rcc28',
+			name: {
+				en: 'Simon Fraser University',
+				fr_CA: 'Simon Fraser University'
+			}
+		},
+		{
+			id: 2,
+			authorId: 1,
+			ror: 'https://ror.org/02e2c7k09',
+			name: {
+				en: 'Delft University of Technology',
+				fr_CA: 'Delft University of Technology'
+			}
+		}
+	],
+};
