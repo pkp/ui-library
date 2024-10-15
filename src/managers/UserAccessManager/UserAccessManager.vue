11ff6446e757208ec618de51903874deac31d9a5
ipula <ipularanasinghe007@gmail.com>
2024-10-15 15:14:34 +0200

    add user search function

--- src/managers/UserAccessManager/UserAccessManager.vue
+++ src/managers/UserAccessManager/UserAccessManager.vue
@@ -3,103 +3,61 @@
 		<template #label>
 			<h3 class="text-3xl-bold">
 				{{ t('grid.user.currentUsers') }}({{
-					store.invitationsPagination.itemCount
+					store.userAccessPagination.itemCount
 				}})
 			</h3>
 		</template>
 		<template #top-controls>
-			<div class="flex space-x-0">
-				<Icon
-					icon="search"
-					class="border-x-2 border-y-2 p-3"
-					:inline="true"
-				></Icon>
-				<FieldText
-					:value="fields.search"
-					name="search"
-					size="large"
-					@change="store.search"
-				/>
-			</div>
+			<Search
+				:search-phrase="searchPhrase"
+				:search-label="t('user.search')"
+				@search-phrase-changed="store.setSearchPhrase"
+			/>
 		</template>
 		<TableHeader>
 			<TableColumn>{{ t('invitation.tableHeader.name') }}</TableColumn>
 			<TableColumn>{{ t('about.contact.email') }}</TableColumn>
 			<TableColumn>{{ t('user.roles') }}</TableColumn>
-			<TableColumn>{{ t('user.startDate') }}</TableColumn>
+			<TableColumn>{{ t('userInvitation.roleTable.startDate') }}</TableColumn>
 			<TableColumn>{{ t('user.affiliation') }}</TableColumn>
 			<TableColumn>
 				<span class="sr-only">{{ t('common.moreActions') }}</span>
 			</TableColumn>
 		</TableHeader>
 		<TableBody>
-			<TableRow v-for="(invitation, index) in store.invitations" :key="index">
+			<TableRow v-for="(user, index) in store.userList" :key="index">
 				<TableCell>
-					{{ invitation.fullName }}
-					<Icon v-if="invitation.orcidIsVerified" icon="orcid" :inline="true" />
+					{{ user.fullName }}
+					<Icon v-if="user.orcidIsVerified" icon="orcid" :inline="true" />
 				</TableCell>
 				<TableCell>
-					{{ invitation.email }}
+					{{ user.email }}
 				</TableCell>
 				<TableCell>
-					<template v-for="(userGroups, i) in invitation.groups" :key="i">
+					<template v-for="(userGroups, i) in user.groups" :key="i">
 						<div class="flex flex-col">
 							{{ localize(userGroups.name) }}
 						</div>
 					</template>
 				</TableCell>
 				<TableCell>
-					<template v-for="(userGroups, i) in invitation.groups" :key="i">
+					<template v-for="(userGroups, i) in user.groups" :key="i">
 						<div class="flex flex-col">
 							{{ formatShortDate(userGroups?.startDate) }}
 						</div>
 					</template>
 				</TableCell>
 				<TableCell>
-					{{ localize(invitation.affiliation) }}
+					{{ localize(user.affiliation) }}
 				</TableCell>
 				<TableCell>
 					<DropdownActions
-						:actions="[
-							{
-								label: t('common.edit'),
-								name: 'editUser',
-								icon: 'Edit',
-							},
-							{
-								label: t('email.email'),
-								icon: 'Email',
-								name: 'sendEmail',
-							},
-							{
-								label: t('grid.action.logInAs'),
-								icon: 'LoginAs',
-								name: 'sendEmail',
-							},
-							{
-								label: t('common.cancel'),
-								icon: 'Cancel',
-								name: 'RemoveUser',
-								isWarnable: true,
-							},
-							{
-								label: t('grid.action.disable'),
-								icon: 'DisableUser',
-								name: 'DisableUser',
-								isWarnable: true,
-							},
-							{
-								label: t('grid.action.mergeUser'),
-								icon: 'MergeUser',
-								name: 'mergeUser',
-							},
-						]"
+						:actions="actions"
 						:label="t('invitation.management.options')"
 						:display-as-ellipsis="true"
 						direction="left"
 						@action="
-							(actionName) =>
-								store.handleInvitationAction(actionName, invitation)
+							(actionName) => store.handleUserAccessAction(actionName, user)
 						"
 					/>
 				</TableCell>
@@ -108,9 +66,9 @@
 	</PkpTable>
 	<div class="flex justify-end">
 		<Pagination
-			:current-page="store.invitationsPagination.currentPage"
-			:last-page="store.invitationsPagination.pageCount"
-			:is-loading="store.isInvitationLoading"
+			:current-page="store.userAccessPagination.currentPage"
+			:last-page="store.userAccessPagination.pageCount"
+			:is-loading="store.isUserAccessLoading"
 			:show-adjacent-pages="3"
 			@set-page="store.setCurrentPage"
 		/>
@@ -130,10 +88,45 @@ import Pagination from '@/components/Pagination/Pagination.vue';
 import {useTranslation} from '@/composables/useTranslation';
 import {useDate} from '@/composables/useDate';
 import DropdownActions from '@/components/DropdownActions/DropdownActions.vue';
-import FieldText from '@/components/Form/fields/FieldText.vue';
+import Search from '@/components/Search/Search.vue';
 import {ref} from 'vue';
+
 const store = useUserAccessManagerStore();
 const {t} = useTranslation();
 const {formatShortDate} = useDate();
-const fields = ref({search: ''});
+const searchPhrase = ref('');
+const actions = [
+	{
+		label: t('common.edit'),
+		name: 'editUser',
+		icon: 'Edit',
+	},
+	{
+		label: t('email.email'),
+		icon: 'Email',
+		name: 'sendEmail',
+	},
+	{
+		label: t('grid.action.logInAs'),
+		icon: 'LoginAs',
+		name: 'sendEmail',
+	},
+	{
+		label: t('common.cancel'),
+		icon: 'Cancel',
+		name: 'RemoveUser',
+		isWarnable: true,
+	},
+	{
+		label: t('grid.action.disable'),
+		icon: 'DisableUser',
+		name: 'DisableUser',
+		isWarnable: true,
+	},
+	{
+		label: t('grid.action.mergeUser'),
+		icon: 'MergeUser',
+		name: 'mergeUser',
+	},
+];
 </script>
