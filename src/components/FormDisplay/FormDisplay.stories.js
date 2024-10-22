105897548065472f9aec5c9d6dd1854e7865f688
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2024-10-22 16:53:04 +0200

    Basic support for multilingual fields in FormDisplay component

--- src/components/FormDisplay/FormDisplay.stories.js
+++ src/components/FormDisplay/FormDisplay.stories.js
@@ -12,7 +12,7 @@ export default {
 				givenName: 'Jarda',
 				familyName: 'Kotesovec',
 				affiliation: 'PKP',
-				country: 'CA',
+				country: 'AI',
 			});
 			connectWithPayload(payload);
 			return {args};
@@ -22,6 +22,21 @@ export default {
 };
 
 export const Base = {
+	render: (args) => ({
+		components: {FormDisplay},
+		setup() {
+			const {connectWithPayload} = useForm(args.form);
+			const payload = ref({
+				givenName: 'Jarda',
+				familyName: 'Kotesovec',
+				affiliation: 'PKP',
+				country: 'AI',
+			});
+			connectWithPayload(payload);
+			return {args};
+		},
+		template: '<FormDisplay v-bind="args.form"></FormDisplay>',
+	}),
 	args: {
 		form: {
 			id: 'userDetails',
@@ -130,6 +145,167 @@ export const Base = {
 							value: 'AG',
 							label: 'Antigua and Barbuda',
 						},
+					],
+					size: 'large',
+					prefix: '',
+				},
+			],
+			groups: [
+				{
+					id: 'default',
+					pageId: 'default',
+				},
+			],
+			hiddenFields: {},
+			pages: [
+				{
+					id: 'default',
+					submitButton: {
+						label: 'Save',
+					},
+				},
+			],
+			primaryLocale: 'en',
+			visibleLocales: ['en'],
+			supportedFormLocales: [
+				{
+					key: 'en',
+					label: 'English',
+				},
+				{
+					key: 'fr_CA',
+					label: 'French',
+				},
+			],
+			errors: {},
+		},
+	},
+};
+
+export const Multilingual = {
+	render: (args) => ({
+		components: {FormDisplay},
+		setup() {
+			const {connectWithPayload} = useForm(args.form);
+			const payload = ref({
+				givenName: {en: 'Jarda', fr_CA: 'Jardous'},
+				familyName: {en: 'Kotesovec', fr_CA: ''},
+				affiliation: {en: 'PKP', fr_CA: 'PKP fr'},
+				country: 'AI',
+			});
+			connectWithPayload(payload);
+			return {args};
+		},
+		template: '<FormDisplay v-bind="args.form"></FormDisplay>',
+	}),
+	args: {
+		form: {
+			id: 'userDetails',
+			method: 'POST',
+			action: 'http://localhost/ojs/index.php/publicknowledge/api/v1/users',
+			fields: [
+				{
+					name: 'givenName',
+					component: 'field-text',
+					label: 'Given Name',
+					groupId: 'default',
+					isRequired: true,
+					isMultilingual: true,
+					description:
+						'Also known as a forename or the first name, it is tha part of a personal name that identifies a preson',
+					value: null,
+					inputType: 'text',
+					optIntoEdit: false,
+					optIntoEditLabel: '',
+					size: 'large',
+					prefix: '',
+				},
+				{
+					name: 'familyName',
+					component: 'field-text',
+					label: 'Family Name',
+					groupId: 'default',
+					isRequired: true,
+					isMultilingual: true,
+					description:
+						"A surname, family name, or last name is the mostly  hereditary portion of one's personal name that indicates one's family",
+					value: null,
+					inputType: 'text',
+					optIntoEdit: false,
+					optIntoEditLabel: '',
+					size: 'large',
+					prefix: '',
+				},
+				{
+					name: 'affiliation',
+					component: 'field-text',
+					label: 'Affiliation',
+					groupId: 'default',
+					isRequired: true,
+					isMultilingual: true,
+					description: 'This is the institute you are affiliated with',
+					value: null,
+					inputType: 'text',
+					optIntoEdit: false,
+					optIntoEditLabel: '',
+					size: 'large',
+					prefix: '',
+				},
+				{
+					name: 'country',
+					component: 'field-select',
+					label: 'County of affiliation',
+					groupId: 'default',
+					isRequired: true,
+					isMultilingual: false,
+					description:
+						'This is a country in which the institute you are affiliated with is situated',
+					value: null,
+					inputType: 'text',
+					optIntoEdit: false,
+					optIntoEditLabel: '',
+					options: [
+						{
+							value: '',
+							label: 'Select a country',
+							disabled: true,
+						},
+						{
+							value: 'AF',
+							label: 'Afghanistan',
+						},
+						{
+							value: 'AL',
+							label: 'Albania',
+						},
+						{
+							value: 'DZ',
+							label: 'Algeria',
+						},
+						{
+							value: 'AS',
+							label: 'American Samoa',
+						},
+						{
+							value: 'AD',
+							label: 'Andorra',
+						},
+						{
+							value: 'AO',
+							label: 'Angola',
+						},
+						{
+							value: 'AI',
+							label: 'Anguilla',
+						},
+						{
+							value: 'AQ',
+							label: 'Antarctica',
+						},
+						{
+							value: 'AG',
+							label: 'Antigua and Barbuda',
+						},
 						{
 							value: 'AR',
 							label: 'Argentina',
@@ -141,122 +317,6 @@ export const Base = {
 						{
 							value: 'AW',
 							label: 'Aruba',
-						},
-						{
-							value: 'AU',
-							label: 'Australia',
-						},
-						{
-							value: 'AT',
-							label: 'Austria',
-						},
-						{
-							value: 'AZ',
-							label: 'Azerbaijan',
-						},
-						{
-							value: 'BS',
-							label: 'Bahamas',
-						},
-						{
-							value: 'BH',
-							label: 'Bahrain',
-						},
-						{
-							value: 'BD',
-							label: 'Bangladesh',
-						},
-						{
-							value: 'BB',
-							label: 'Barbados',
-						},
-						{
-							value: 'BY',
-							label: 'Belarus',
-						},
-						{
-							value: 'BE',
-							label: 'Belgium',
-						},
-						{
-							value: 'BZ',
-							label: 'Belize',
-						},
-						{
-							value: 'BJ',
-							label: 'Benin',
-						},
-						{
-							value: 'BM',
-							label: 'Bermuda',
-						},
-						{
-							value: 'BT',
-							label: 'Bhutan',
-						},
-						{
-							value: 'BO',
-							label: 'Bolivia, Plurinational State of',
-						},
-						{
-							value: 'BQ',
-							label: 'Bonaire, Sint Eustatius and Saba',
-						},
-						{
-							value: 'BA',
-							label: 'Bosnia and Herzegovina',
-						},
-						{
-							value: 'BW',
-							label: 'Botswana',
-						},
-						{
-							value: 'BV',
-							label: 'Bouvet Island',
-						},
-						{
-							value: 'BR',
-							label: 'Brazil',
-						},
-						{
-							value: 'IO',
-							label: 'British Indian Ocean Territory',
-						},
-						{
-							value: 'BN',
-							label: 'Brunei Darussalam',
-						},
-						{
-							value: 'BG',
-							label: 'Bulgaria',
-						},
-						{
-							value: 'BF',
-							label: 'Burkina Faso',
-						},
-						{
-							value: 'BI',
-							label: 'Burundi',
-						},
-						{
-							value: 'CV',
-							label: 'Cabo Verde',
-						},
-						{
-							value: 'KH',
-							label: 'Cambodia',
-						},
-						{
-							value: 'CM',
-							label: 'Cameroon',
-						},
-						{
-							value: 'CA',
-							label: 'Canada',
-						},
-						{
-							value: '...',
-							label: '...',
 						},
 					],
 					size: 'large',
