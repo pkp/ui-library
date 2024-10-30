2a9db03e6c6b7b038b3c38f0364b902fbf3e0ffc
Touhidur Rahman <abir.ruet@gmail.com>
2024-10-30 18:24:32 +0600

    pkp/pkp-lib#4787 reviewer adding from suggestion working

--- src/pages/workflow/composables/useWorkflowConfig/workflowConfigEditorialOJS.js
+++ src/pages/workflow/composables/useWorkflowConfig/workflowConfigEditorialOJS.js
@@ -280,7 +280,12 @@ export const WorkflowConfig = {
 
 			return items;
 		},
-		getSecondaryItems: ({submission, selectedReviewRound, selectedStageId}) => {
+		getSecondaryItems: ({
+			submission,
+			selectedReviewRound,
+			selectedStageId,
+			pageInitConfig,
+		}) => {
 			const items = [];
 			if (!selectedReviewRound) {
 				return [];
@@ -299,6 +304,7 @@ export const WorkflowConfig = {
 					},
 				});
 			}
+
 			items.push({
 				component: 'ParticipantManager',
 				props: {
@@ -306,6 +312,17 @@ export const WorkflowConfig = {
 					submissionStageId: selectedStageId,
 				},
 			});
+
+			if (pageInitConfig.publicationSettings.isReviewerSuggestionEnabled) {
+				items.push({
+					component: 'ReviewerSuggestionManager',
+					props: {
+						submission,
+						submissionStageId: selectedStageId,
+						reviewRoundId: selectedReviewRound.id,
+					},
+				});
+			}
 
 			return items;
 		},
