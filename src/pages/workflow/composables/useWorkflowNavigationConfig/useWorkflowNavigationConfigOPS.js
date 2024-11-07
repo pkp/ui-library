ffc0182e109ab1e0d4f5d7ff79d19fb68fb5290e
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2024-11-07 15:55:09 +0100

    pkp/pkp-lib#7495 OPS navigaton config clean up

--- src/pages/workflow/composables/useWorkflowNavigationConfig/useWorkflowNavigationConfigOPS.js
+++ src/pages/workflow/composables/useWorkflowNavigationConfig/useWorkflowNavigationConfigOPS.js
@@ -1,43 +1,12 @@
 import {useSubmission} from '@/composables/useSubmission';
 import {useLocalize} from '@/composables/useLocalize';
 import {DashboardPageTypes} from '@/pages/dashboard/dashboardPageStore';
+import {
+	getPublicationItem,
+	getWorkflowItem,
+} from './useWorkflowNavigationConfigOJS';
 
 const {t} = useLocalize();
-
-const StageColors = {
-	[pkp.const.WORKFLOW_STAGE_ID_SUBMISSION]: 'border-stage-desk-review',
-	[pkp.const.WORKFLOW_STAGE_ID_EXTERNAL_REVIEW]: 'border-stage-in-review',
-	[pkp.const.WORKFLOW_STAGE_ID_EDITING]: 'border-stage-copyediting',
-	[pkp.const.WORKFLOW_STAGE_ID_PRODUCTION]: 'border-stage-production',
-};
-
-export function getPublicationItem({label, name}) {
-	return {
-		key: `publication_${name}`,
-		label: label,
-		action: 'selectMenu',
-		actionArgs: {
-			primaryMenuItem: 'publication',
-			secondaryMenuItem: name,
-			title: getPublicationTitle(label),
-		},
-	};
-}
-
-export function getWorkflowItem({stageId, label, isActive, isDisabled, items}) {
-	return {
-		key: `workflow_${stageId}`,
-		label: label,
-		colorStripe: isActive ? StageColors[stageId] : null,
-		action: isDisabled ? undefined : 'selectMenu',
-		actionArgs: {
-			primaryMenuItem: 'workflow',
-			stageId: stageId,
-			title: getWorkflowTitle(label),
-		},
-		items,
-	};
-}
 
 export function getWorkflowTitle(stageLabel) {
 	return `${t('semicolon', {label: t('manager.workflow')})} ${stageLabel} `;
@@ -47,48 +16,6 @@ export function getPublicationTitle(publicationMenuTitle) {
 	return `${t('semicolon', {
 		label: t('submission.publication'),
 	})} ${publicationMenuTitle}`;
-}
-
-export function getReviewItems({submission, stageId, title}) {
-	const {getActiveStage, getCurrentReviewRound} = useSubmission();
-
-	const activeStage = getActiveStage(submission);
-
-	const activeReviewRound = getCurrentReviewRound(submission, stageId);
-
-	const reviewMenuItems = [];
-
-	const {getReviewRoundsForStage} = useSubmission();
-	const reviewRounds = getReviewRoundsForStage(submission, stageId);
-
-	reviewRounds.forEach((reviewRound) => {
-		reviewMenuItems.push(
-			getReviewItem({
-				stageId,
-				reviewRound,
-				isActive:
-					activeStage.id === stageId && activeReviewRound.id === reviewRound.id,
-				title: title,
-			}),
-		);
-	});
-
-	return reviewMenuItems;
-}
-
-export function getReviewItem({stageId, reviewRound, isActive, title}) {
-	return {
-		key: `workflow_${stageId}_${reviewRound.id}`,
-		label: t('dashboard.workflow.reviewRoundN', {number: reviewRound.round}),
-		colorStripe: isActive ? StageColors[stageId] : null,
-		action: 'selectMenu',
-		actionArgs: {
-			primaryMenuItem: 'workflow',
-			stageId: stageId,
-			reviewRoundId: reviewRound.id,
-			title: title,
-		},
-	};
 }
 
 export function useWorkflowNavigationConfigOPS(pageInitConfig) {
