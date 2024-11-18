53a90aab92631757d03257c0ec1aff0036fbe49b
ipula <ipularanasinghe007@gmail.com>
2024-11-18 18:00:21 +0100

    hide actions that not allowed to do current user in the user list

--- src/managers/UserAccessManager/useUserAccessManagerActions.js
+++ src/managers/UserAccessManager/useUserAccessManagerActions.js
@@ -3,6 +3,7 @@ import {useLegacyGridUrl} from '@/composables/useLegacyGridUrl';
 import {useModal} from '@/composables/useModal';
 import {useFetch, getCSRFToken} from '@/composables/useFetch';
 import {useUrl} from '@/composables/useUrl';
+import {useCurrentUser} from '@/composables/useCurrentUser';
 
 export const Actions = {
 	USER_ACCESS_EDIT: 'editUser',
@@ -15,6 +16,7 @@ export const Actions = {
 
 export function useUserAccessManagerActions() {
 	const {t} = useLocalize();
+	const {getCurrentUserId} = useCurrentUser();
 
 	function getItemActions(userObj) {
 		const actions = [];
@@ -31,33 +33,35 @@ export function useUserAccessManagerActions() {
 			name: Actions.USER_ACCESS_SEND_MAIL,
 		});
 
-		actions.push({
-			label: t('grid.action.logInAs'),
-			icon: 'LoginAs',
-			name: Actions.USER_ACCESS_LOGIN_AS,
-		});
+		if (getCurrentUserId() !== userObj.id) {
+			actions.push({
+				label: t('grid.action.logInAs'),
+				icon: 'LoginAs',
+				name: Actions.USER_ACCESS_LOGIN_AS,
+			});
 
-		actions.push({
-			label: t('grid.action.remove'),
-			icon: 'Cancel',
-			name: Actions.USER_ACCESS_REMOVE_USER,
-			isWarnable: true,
-		});
+			actions.push({
+				label: t('grid.action.remove'),
+				icon: 'Cancel',
+				name: Actions.USER_ACCESS_REMOVE_USER,
+				isWarnable: true,
+			});
 
-		actions.push({
-			label: userObj.disabled
-				? t('grid.action.enable')
-				: t('grid.action.disable'),
-			icon: 'DisableUser',
-			name: Actions.USER_ACCESS_DISABLE_USER,
-			isWarnable: true,
-		});
+			actions.push({
+				label: userObj.disabled
+					? t('grid.action.enable')
+					: t('grid.action.disable'),
+				icon: 'DisableUser',
+				name: Actions.USER_ACCESS_DISABLE_USER,
+				isWarnable: true,
+			});
 
-		actions.push({
-			label: t('grid.action.mergeUser'),
-			icon: 'MergeUser',
-			name: Actions.USER_ACCESS_MERGE_USER,
-		});
+			actions.push({
+				label: t('grid.action.mergeUser'),
+				icon: 'MergeUser',
+				name: Actions.USER_ACCESS_MERGE_USER,
+			});
+		}
 
 		return actions;
 	}
