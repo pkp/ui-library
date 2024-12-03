09c7970e6b3825885e8d4bcd292752a8feaf9e84
Erik Hanson <erik@erikhanson.dev>
2024-12-03 11:05:03 -0800

    pkp/pkp-lib#9661 Allow Journal Managers to invite users to adopt a role - ORCiD

--- src/pages/acceptInvitation/AcceptInvitationVerifyOrcid.vue
+++ src/pages/acceptInvitation/AcceptInvitationVerifyOrcid.vue
@@ -15,23 +15,57 @@
 
 <script setup>
 import PkpButton from '@/components/Button/Button.vue';
-import {defineProps} from 'vue';
+import {defineProps, onMounted} from 'vue';
 import {useLocalize} from '@/composables/useLocalize';
 import {useAcceptInvitationPageStore} from './AcceptInvitationPageStore';
 
-defineProps({});
+const props = defineProps({
+	orcidUrl: {type: String, required: true},
+	orcidOAuthUrl: {type: String, required: true},
+});
+
 const store = useAcceptInvitationPageStore();
 const {t} = useLocalize();
+
+onMounted(() => {
+	pkp.eventBus.$on('addOrcidInvitationData', (data) => setOrcidData(data));
+});
+
+/**
+ * Processes ORCID data for an invitation.
+ *
+ * @param {Object|null} data - The ORCID OAuth data object.
+ * @param {string} data.orcid - The ORCID URL of the user.
+ * @param {boolean} data.orcidIsVerified - Indicates if the user's ORCID is verified.
+ * @param {null|string} data.orcidAccessDenied - Indicates if access to ORCID is denied (null if not denied).
+ * @param {string} data.orcidAccessToken - The access token for ORCID API.
+ * @param {string} data.orcidAccessScope - The scope of access for the ORCID API.
+ * @param {string} data.orcidRefreshToken - The refresh token for obtaining new access tokens.
+ * @param {string} data.orcidAccessExpiresOn - The expiration date and time of the access token in ISO format.
+ *
+ * @returns {void}
+ */
+async function setOrcidData(data) {
+	store.setOrcidData(data);
+	await store.nextStep();
+}
 
 /**
  * Go to the next step
  */
 function skipOrcid() {
-	delete store.acceptInvitationPayload.userOrcid;
 	store.openStep(store.steps[1 + store.currentStepIndex].id);
 }
 
+/**
+ * Initiates ORCID OAuth granting flow
+ */
 function verifyOrcid() {
-	store.openStep(store.steps[1 + store.currentStepIndex].id);
+	const oauthWindow = window.open(
+		props.orcidOAuthUrl,
+		'_blank',
+		'toolbar=no, scrollbars=yes, width=540, height=700, top=500, left=500',
+	);
+	oauthWindow.opener = self;
 }
 </script>
