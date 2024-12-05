f0c5b2674caf9dc40b8c12cf0b8dd2b67f6a03ba
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2024-12-05 14:22:19 +0100

    pkp/pkp-lib#10598 Calculate days from last activity in submission listing

--- src/pages/dashboard/components/DashboardTable/CellSubmissionDays.vue
+++ src/pages/dashboard/components/DashboardTable/CellSubmissionDays.vue
@@ -1,11 +1,19 @@
 <template>
-	<TableCell>(todo)</TableCell>
+	<TableCell>{{ days }}</TableCell>
 </template>
 <script setup>
-import {defineProps} from 'vue';
+import {computed} from 'vue';
+import {useDate} from '@/composables/useDate';
+
 import TableCell from '@/components/Table/TableCell.vue';
 
-defineProps({
+const props = defineProps({
 	item: {type: Object, required: true},
+});
+
+const {calculateDaysBetweenDates} = useDate();
+
+const days = computed(() => {
+	return calculateDaysBetweenDates(props.item.dateLastActivity, new Date());
 });
 </script>
