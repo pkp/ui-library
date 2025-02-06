b073674bb2959ebde27ce98f06df036be90be0b1
Taslan Graham <gtaslan1@gmail.com>
2025-02-06 11:52:41 -0500

    pkp/pkp-lib#10449 WIP: initial work

---
+++ src/components/Category/Category.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>{{ categories }} {{ apiUrl }}</div>
+</template>
+
+<script setup>
+defineProps({
+	categories: {
+		type: Array,
+		required: true,
+		validator: (value) => {
+			return true;
+		},
+	},
+
+	apiUrl: {
+		type: String,
+		default: '',
+	},
+});
+</script>
