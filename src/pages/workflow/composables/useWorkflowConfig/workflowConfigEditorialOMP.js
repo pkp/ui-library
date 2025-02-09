320cc984c1cdd58997d0a73d2d4d062ac470bc83
Touhidur Rahman <abir.ruet@gmail.com>
2025-02-09 12:34:37 +0600

    pkp/pkp-lib#4787 compatibility for OMP added

--- src/pages/workflow/composables/useWorkflowConfig/workflowConfigEditorialOMP.js
+++ src/pages/workflow/composables/useWorkflowConfig/workflowConfigEditorialOMP.js
@@ -217,7 +217,12 @@ export const WorkflowConfig = {
 
 			return items;
 		},
-		getSecondaryItems: ({submission, selectedReviewRound, selectedStageId}) => {
+		getSecondaryItems: ({
+			submission,
+			selectedReviewRound,
+			selectedStageId,
+			pageInitConfig,
+		}) => {
 			const items = [];
 			if (!selectedReviewRound) {
 				return [];
@@ -242,6 +247,17 @@ export const WorkflowConfig = {
 					submissionStageId: selectedStageId,
 				},
 			});
+
+			if (pageInitConfig.publicationSettings.isReviewerSuggestionEnabled) {
+				items.push({
+					component: 'ReviewerSuggestionManager',
+					props: {
+						submission,
+						submissionStageId: selectedStageId,
+						reviewRoundId: selectedReviewRound?.id,
+					},
+				});
+			}
 
 			return items;
 		},
