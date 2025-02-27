933a932884be939be41ead55ac10386c230556c5
ipula <ipularanasinghe007@gmail.com>
2025-02-27 16:50:38 +0100

    add new prop for html tag in form item display

--- src/components/FormDisplay/FormDisplayItemBasic.vue
+++ src/components/FormDisplay/FormDisplayItemBasic.vue
@@ -2,7 +2,8 @@
 	<component :is="headingElement" class="text-base-normal text-secondary">
 		{{ heading }}
 	</component>
-	<p class="pt-1 text-lg-normal">{{ value }}</p>
+	<p v-if="!htmlValue" class="pt-1 text-lg-normal">{{ value }}</p>
+	<p v-else v-strip-unsafe-html="htmlValue" class="pt-1 text-lg-normal"></p>
 </template>
 
 <script setup>
@@ -10,5 +11,6 @@ defineProps({
 	headingElement: {required: true, type: String},
 	heading: {type: String, required: true},
 	value: {type: String, required: false, default: '-'},
+	htmlValue: {type: String, required: false, default: null},
 });
 </script>
