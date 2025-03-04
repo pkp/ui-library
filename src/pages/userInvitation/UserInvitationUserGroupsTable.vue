f1ebcf4594d411176490065651be74c206573e73
ipula <ipularanasinghe007@gmail.com>
2025-03-04 15:50:16 +0100

    allow users to invite romved role again

--- src/pages/userInvitation/UserInvitationUserGroupsTable.vue
+++ src/pages/userInvitation/UserInvitationUserGroupsTable.vue
@@ -66,7 +66,7 @@
 							:label="t('invitation.role.selectRole')"
 							:is-required="true"
 							:value="userGroupToAdd.userGroupId"
-							:options="availableUserGroups"
+							:options="userGroups"
 							:all-errors="{
 								userGroupId:
 									userGroupErrors['userGroupsToAdd.' + index + '.userGroupId'],
@@ -182,10 +182,6 @@ function updateUserGroup(index, fieldName, newValue) {
 	store.updatePayload('userGroupsToAdd', userGroupsUpdate, false);
 	updateWithSelectedUserGroups(props.userGroups);
 }
-
-const availableUserGroups = computed(() => {
-	return props.userGroups;
-});
 
 /**
  * add user groups to the invitation payload
