10a8f982a837943237b249d2ee2e85345f998fd8
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2025-03-05 16:09:23 +0100

    pkp/pkp-lib#10768 Add warning messages for author/reviewer combined with editor

--- src/pages/dashboard/components/DashboardTable/DashboardCellSubmissionActions.vue
+++ src/pages/dashboard/components/DashboardTable/DashboardCellSubmissionActions.vue
@@ -15,14 +15,51 @@
 import {defineProps, computed} from 'vue';
 import PkpButton from '@/components/Button/Button.vue';
 import TableCell from '@/components/Table/TableCell.vue';
-import {useDashboardPageStore} from '@/pages/dashboard/dashboardPageStore.js';
+import {useCurrentUser} from '@/composables/useCurrentUser';
+
+import {
+	DashboardPageTypes,
+	useDashboardPageStore,
+} from '@/pages/dashboard/dashboardPageStore.js';
+
+const {
+	hasCurrentUserAtLeastOneAssignedRoleInAnyStage,
+	isCurrentUserAssignedAsReviewer,
+} = useCurrentUser();
 
 const props = defineProps({
 	item: {type: Object, required: true},
 });
 
-const showButton = computed(() => !props.item.submissionProgress);
+const showButton = computed(() => {
+	if (props.item.submissionProgress) {
+		return false;
+	}
 
+	if (
+		dashboardPageStore.dashboardPage === DashboardPageTypes.EDITORIAL_DASHBOARD
+	) {
+		// Don't show View button, when being author/reviewer on editorial dashboard while not being explicitely assigned to editorial role
+
+		const isCurrentUserAssignedAsAuthorOrReviewer =
+			isCurrentUserAssignedAsReviewer(props.item) ||
+			hasCurrentUserAtLeastOneAssignedRoleInAnyStage(props.item, [
+				pkp.const.ROLE_ID_AUTHOR,
+			]);
+		if (
+			isCurrentUserAssignedAsAuthorOrReviewer &&
+			!hasCurrentUserAtLeastOneAssignedRoleInAnyStage(props.item, [
+				pkp.const.ROLE_ID_MANAGER,
+				pkp.const.ROLE_ID_SUB_EDITOR,
+				pkp.const.ROLE_ID_ASSISTANT,
+			])
+		) {
+			return false;
+		}
+	}
+
+	return true;
+});
 function handleAction() {
 	dashboardPageStore.openWorkflowModal(props.item.id);
 }
