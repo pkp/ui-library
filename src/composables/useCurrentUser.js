203b965775c68c2aadd61d4148ac8f8a1087fcab
Blesilda Ramirez <ramirezblesilda@gmail.com>
2025-03-28 00:59:30 -0400

    pkp/pkp-lib#10779 Add more methods to useCurrentUser api

--- src/composables/useCurrentUser.js
+++ src/composables/useCurrentUser.js
@@ -16,6 +16,44 @@ export function useCurrentUser() {
 		return pkp.currentUser.id;
 	}
 
+	function getCurrentUserName() {
+		return pkp.currentUser.username;
+	}
+
+	function getCurrentUserInitials() {
+		return pkp.currentUser.initials;
+	}
+
+	function getUserLoggedInAsUserName() {
+		if (!isUserLoggedInAs()) {
+			return '';
+		}
+
+		const userLoggedInAs = getUserLoggedInAs();
+		return userLoggedInAs?.username || '';
+	}
+
+	function getUserLoggedInAsInitials() {
+		if (!isUserLoggedInAs()) {
+			return '';
+		}
+
+		const userLoggedInAs = getUserLoggedInAs();
+		return userLoggedInAs?.initials || '';
+	}
+
+	function getUnreadNotifications() {
+		return pkp.currentUser.unreadTasksCount;
+	}
+
+	function isUserLoggedInAs() {
+		return pkp.currentUser.isUserLoggedInAs;
+	}
+
+	function getUserLoggedInAs() {
+		return pkp.currentUser.loggedInAsUser;
+	}
+
 	function hasCurrentUserAtLeastOneAssignedRoleInStage(
 		submission,
 		stageId,
@@ -64,5 +102,12 @@ export function useCurrentUser() {
 		hasCurrentUserAtLeastOneAssignedRoleInStage,
 		hasCurrentUserAtLeastOneAssignedRoleInAnyStage,
 		isCurrentUserAssignedAsReviewer,
+		getUnreadNotifications,
+		isUserLoggedInAs,
+		getUserLoggedInAs,
+		getCurrentUserName,
+		getCurrentUserInitials,
+		getUserLoggedInAsUserName,
+		getUserLoggedInAsInitials,
 	};
 }
