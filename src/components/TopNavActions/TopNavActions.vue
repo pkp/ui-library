957f0df9e62cde50641b5198ebb02c06433ddfbc
Blesilda Ramirez <ramirezblesilda@gmail.com>
2025-03-28 01:00:47 -0400

    pkp/pkp-lib#10779 Move top nav action buttons to TopNavActions component

--- src/components/TopNavActions/TopNavActions.vue
+++ src/components/TopNavActions/TopNavActions.vue
@@ -1,22 +1,195 @@
 <template>
-	<div
-		class="pointer-events-auto flex h-12 items-center justify-end bg-selection-dark px-4"
-	>
-		<div class="ms-auto flex h-full whitespace-nowrap">
-			<div>
-				<a
-					href="#"
-					target="_blank"
-					class="relative flex h-full items-center p-2 leading-8 !text-on-dark outline-none hover:-translate-y-1 hover:text-on-dark hover:shadow-[0_0.25rem_#fff] focus:-translate-y-1 focus:text-on-dark focus:shadow-[0_0.25rem_#fff]"
+	<div class="z-50 ms-auto flex h-full whitespace-nowrap">
+		<div>
+			<a
+				href="#"
+				target="_blank"
+				:class="animationStyle"
+				class="flex h-full items-center p-2"
+			>
+				<Icon icon="HelpTopNav" class="h-7 w-7"></Icon>
+				<span class="-screenReader">{{ t('common.help') }}</span>
+			</a>
+		</div>
+		<div>
+			<button
+				ref="tasksButton"
+				:class="animationStyle"
+				class="border-none p-2"
+				@click="openTasks"
+			>
+				<Icon icon="Notifications" class="h-7 w-7"></Icon>
+				<span class="-screenReader">{{ t('common.tasks') }}</span>
+				<span
+					v-if="unreadTasksCount"
+					class="absolute right-3 top-2 rounded-[2px] bg-negative p-[2px] text-lg-normal leading-none text-on-dark shadow"
 				>
-					<Icon icon="HelpTopNav" class="h-7 w-7"></Icon>
-					<span class="-screenReader">{translate key="common.help"}</span>
-				</a>
-			</div>
+					{{ unreadTasksCount }}
+				</span>
+			</button>
+		</div>
+		<div>
+			<Dropdown class="flex h-full" :is-transparent="true">
+				<template #button>
+					<InitialsAvatar
+						:initials="activeUser.initials"
+						:is-secondary="true"
+						:is-disabled="_isUserLoggedInAs"
+					></InitialsAvatar>
+					<span class="-screenReader">{{ activeUser.username }}</span>
+
+					<InitialsAvatar
+						v-if="_isUserLoggedInAs"
+						:initials="currentUser.initials"
+						class="absolute top-0 h-5 w-5 rounded-full shadow ltr:right-0 rtl:left-0"
+						:is-warnable="true"
+						:shrink="true"
+					></InitialsAvatar>
+					<span class="-screenReader">{{ currentUser.username }}</span>
+				</template>
+				<nav
+					:aria-label="t('common.navigation.user')"
+					class="mb-2 min-w-52 max-w-[20em]"
+				>
+					<div class="-ml-2 -mr-2 border-b border-b-light pb-2 pl-2 pr-2">
+						<div class="ps-2 text-base-bold leading-6 text-secondary">
+							Change Language
+						</div>
+						<ul>
+							<li
+								v-for="locale in supportedLocales"
+								:key="locale.key"
+								:class="actionLinkStyle"
+							>
+								<a :href="locale.href" class="block w-full no-underline">
+									<Icon
+										v-if="locale.key === currentLocale"
+										icon="Complete"
+										class="h-5 w-5"
+										:inline="true"
+									></Icon>
+									{{ locale.value }}
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div
+						v-if="_isUserLoggedInAs"
+						class="-ml-2 -mr-2 text-wrap border-b border-b-light p-2"
+					>
+						<div
+							class="mb-1 mt-1 pl-2 pr-2 text-base-normal leading-5 text-secondary"
+						>
+							{{
+								t('manager.people.signedInAs', {username: currentUser.username})
+							}}
+
+							<a :href="signOutLink">
+								{{ t('user.logOutAs', {username: currentUser.username}) }}
+							</a>
+							.
+						</div>
+					</div>
+					<div class="-ml-2 -mr-2 text-wrap px-2 pt-2">
+						<ul>
+							<li :class="actionLinkStyle">
+								<a :href="editProfileLink" class="w-full">
+									{{ t('user.profile.editProfile') }}
+								</a>
+							</li>
+							<li :class="actionLinkStyle">
+								<a :href="signOutLink" class="w-full">
+									{{
+										_isUserLoggedInAs
+											? t('user.logOutAs', {username: currentUser.username})
+											: t('user.logOut')
+									}}
+								</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</Dropdown>
 		</div>
 	</div>
 </template>
 
 <script setup>
 import Icon from '@/components/Icon/Icon.vue';
+import Dropdown from '@/components/Dropdown/Dropdown.vue';
+import InitialsAvatar from '@/components/InitialsAvatar/InitialsAvatar.vue';
+import {useLocalize} from '@/composables/useLocalize';
+import {useLegacyGridUrl} from '@/composables/useLegacyGridUrl';
+import {useCurrentUser} from '@/composables/useCurrentUser';
+import {useUrl} from '@/composables/useUrl';
+
+const {t} = useLocalize();
+
+const {
+	getUnreadNotifications,
+	isUserLoggedInAs,
+	getCurrentUserName,
+	getCurrentUserInitials,
+	getUserLoggedInAsUserName,
+	getUserLoggedInAsInitials,
+} = useCurrentUser();
+
+const unreadTasksCount = getUnreadNotifications();
+const _isUserLoggedInAs = isUserLoggedInAs();
+const currentUser = {
+	username: getCurrentUserName(),
+	initials: getCurrentUserInitials(),
+};
+const activeUser = _isUserLoggedInAs
+	? {
+			username: getUserLoggedInAsUserName(),
+			initials: getUserLoggedInAsInitials(),
+		}
+	: currentUser;
+
+const supportedLocales = Object.entries(
+	pkp.context?.supportedLocales || {},
+).map(([key, value]) => {
+	const {pageUrl} = useUrl(`user/setLocale/${key}`);
+	return {key, value, href: pageUrl.value};
+});
+
+const currentLocale = pkp.context?.currentLocale;
+
+const animationStyle = [
+	'relative leading-8 !text-on-dark outline-none bg-transparent',
+	'hover:-translate-y-1 hover:text-on-dark hover:shadow-[0_0.25rem_#fff]',
+	'focus:-translate-y-1 focus:text-on-dark focus:shadow-[0_0.25rem_#fff]',
+];
+
+const actionLinkStyle = [
+	'max-w-full cursor-pointer overflow-hidden truncate whitespace-nowrap rounded border border-transparent bg-transparent px-2 py-1 leading-6 text-primary hover:!border-primary',
+];
+
+const {pageUrl: signOutLink} = useUrl(
+	`login/${_isUserLoggedInAs ? 'signOutAsUser' : 'signOut'}`,
+);
+
+const {pageUrl: editProfileLink} = useUrl('user/profile');
+
+/**
+ * Open a modal showing the user's tasks
+ */
+function openTasks() {
+	const {openLegacyModal} = useLegacyGridUrl({
+		component: 'grid.notifications.taskNotificationsGridHandler',
+		op: 'fetchGrid',
+	});
+
+	openLegacyModal({});
+}
 </script>
+
+<style>
+.pkpDropdown > button {
+	@apply h-full !rounded-none hover:-translate-y-1 hover:text-on-dark hover:shadow-[0_0.25rem_#fff] focus:-translate-y-1 focus:text-on-dark focus:shadow-[0_0.25rem_#fff];
+}
+.pkpDropdown .pkpDropdown__content {
+	@apply ltr:right-1 rtl:left-1;
+}
+</style>
