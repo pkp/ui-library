7782673b03281be60cc11c193a4d1c4f6008d3ef
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2025-04-09 14:06:27 +0200

    pkp/pkp-lib#10067 Fix storybook url for legacy grids

--- public/globals.js
+++ public/globals.js
@@ -36,7 +36,7 @@ window.pkp = {
 		datetimeFormatLong: 'F j, Y - h:i A',
 		timeFormat: 'h:i A',
 		legacyGridBaseUrl:
-			'http://mock/index.php/publicknowledge/$$$call$$$/component/action',
+			'https://mock/index.php/publicknowledge/$$$call$$$/component/action',
 	},
 	/**
 	 * Dummy constants required by components
@@ -872,8 +872,9 @@ window.pkp = {
 		'user.password': 'Password',
 		'user.phone': 'Phone',
 		'user.removeRole.message':
-			'Are you sure you want to permanently remove this role? This action will revoke the user\'s access to all information and permissions associated with this role.',
-		'user.removeRole.roleRemainMessage' :'You cannot remove the role. At least one role must be assigned to the user.',
+			"Are you sure you want to permanently remove this role? This action will revoke the user's access to all information and permissions associated with this role.",
+		'user.removeRole.roleRemainMessage':
+			'You cannot remove the role. At least one role must be assigned to the user.',
 		'user.role.reviewer': 'Reviewer',
 		'user.role.reviewers': 'Reviewers',
 		'user.roles': 'Roles',
@@ -893,7 +894,7 @@ window.pkp = {
 		'userInvitation.edit.title': 'Edit Invitation',
 		'userInvitation.modal.button': 'View All Users',
 		'userInvitation.modal.message':
-			'{$email} has been invited to new role in OJS. You can be updated about the user\'s decision on the User & Role page, your OJS notification and/or your email.',
+			"{$email} has been invited to new role in OJS. You can be updated about the user's decision on the User & Role page, your OJS notification and/or your email.",
 		'userInvitation.modal.title': 'Invitation Sent',
 		'userInvitation.roleTable.endDate': 'End Date',
 		'userInvitation.roleTable.journalMasthead': 'Journal Masthead',
@@ -925,9 +926,9 @@ window.pkp = {
 			'The submission advanced to the next review round, was accepted, and is currently in the {$stage} stage.',
 		'workflow.uploadRevisions': 'Upload revisions',
 		'grid.user.grid.user.enableReasonDescription':
-			'Once the user is enabled, they will regain access to OJS, and you\'ll be able to invite them to roles as needed.',
+			"Once the user is enabled, they will regain access to OJS, and you'll be able to invite them to roles as needed.",
 		'grid.user.grid.user.disableReasonDescription':
-			'Please note that once a user is disabled, you won\'t be able to add them to any roles until they are enabled again.'
+			"Please note that once a user is disabled, you won't be able to add them to any roles until they are enabled again.",
 	},
 	tinyMCE: {
 		skinUrl: '/styles/tinymce',
