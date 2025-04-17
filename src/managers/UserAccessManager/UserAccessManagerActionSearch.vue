6adbc96e5edf6a01c9a5159d98bb23bf16fd23ea
ipula <ipularanasinghe007@gmail.com>
2025-04-17 13:53:39 +0200

    improve user access manager extensibility

---
+++ src/managers/UserAccessManager/UserAccessManagerActionSearch.vue
@@ -0,0 +1,14 @@
+<template>
+	<Search
+		:search-phrase="store.searchPhrase"
+		:search-label="t('userAccess.search')"
+		@search-phrase-changed="store.setSearchPhrase"
+	/>
+</template>
+<script setup>
+import Search from '@/components/Search/Search.vue';
+
+import {useUserAccessManagerStore} from './UserAccessManagerStore';
+
+const store = useUserAccessManagerStore();
+</script>
