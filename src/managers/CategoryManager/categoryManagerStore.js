4b5af0b8b2eaaf7f5467bd3d7a9599a4b5f1814d
taslangraham <gtaslan1@gmail.com>
2025-04-30 15:38:29 -0500

    pkp/pkp-lib#10449 Refactor based on code review

--- src/managers/CategoryManager/categoryManagerStore.js
+++ src/managers/CategoryManager/categoryManagerStore.js
@@ -7,133 +7,115 @@ import {useUrl} from '@/composables/useUrl';
 import {useFetch} from '@/composables/useFetch';
 import EditCategoryFormModal from '@/managers/CategoryManager/EditCategoryFormModal.vue';
 import {useForm} from '@/composables/useForm';
+import {useCategoryManagerConfig} from '@/managers/CategoryManager/useCategoryManagerConfig';
+import {useExtender} from '@/composables/useExtender';
+import {cloneDeep} from 'lodash';
 
-const Actions = {
+export const Actions = {
 	CATEGORY_EDIT: 'categoryEdit',
 	CATEGORY_ADD: 'categoryAdd',
 	CATEGORY_DELETE: 'categoryDelete',
 };
-
-const ActionFormLabel = {
-	[Actions.CATEGORY_EDIT]: 'Edit category',
-	[Actions.CATEGORY_ADD]: 'Add category',
-};
+const {t} = useLocalize();
 export const useCategoryManagerStore = defineComponentStore(
 	'categoryManagerStore',
 	(props) => {
 		const {apiUrl} = useUrl('categories');
-		const {t} = useLocalize();
+		const extender = useExtender();
+		const categoryManagerConfig = extender.addFns(useCategoryManagerConfig());
+		const {data: categories, fetch: fetchCategories} = useFetch(apiUrl, {
+			method: 'GET',
+		});
 
-		const _categories = ref(props.categories || []);
 		const _expandedIds = ref(new Set());
-		const isLoading = ref(false);
-
-		const columns = computed(() => props.columns);
+		const columns = computed(() => categoryManagerConfig.getColumns());
 		const expanded = computed(() => Array.from(_expandedIds.value));
-
-		const categories = computed(() => _categories.value);
-		const currentCategoryForm = ref({});
-		const categoryForm = props.categoryForm; // Blank form used only for adding new categories
+		const categoryForm = props.categoryForm;
+		fetchCategories();
 
 		/**
-		 * Prepares the form data for adding or editing a category.
-		 * - When adding a new category, it initializes the form with default values.
-		 * - When editing an existing category, it fetches the form data for the specified category.
+		 * Get the form data for adding or editing a category.
 		 * @param {Object|null} category - The category object being edited or the parent category for a new subcategory.
 		 *                                 Pass `null` when adding a top-level category.
 		 * @param {string} action - Action being performed. One of Actions.CATEGORY_ADD or Actions.CATEGORY_EDIT.
+		 * @return {Object} - The category form.
 		 */
-		async function setupCategoryForm(category, action) {
+		function getCategoryForm(category, action) {
 			// Get a deep copy of the form to eliminate references
-			let preparedForm = JSON.parse(JSON.stringify(categoryForm));
+			let preparedForm = cloneDeep(categoryForm);
+			const {setValues, setAction, setMethod} = useForm(preparedForm);
+
 			category = category || {};
 
 			// If adding a new Category as sub category of an existing one, then set existing category ID as parent ID of new the category
 			if (Object.entries(category).length && action === Actions.CATEGORY_ADD) {
-				preparedForm.action = `${preparedForm.action}?parentCategoryId=${category.id}`;
+				setAction(`${preparedForm.action}?parentCategoryId=${category.id}`);
+				return preparedForm;
 			}
 
 			// return empty form if adding a new top-level category
 			if (!Object.entries(category).length || action === Actions.CATEGORY_ADD) {
-				currentCategoryForm.value = preparedForm;
-				return;
+				return preparedForm;
 			}
 
 			// Prepare form for editing existing category
 			if (Object.entries(category).length && action === Actions.CATEGORY_EDIT) {
-				preparedForm = await getEditForm(category.id);
-				preparedForm.method = 'PUT';
-				preparedForm.action = `${preparedForm.action}/${category.id}`;
+				setValues({
+					...category,
+					...transformAssignedEditorsToSubEditorFields(
+						category.assignedEditors,
+					),
+				});
+				setMethod('PUT');
+				setAction(`${preparedForm.action}/${category.id}`);
 			}
-			currentCategoryForm.value = preparedForm;
+
+			return preparedForm;
 		}
 
 		/**
-		 * Get configured form to edit a category.
-		 * @param {number} categoryId - The ID of the category to edit.
-		 * @return {Promise<Object>} - The populated form for editing the category.
+		 * Transforms the assigned editors data into form field entries for subEditor fields.
+		 * @param {Array<{id: number, userGroupIds: Array<number>}>} assignedEditors - Array of assigned editor objects.
+		 * @return {Object.<string, Array<number>>}  - The transformed object with subEditor fields.
+		 * Example:
+		 * ```
+		 * {
+		 *   subEditors[3]: [1, 2, 3],
+		 * }
+		 * ```
+		 * In this example, the key `subEditors[3]` represents the group ID 3 (e.g. Journal editor), while the value is an array of editor IDs (1, 2, 3) assigned to that group.
 		 */
-		async function getEditForm(categoryId) {
-			const url =
-				apiUrl.value + `/categoryFormComponent?categoryId=${categoryId}`;
-			const {fetch, data} = useFetch(url, {method: 'GET'});
-			await fetch();
-			return data.value;
+		function transformAssignedEditorsToSubEditorFields(assignedEditors) {
+			return assignedEditors.reduce((result, editor) => {
+				editor.userGroupIds.forEach((userGroupId) => {
+					const key = `subEditors[${userGroupId}]`;
+					result[key] = result[key] || [];
+					result[key].push(editor.id);
+				});
+				return result;
+			}, {});
 		}
 
 		/**
 		 * Get the actions available for a category item.
 		 * @return {Array<{ label: string, icon: string, name: string, isWarnable?: boolean}>} - The list of actions available for the category item.
 		 */
-		function getItemActions() {
-			return [
-				{
-					label: t('common.add'),
-					icon: 'Add',
-					name: Actions.CATEGORY_ADD,
-				},
-				{
-					label: t('common.edit'),
-					icon: 'Edit',
-					name: Actions.CATEGORY_EDIT,
-				},
-				{
-					label: t('manager.category.deleteCategory'),
-					icon: 'Cancel',
-					name: Actions.CATEGORY_DELETE,
-					isWarnable: true,
-				},
-			];
-		}
-
-		/*
-		 * Handle the selected action for a category item.
-		 * @param {string} actionName - The name of the action to perform.
-		 * @param {Object|null} category - The category object to perform the action on. This will be null when adding a top-level category.
-		 */
-		async function handleItemAction(actionName, category = null) {
-			if (actionName === Actions.CATEGORY_DELETE) {
-				deleteCategory(category);
-			} else if (
-				actionName === Actions.CATEGORY_ADD ||
-				actionName === Actions.CATEGORY_EDIT
-			) {
-				await setupCategoryForm(category, actionName);
-				openCategoryFormModal(ActionFormLabel[actionName]);
-			}
+		function getItemActions({category}) {
+			return categoryManagerConfig.getItemActions({category});
 		}
 
 		/***
 		 * Deletes a category.
 		 * @param {Object} category - The category to delete.
 		 */
-		function deleteCategory(category) {
+		function categoryDelete(category) {
 			const {openDialog} = useModal();
 			const {apiUrl} = useUrl(`categories/${category.id}`);
 			const {fetch: fetchDelete, isSuccess} = useFetch(apiUrl, {
 				method: 'DELETE',
 			});
 
+			const subCategoriesCount = getSubCategoriesCount(category);
 			openDialog({
 				bodyComponent: CategoryDeleteDialogBody,
 				bodyProps: {
@@ -143,13 +125,13 @@ export const useCategoryManagerStore = defineComponentStore(
 						if (isSuccess.value) {
 							openCategoryDeletedDialog(
 								category.localizedTitle,
-								getSubcategoriesCount(category),
+								subCategoriesCount,
 							);
-							await refreshCategories();
+							await fetchCategories();
 						}
 					},
 					title: category.localizedTitle,
-					subCategoryCount: getSubcategoriesCount(category),
+					subCategoryCount: subCategoriesCount,
 				},
 				title: t('manager.category.delete.confirmationTitle', {
 					title: category.localizedTitle,
@@ -162,16 +144,16 @@ export const useCategoryManagerStore = defineComponentStore(
 		/**
 		 * Open a dialog indicating that the category was deleted.
 		 * @param {string} title - Category title. Used in message displayed to user.
-		 * @param {number} subCategoryCount - The number of subcategories deleted. Used in message displayed to user.
+		 * @param {number} subCategoriesCount - The number of sub-categories deleted. Used in message displayed to user.
 		 */
-		function openCategoryDeletedDialog(title, subCategoryCount) {
+		function openCategoryDeletedDialog(title, subCategoriesCount) {
 			const {openDialog} = useModal();
 
 			openDialog({
 				title: t('manager.category.deleted'),
 				message: t('manager.category.deleted.description', {
 					title,
-					subCategoryCount,
+					subCategoryCount: subCategoriesCount,
 				}),
 
 				actions: [
@@ -185,10 +167,10 @@ export const useCategoryManagerStore = defineComponentStore(
 		}
 
 		/**
-		 * Get the count of subcategories for a given category.
-		 * @param category - The category object to check
+		 * Get the count of sub-categories for a given category.
+		 * @param category - The category object to check.
 		 */
-		function getSubcategoriesCount(category) {
+		function getSubCategoriesCount(category) {
 			if (!category.subCategories || category.subCategories.length === 0) {
 				return 0;
 			}
@@ -196,7 +178,7 @@ export const useCategoryManagerStore = defineComponentStore(
 			let count = category.subCategories.length;
 
 			for (const sub of category.subCategories) {
-				count += getSubcategoriesCount(sub);
+				count += getSubCategoriesCount(sub);
 			}
 
 			return count;
@@ -205,10 +187,10 @@ export const useCategoryManagerStore = defineComponentStore(
 		/**
 		 * Open the form modal to add/edit category.
 		 * @param {string} title - The title to display in the modal.
+		 * @param {Object} form - The category form object to display in the modal.
 		 */
-		function openCategoryFormModal(title) {
+		function openCategoryFormModal(title, form) {
 			const {openSideModal} = useModal();
-			const {form} = useForm(currentCategoryForm);
 			openSideModal(EditCategoryFormModal, {
 				title,
 				formProps: form,
@@ -223,7 +205,7 @@ export const useCategoryManagerStore = defineComponentStore(
 		async function categorySaved(category) {
 			pkp.eventBus.$emit('notify', t('manager.category.saved'), 'success');
 			closeCategoryFormModal();
-			await refreshCategories();
+			await fetchCategories();
 
 			// If saved category has a parent then expand parent to have saved category displayed
 			if (category.parentId) {
@@ -240,17 +222,6 @@ export const useCategoryManagerStore = defineComponentStore(
 		}
 
 		/**
-		 * Loads updated list of categories from API.
-		 */
-		async function refreshCategories() {
-			isLoading.value = true;
-			const {data, fetch} = useFetch(apiUrl, {method: 'GET'});
-			await fetch();
-			_categories.value = data.value;
-			isLoading.value = false;
-		}
-
-		/**
 		 * Expand or collapse a parent in the category tree.
 		 * @param {number} id - The ID of the category to expand/collapse.
 		 */
@@ -262,14 +233,37 @@ export const useCategoryManagerStore = defineComponentStore(
 			}
 		}
 
+		/**
+		 * Edit a category.
+		 * @param {Object} category - The category object to edit.
+		 */
+		function categoryEdit(category) {
+			openCategoryFormModal(
+				t('grid.category.edit'),
+				getCategoryForm(category, Actions.CATEGORY_EDIT),
+			);
+		}
+
+		/**
+		 * Add a new category.
+		 * @param {Object|null} category - If provided, the new category will be added as a subcategory of this category.
+		 */
+		function categoryAdd(category = null) {
+			openCategoryFormModal(
+				t('grid.category.add'),
+				getCategoryForm(category, Actions.CATEGORY_ADD),
+			);
+		}
+
 		return {
 			getItemActions,
-			handleItemAction,
 			toggleItemExpansion,
+			categoryEdit,
+			categoryAdd,
+			categoryDelete,
 			columns,
 			categories,
 			expanded,
-			isLoading,
 		};
 	},
 );
