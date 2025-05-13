be5de1f20b64e6444babef4bc360a93782d81163
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2025-05-13 13:18:42 +0200

    pkp/pkp-lib#11372 Vocabulary for autosuggest

---
+++ src/components/Form/fields/VocabularyModal/VocabularyModalCellName.vue
@@ -0,0 +1,39 @@
+<template>
+	<TableCell>
+		<label
+			v-if="depth > 0"
+			:style="{'padding-inline-start': `${depth + 0.5}rem`}"
+			class="flex items-center gap-x-2"
+		>
+			<input
+				type="checkbox"
+				:checked="
+					vocabularymodalStore.selectedItemIds.includes(item.identifier)
+				"
+				@change="vocabularymodalStore.toggleItemSelection(item)"
+			/>
+			<span>{{ item.name }}</span>
+		</label>
+		<span v-else :class="{'text-base-bold': depth === 0}">
+			{{ item.name }}
+		</span>
+	</TableCell>
+</template>
+
+<script setup>
+import TableCell from '@/components/Table/TableCell.vue';
+import {useVocabularyModalStore} from './vocabularyModalStore';
+
+defineProps({
+	item: {
+		type: Object,
+		required: true,
+	},
+	depth: {
+		type: Number,
+		required: true,
+	},
+});
+
+const vocabularymodalStore = useVocabularyModalStore();
+</script>
--- src/components/Form/fields/VocabularyModal/VocabularyTableRows.vue
+++ src/components/Form/fields/VocabularyModal/VocabularyTableRows.vue
@@ -1,51 +1,39 @@
 <template>
 	<template v-for="item in items" :key="item.identifier">
 		<TableRow>
-			<TableCell>
-				<label
-					v-if="depth > 0"
-					:style="{'padding-inline-start': `${depth + 0.5}rem`}"
-					class="flex items-center gap-x-2"
-				>
-					<input
-						type="checkbox"
-						:checked="selectedItemIds.includes(item.identifier)"
-						@change="emit('toggleItemSelection', item)"
-					/>
-					<span>{{ item.name }}</span>
-				</label>
-				<span v-else :class="{'text-base-bold': depth === 0}">
-					{{ item.name }}
-				</span>
-			</TableCell>
+			<component
+				:is="Components[column.component] || column.component"
+				v-for="(column, i) in store.columns"
+				:key="i"
+				:item="item"
+				:depth="depth"
+			/>
 			<TableCellTreeExpand
 				:is-displayed="item?.items?.length"
-				:is-expanded="expandedIds.includes(item.identifier)"
-				@toggle="emit('toggleItemExpansion', item.identifier)"
+				:is-expanded="store.expandedIds.includes(item.identifier)"
+				@toggle="store.toggleItemExpansion(item.identifier)"
 			></TableCellTreeExpand>
 		</TableRow>
 		<VocabularyTableRows
-			v-if="item?.items?.length && expandedIds.includes(item.identifier)"
+			v-if="item?.items?.length && store.expandedIds.includes(item.identifier)"
 			:items="item.items"
 			:depth="depth + 1"
-			:expanded-ids="expandedIds"
-			:selected-item-ids="selectedItemIds"
-			@toggle-item-expansion="(...args) => emit('toggleItemExpansion', ...args)"
-			@toggle-item-selection="(...args) => emit('toggleItemSelection', ...args)"
 		/>
 	</template>
 </template>
 
 <script setup>
-import TableCell from '@/components/Table/TableCell.vue';
 import TableRow from '@/components/Table/TableRow.vue';
 import TableCellTreeExpand from '@/components/Table/TableCellTreeExpand.vue';
+import VocabularyModalCellName from './VocabularyModalCellName.vue';
+import {useVocabularyModalStore} from './vocabularyModalStore';
+
 defineProps({
 	depth: {type: Number, default: 0},
 	items: {type: Array, default: () => []},
-	expandedIds: {type: Array, default: () => []},
-	selectedItemIds: {type: Array, default: () => []},
 });
 
-const emit = defineEmits(['toggleItemExpansion', 'toggleItemSelection']);
+const Components = {VocabularyModalCellName};
+
+const store = useVocabularyModalStore();
 </script>
