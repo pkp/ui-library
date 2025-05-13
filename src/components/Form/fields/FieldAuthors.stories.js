6572cc73e6824195feb732dc403d302c9cb1a582
GaziYucel <84437883+GaziYucel@users.noreply.github.com>
2025-05-13 16:31:59 +0200

    structured citations (development)

---
+++ src/components/Form/fields/FieldAuthors.stories.js
@@ -0,0 +1,28 @@
+import FieldAuthors from './FieldAuthors.vue';
+import FieldAuthorsMock from '@/components/Form/mocks/field-citation-authors.js';
+
+const args = {...FieldAuthorsMock};
+
+export default {
+	title: 'Forms/FieldAuthors',
+	component: FieldAuthors,
+	args: {},
+	parameters: {},
+	render: (args) => ({
+		components: {FieldAuthors},
+		setup() {
+			return {args};
+		},
+		template: `
+			<FieldAuthors v-bind="args"/>`,
+	}),
+	decorators: [
+		() => ({
+			template: '<div style="height: 600px"><story/></div>',
+		}),
+	],
+};
+
+export const Base = {
+	args: {...FieldAuthorsMock},
+};
---
+++ src/managers/CitationManager/_dummyComponent.vue
@@ -0,0 +1,10 @@
+<template>
+	_dummy_
+</template>
+
+<script setup>
+const props = defineProps({
+	// submission: {type: Object, required: true},
+	// publication: {type: Object, required: true},
+});
+</script>
