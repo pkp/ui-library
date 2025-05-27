ec84c0f13fbfcef2b0c0cacd9dbe309c4d2152f2
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2025-05-27 20:02:00 +0200

    pkp/pkp-lib#11421 omp does not have recommendation

--- src/pages/dashboard/composables/useDashboardConfigReviewActivity.js
+++ src/pages/dashboard/composables/useDashboardConfigReviewActivity.js
@@ -162,6 +162,9 @@ const ConfigPerStatus = {
 		},
 		titleKey: tk('dashboard.reviewAssignment.statusReceived.title'),
 		descriptionKey: tk('dashboard.reviewAssignment.statusReceived.description'),
+		descriptionWithoutRecommendationKey: tk(
+			'dashboard.reviewAssignment.statusReceived.withoutRecommendation.description',
+		),
 		textAction: null,
 		primaryAction: ReviewActivityActions.VIEW_UNREAD_RECOMMENDATION,
 		negativeAction: null,
@@ -180,6 +183,9 @@ const ConfigPerStatus = {
 		},
 		titleKey: tk('dashboard.reviewAssignment.statusReceived.title'),
 		descriptionKey: tk('dashboard.reviewAssignment.statusReceived.description'),
+		descriptionWithoutRecommendationKey: tk(
+			'dashboard.reviewAssignment.statusReceived.withoutRecommendation.description',
+		),
 		textAction: null,
 		primaryAction: ReviewActivityActions.VIEW_RECOMMENDATION,
 		negativeAction: null,
@@ -339,10 +345,18 @@ export function useDashboardConfigReviewActivity() {
 		const title = getTitle(config, date, days);
 
 		function getDescription() {
-			return t(config.descriptionKey, {
+			const recommendation = getRecommendation();
+			let descriptionKey = config.descriptionKey;
+
+			// use different description if recommendation is not available (omp)
+			if (config.descriptionWithoutRecommendationKey && !recommendation) {
+				descriptionKey = config.descriptionWithoutRecommendationKey;
+			}
+
+			return t(descriptionKey, {
 				date: formatShortDate(date),
 				days: Math.abs(days),
-				recommendation: getRecommendation(),
+				recommendation,
 			});
 		}
 
