eda812c522e280a59c21edfeff1bc4b164827eb1
GaziYucel <84437883+GaziYucel@users.noreply.github.com>
2025-05-30 15:05:56 +0200

    structured citations (development)

---
+++ src/managers/CitationManager/CitationManagerCellExpandAll.vue
@@ -0,0 +1,15 @@
+<template>
+	<TableCell :is-row-header="true">
+		<span class="text-base-normal">
+			CitationManagerCellExpandAll
+		</span>
+	</TableCell>
+</template>
+
+<script setup>
+import TableCell from '@/components/Table/TableCell.vue';
+
+const props = defineProps({
+	// citation: {type: Object, required: true}
+});
+</script>
