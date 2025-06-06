477103354e835cc74bd884128930c32972ed4c68
Erik Hanson <erik@erikhanson.dev>
2025-06-05 19:05:25 -0700

    pkp/pkp-lib#11417 Fix pre-release invitation regressions

--- src/components/FormDisplay/FormDisplayItemBasic.vue
+++ src/components/FormDisplay/FormDisplayItemBasic.vue
@@ -2,8 +2,11 @@
 	<component :is="headingElement" class="text-base-normal text-secondary">
 		{{ heading }}
 	</component>
-	<p v-if="!htmlValue" class="pt-1 text-lg-normal">{{ value }}</p>
-	<p v-else v-strip-unsafe-html="htmlValue" class="pt-1 text-lg-normal"></p>
+	<div class="flex items-center">
+		<p v-if="!htmlValue" class="pt-1 text-lg-normal">{{ value }}</p>
+		<p v-else v-strip-unsafe-html="htmlValue" class="pt-1 text-lg-normal"></p>
+		<slot name="valueSuffix"></slot>
+	</div>
 </template>
 
 <script setup>
