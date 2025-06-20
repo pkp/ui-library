8a11981df7a0e47ada46a12aac4afa9a3de1ea10
Blesilda Ramirez <ramirezblesilda@gmail.com>
2025-06-20 13:59:25 -0400

    pkp/pkp-lib#11291 Add TableColGroup component

---
+++ src/components/Table/TableColGroup.vue
@@ -0,0 +1,20 @@
+<template>
+	<th
+		:id="colGroupId"
+		:colspan="tableContext.columnsCount.value"
+		scope="colgroup"
+		class="whitespace-nowrap border-b border-t border-light bg-tertiary px-2 py-4 text-start text-lg-medium text-secondary first:border-s first:ps-3 last:border-e last:pe-3"
+	>
+		<slot />
+	</th>
+</template>
+
+<script setup>
+import {onMounted, inject, useId} from 'vue';
+const tableContext = inject('tableContext');
+const colGroupId = useId();
+
+onMounted(() => {
+	tableContext.registerColGroup(colGroupId);
+});
+</script>
