b5c5e646cefaec4d24e7fc8b291c80a1e79b829f
Blesilda Ramirez <ramirezblesilda@gmail.com>
2025-07-03 22:10:18 -0400

    pkp/pkp-lib#11291 Add fields to add new discussion form

--- src/managers/DiscussionManager/useDiscussionManagerForm.js
+++ src/managers/DiscussionManager/useDiscussionManagerForm.js
@@ -1,12 +1,43 @@
+import {computed} from 'vue';
 import {useForm} from '@/composables/useForm';
+import {useModal} from '@/composables/useModal';
 import {useLocalize} from '@/composables/useLocalize';
+import {useCurrentUser} from '@/composables/useCurrentUser';
+import {useParticipantManagerStore} from '../ParticipantManager/participantManagerStore';
+import FileAttacherModal from '@/components/Composer/FileAttacherModal.vue';
+import FieldPreparedContentInsertModal from '@/components/Form/fields/FieldPreparedContentInsertModal.vue';
+import preparedContent from '../../mixins/preparedContent';
 
-export function useDiscussionManagerForm(
+export function useDiscussionManagerForm({
 	status = 'New',
+	submission,
+	submissionStageId,
 	closeDialog = () => {},
 	onSubmitFn = null,
-) {
+} = {}) {
 	const {t} = useLocalize();
+	const participantManagerStore = useParticipantManagerStore({
+		submission,
+		submissionStageId,
+	});
+
+	const currentUser = useCurrentUser();
+
+	const participantOptions = computed(() => {
+		return participantManagerStore.participantsList.map((participant) => {
+			let label = `${participant.fullName} (${participant.userName})`;
+
+			if (participant.userName === currentUser.getCurrentUserName()) {
+				label += ` (${t('common.me')})`;
+			}
+
+			return {
+				label,
+				subLabel: participant.roleName,
+				value: participant.id,
+			};
+		});
+	});
 
 	function getBadgeProps() {
 		let badgeProps = {};
@@ -58,20 +89,91 @@ export function useDiscussionManagerForm(
 		};
 	}
 
-	const {form, initEmptyForm, addPage, addGroup, set} = useForm(
-		{},
-		{customSubmit: handleFormSubmission},
-	);
+	const {
+		form,
+		initEmptyForm,
+		addPage,
+		addGroup,
+		set,
+		addFieldText,
+		addFieldOptions,
+		addFieldRichTextArea,
+	} = useForm({}, {customSubmit: handleFormSubmission});
 
-	initEmptyForm('version', {
+	initEmptyForm('discussion', {
 		showErrorFooter: false,
-		spacingVariant: 'fullWidth',
 	});
+
 	addPage('default', {
 		submitButton: {label: t('common.save')},
 		cancelButton: {label: t('common.cancel')},
 	});
-	addGroup('default');
+
+	addGroup('details', {
+		label: t('common.details'),
+		description: 'Placeholder',
+	});
+
+	addFieldText('detailsName', {
+		label: t('common.name'),
+		description: t('tasks.discussions.form.details.name.description'),
+		groupId: 'details',
+		size: 'large',
+	});
+
+	addFieldOptions('detailsParticipants', 'checkbox', {
+		label: t('editor.submission.stageParticipants'),
+		description: t('tasks.discussions.form.details.participants.description'),
+		groupId: 'details',
+		name: 'detailsParticipants',
+		options: participantOptions,
+	});
+
+	addGroup('taskInformation');
+
+	addGroup('discussion', {
+		label: t('submission.discussion'),
+		description: t('tasks.discussions.form.discussion.description'),
+	});
+
+	addFieldRichTextArea('discussionText', {
+		groupId: 'discussion',
+		toolbar: 'bold italic underline bullist | pkpAttachFiles | pkpInsert',
+		plugins: ['lists'],
+		size: 'large',
+		init: {
+			setup: (editor) => {
+				editor.ui.registry.addButton('pkpAttachFiles', {
+					icon: 'upload',
+					text: t('common.attachFiles'),
+					onAction() {
+						const {openSideModal} = useModal();
+
+						openSideModal(FileAttacherModal, {
+							title: t('common.attachFiles'),
+							attachers: [],
+							onAddAttachments: [],
+						});
+					},
+				});
+
+				editor.ui.registry.addButton('pkpInsert', {
+					icon: 'plus',
+					text: t('common.insertContent'),
+					onAction() {
+						const {openSideModal} = useModal(FieldPreparedContentInsertModal);
+						openSideModal(FieldPreparedContentInsertModal, {
+							title: t('common.insertContent'),
+							insertLabel: t('common.insert'),
+							preparedContent,
+							preparedContentLabel: 'Label',
+							onInsert: () => {},
+						});
+					},
+				});
+			},
+		},
+	});
 
 	const badgeProps = getBadgeProps(status);
 
