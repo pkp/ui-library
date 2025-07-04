42dd74d32da22e51f6df131377a82e6f30beef66
Blesilda Ramirez <ramirezblesilda@gmail.com>
2025-07-03 22:08:15 -0400

    pkp/pkp-lib#11291 Add form story for discussion manager component

--- src/managers/DiscussionManager/DiscussionManager.stories.js
+++ src/managers/DiscussionManager/DiscussionManager.stories.js
@@ -1,29 +1,61 @@
+import {within, userEvent} from '@storybook/test';
+import {http, HttpResponse} from 'msw';
 import DiscussionManager from './DiscussionManager.vue';
-import {getSubmissionMock} from '@/mockFactories/submissionMock';
 import {DiscussionsDataMock} from '@/mockFactories/discussionMock';
+import {getSubmissionMock} from '@/mockFactories/submissionMock';
+import {getParticipantMock} from '@/mockFactories/participantMock';
+
 export default {
 	title: 'Managers/DiscussionManager',
 	component: DiscussionManager,
 };
 
-export const Default = {
-	render: (args) => ({
-		components: {DiscussionManager},
-		setup() {
-			return {args};
-		},
-		template: `
-            <DiscussionManager v-bind="args"/>`,
+const baseArgs = {
+	submission: getSubmissionMock({
+		stages: [
+			{
+				id: pkp.const.WORKFLOW_STAGE_ID_PRODUCTION,
+				currentUserAssignedRoles: [16, 1],
+			},
+		],
 	}),
-	args: {
-		submission: getSubmissionMock({
-			stages: [
-				{
-					id: pkp.const.WORKFLOW_STAGE_ID_PRODUCTION,
-					currentUserAssignedRoles: [16, 1],
-				},
+	submissionStageId: pkp.const.WORKFLOW_STAGE_ID_PRODUCTION,
+	discussions: DiscussionsDataMock,
+};
+
+const renderComponent = (args) => ({
+	components: {DiscussionManager},
+	setup() {
+		return {args};
+	},
+	template: `<DiscussionManager v-bind="args" />`,
+});
+
+export const Default = {
+	render: renderComponent,
+	args: baseArgs,
+};
+
+export const AddForm = {
+	render: renderComponent,
+	args: baseArgs,
+	parameters: {
+		msw: {
+			handlers: [
+				http.get(
+					'https://mock/index.php/publicknowledge/api/v1/submissions/19/participants/5',
+					({request}) => {
+						return HttpResponse.json([getParticipantMock()]);
+					},
+				),
 			],
-		}),
-		discussions: DiscussionsDataMock,
+		},
+	},
+	play: async ({canvasElement}) => {
+		// Assigns canvas to the component root element
+		const canvas = within(canvasElement);
+		const user = userEvent.setup();
+
+		await user.click(canvas.getByText('Add'));
 	},
 };
