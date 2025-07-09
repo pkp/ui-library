54b86d7b2c100a85e74c005c9fcc264122ea2f77
Blesilda Ramirez <ramirezblesilda@gmail.com>
2025-07-09 11:30:01 -0400

    pkp/pkp-lib#11577 [main] [UI] Top menu items are only clickable on text (#657)
    * pkp/pkp-lib#11577 Render links in full on top nav menu items
    
    * pkp/pkp-lib#11577 Move li padding to the link in top nav links
    
    * pkp/pkp-lib#11577 Add Change Language locale key

--- src/components/TopNavActions/TopNavActions.vue
+++ src/components/TopNavActions/TopNavActions.vue
@@ -57,7 +57,7 @@
 						class="-ml-2 -mr-2 border-b border-b-light pb-2 pl-2 pr-2"
 					>
 						<div class="ps-2 text-base-bold leading-6 text-secondary">
-							Change Language
+							{{ t('common.changeLanguage') }}
 						</div>
 						<ul>
 							<li
@@ -65,7 +65,10 @@
 								:key="locale.key"
 								:class="actionLinkStyle"
 							>
-								<a :href="locale.href" class="block w-full no-underline">
+								<a
+									:href="locale.href"
+									class="block w-full px-2 py-1 no-underline"
+								>
 									<Icon
 										v-if="locale.key === currentLocale"
 										icon="Complete"
@@ -97,12 +100,12 @@
 					<div class="-ml-2 -mr-2 text-wrap px-2 pt-2">
 						<ul>
 							<li :class="actionLinkStyle">
-								<a :href="editProfileLink" class="w-full">
+								<a :href="editProfileLink" class="block px-2 py-1">
 									{{ t('user.profile.editProfile') }}
 								</a>
 							</li>
 							<li :class="actionLinkStyle">
-								<a :href="signOutLink" class="w-full">
+								<a :href="signOutLink" class="block px-2 py-1">
 									{{
 										_isUserLoggedInAs
 											? t('user.logOutAs', {username: currentUser.username})
@@ -186,7 +189,7 @@ const getAnimationStyle = (buttonType) => {
 const actionLinkStyle = [
 	'max-w-full cursor-pointer overflow-hidden truncate whitespace-nowrap',
 	'rounded border border-transparent bg-transparent hover:!border-primary',
-	'px-2 py-1 leading-6 text-primary',
+	'leading-6 text-primary',
 ];
 
 function getSupportedLocalesList() {
