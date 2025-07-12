b06c7465201e7c0eac8d654fea55468818e848f3
Blesilda Ramirez <ramirezblesilda@gmail.com>
2025-07-12 02:02:35 -0400

    pkp/pkp-lib#11291 Add more mockdata to discussion manager stories

--- src/managers/DiscussionManager/DiscussionManager.stories.js
+++ src/managers/DiscussionManager/DiscussionManager.stories.js
@@ -45,7 +45,66 @@ export const AddForm = {
 				http.get(
 					'https://mock/index.php/publicknowledge/api/v1/submissions/19/participants/5',
 					({request}) => {
-						return HttpResponse.json([getParticipantMock()]);
+						const participants = [
+							getParticipantMock(),
+							getParticipantMock({
+								id: 4,
+								fullName: 'Andy Author',
+								userName: 'aauthor',
+								stageAssignments: [
+									{
+										stageAssignmentStageId:
+											pkp.const.WORKFLOW_STAGE_ID_PRODUCTION,
+										stageAssignmentUserGroup: {
+											name: 'Author',
+											roleId: 65536,
+										},
+									},
+								],
+							}),
+							getParticipantMock({
+								id: 5,
+								fullName: 'Kaja Karina',
+								userName: 'kakarina',
+							}),
+						];
+						return HttpResponse.json(participants);
+					},
+				),
+				http.get(
+					'https://mock/index.php/publicknowledge/api/v1/templates',
+					() => {
+						return HttpResponse.json([
+							{
+								id: 1,
+								name: 'Template 1',
+								description: 'Description 1',
+								content: 'Content 1',
+								isTask: true,
+								taskDetails: {
+									participantRoles: [16, 1],
+									dueDate: 'P1W',
+								},
+							},
+							{
+								id: 2,
+								name: 'Template 2',
+								description: 'Description 2',
+								content: 'Content 2',
+								isTask: false,
+							},
+							{
+								id: 3,
+								name: 'Template 3',
+								description: 'Description 3',
+								content: 'Content 3',
+								isTask: true,
+								taskDetails: {
+									participantRoles: [65536],
+									dueDate: 'P3M',
+								},
+							},
+						]);
 					},
 				),
 			],
