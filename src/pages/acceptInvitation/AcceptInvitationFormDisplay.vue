12126723f2e3a096de07936fe701fbb78ad00e1e
Blesilda Ramirez <ramirezblesilda@gmail.com>
2025-08-07 15:47:48 -0400

    pkp/pkp-lib#11291 Add own FormDisplay components to user invitation pages to preserve current form display layout

---
+++ src/pages/acceptInvitation/AcceptInvitationFormDisplay.vue
@@ -0,0 +1,65 @@
+<template>
+	<div>
+		<div v-if="hasMultilingualFields">
+			<div
+				v-for="locale in availableLocales"
+				:key="locale.key"
+				class="mt-8 first:mt-0"
+			>
+				<component :is="localeHeadingElement" class="xl-bold mb-3 text-heading">
+					{{ locale.label }}
+				</component>
+				<div class="flex flex-col gap-y-4">
+					<div v-for="field in fields" :key="field.name">
+						<component
+							:is="
+								FieldComponents[field.component] || `${field.component}-display`
+							"
+							:field="field"
+							:heading-element="fieldHeadingElement"
+							:display-locale="field.isMultilingual ? locale.key : ''"
+						></component>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div
+			v-for="field in fields"
+			v-else
+			:key="field.name"
+			class="flex flex-col gap-y-10"
+		>
+			<component
+				:is="FieldComponents[field.component] || `${field.component}-display`"
+				:field="field"
+				:heading-element="fieldHeadingElement"
+			></component>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import {computed} from 'vue';
+import AcceptInvitationFieldTextDisplay from './AcceptInvitationFieldTextDisplay.vue';
+import AcceptInvitationFieldSelectDisplay from './AcceptInvitationFieldSelectDisplay.vue';
+
+const FieldComponents = {
+	'field-text': AcceptInvitationFieldTextDisplay,
+	'field-select': AcceptInvitationFieldSelectDisplay,
+};
+
+const props = defineProps({
+	fields: {type: Array, required: true},
+	supportedFormLocales: {type: Array, required: true, default: () => []},
+	localeHeadingElement: {required: false, default: 'h3', type: String},
+	fieldHeadingElement: {required: false, default: 'h4', type: String},
+});
+
+const availableLocales = computed(() => {
+	return hasMultilingualFields.value ? props.supportedFormLocales : [];
+});
+
+const hasMultilingualFields = computed(() => {
+	return !!props.fields.find((field) => field.isMultilingual);
+});
+</script>
---
+++ src/pages/acceptInvitation/AcceptInvitationFormDisplayItemBasic.vue
@@ -0,0 +1,19 @@
+<template>
+	<component :is="headingElement" class="text-base-normal text-secondary">
+		{{ heading }}
+	</component>
+	<div class="flex items-center">
+		<p v-if="!htmlValue" class="pt-1 text-lg-normal">{{ value }}</p>
+		<p v-else v-strip-unsafe-html="htmlValue" class="pt-1 text-lg-normal"></p>
+		<slot name="valueSuffix"></slot>
+	</div>
+</template>
+
+<script setup>
+defineProps({
+	headingElement: {required: true, type: String},
+	heading: {type: String, required: true},
+	value: {type: String, required: false, default: '-'},
+	htmlValue: {type: String, required: false, default: null},
+});
+</script>
