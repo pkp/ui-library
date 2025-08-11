c8149d4c982fa5d1d2580f5f074655d649929122
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2025-08-11 21:08:35 +0200

    pkp/pkp-lib#11691 testing

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
+import {rolldownVersion} from 'vite';
+console.log('rolldownVersion:', rolldownVersion);
 
 export default {
 	base: process.env.PKP_DOCS_VERSION
