9557340f86a6cb5edb4dd8a4e8776501e618722e
Blesilda Ramirez <ramirezblesilda@gmail.com>
2025-08-13 01:44:54 -0400

    pkp/pkp-lib#11291 Make the Started and Closed checkboxes clickable

--- src/managers/DiscussionManager/DiscussionManagerCellStarted.vue
+++ src/managers/DiscussionManager/DiscussionManagerCellStarted.vue
@@ -1,7 +1,19 @@
 <template>
 	<TableCell>
 		<div class="flex items-center text-primary">
-			<Icon v-if="workItem.type === 'Task'" :icon="icon" class="h-5 w-5"></Icon>
+			<button
+				type="button"
+				class="m-0 border-0 bg-transparent p-0 outline-none focus:outline-none"
+				:aria-pressed="props.workItem?.started"
+				:disabled="workItem.status !== 'Pending'"
+				@click="discussionManagerStore.discussionStartTask({workItem})"
+			>
+				<Icon
+					v-if="workItem.type === 'Task'"
+					:icon="icon"
+					class="h-5 w-5"
+				></Icon>
+			</button>
 		</div>
 	</TableCell>
 </template>
@@ -9,6 +21,10 @@
 <script setup>
 import TableCell from '@/components/Table/TableCell.vue';
 import Icon from '@/components/Icon/Icon.vue';
+
+import {useDiscussionManagerStore} from './discussionManagerStore';
+
+const discussionManagerStore = useDiscussionManagerStore();
 
 const props = defineProps({workItem: {type: Object, required: true}});
 const icon = props.workItem?.started ? 'CheckboxTicked' : 'Checkbox';
