a462d7669ffc4c21791269921d8b8bbd82816dbe
Blesilda Ramirez <ramirezblesilda@gmail.com>
2025-08-12 23:49:03 -0400

    pkp/pkp-lib#11291 Update task form group components to add or hide elements to it

--- src/managers/DiscussionManager/DiscussionManagerTaskInfo.vue
+++ src/managers/DiscussionManager/DiscussionManagerTaskInfo.vue
@@ -4,17 +4,83 @@
 		:label="t('discussion.form.taskInformation')"
 		:description="t('discussion.form.taskInfoDescription')"
 	/>
+	<div v-if="showStatusUpdateCheckbox" class="relative mt-6">
+		<label class="flex-start flex cursor-pointer gap-2 text-lg-normal">
+			<input
+				v-model="statusUpdateValue"
+				type="checkbox"
+				name="statusUpdateValue"
+			/>
+			{{ statusUpdateLabel }}
+		</label>
+	</div>
+	<div v-if="showTaskStartedInfo" class="mt-6 flex flex-col gap-y-4">
+		<FormDisplayItemBasic
+			heading-element="h2"
+			:heading="t('task.startedBy')"
+			:value="workItem?.startedBy"
+		/>
+		<FormDisplayItemBasic
+			heading-element="h2"
+			:heading="t('common.startDate')"
+			:value="workItem?.startDate"
+		/>
+	</div>
 </template>
 
 <script setup>
+import {ref, watch, computed} from 'vue';
 import {t} from '@/utils/i18n';
+import FormDisplayItemBasic from '@/components/FormDisplay/FormDisplayItemBasic.vue';
 import FormGroupHeader from '@/components/Form/FormGroupHeader.vue';
 
-defineProps({
+const props = defineProps({
 	/** The groupId to use from FormGroup component */
 	groupId: {
 		type: String,
 		required: true,
 	},
+	workItem: {
+		type: Object,
+		required: true,
+	},
+	inDisplayModeRef: {
+		type: Boolean,
+		required: true,
+	},
+});
+
+const emit = defineEmits(['updateStatusCheckbox']);
+const statusUpdateValue = ref(false);
+
+const statusUpdateLabel = computed(() => {
+	switch (props.workItem?.status) {
+		case 'Pending':
+			return t('task.startThisTask');
+		case 'In Progress':
+			return t('task.completeThisTask');
+		default:
+			return '';
+	}
+});
+
+const showStatusUpdateCheckbox = computed(() => {
+	return (
+		props.inDisplayModeRef &&
+		props.workItem?.type === 'Task' &&
+		['Pending', 'In Progress'].includes(props.workItem?.status)
+	);
+});
+
+const showTaskStartedInfo = computed(() => {
+	return (
+		props.workItem?.type === 'Task' &&
+		['In Progress', 'Closed'].includes(props.workItem?.status)
+	);
+});
+
+// emit the event to update the value of the checkbox
+watch(statusUpdateValue, (val) => {
+	emit('updateStatusCheckbox', val);
 });
 </script>
