97ba00a8fb6568c9916041bcb93319f548a44a5c
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2025-08-14 15:45:15 +0200

    styling

--- src/frontend/Comments/CommentsMessageActions.vue
+++ src/frontend/Comments/CommentsMessageActions.vue
@@ -1,1 +1,9 @@
-<template>actions</template>
+<template>
+	<DropdownActions button-variant="ellipsis" :items="items" />
+</template>
+<script setup>
+import {ref} from 'vue';
+import DropdownActions from '@/components/DropdownActions/DropdownActions.vue';
+
+const items = ref([]);
+</script>
