432ae5c6c7cb8c9957093b1de5c70e0404c475b9
Touhidur Rahman <abir.ruet@gmail.com>
2025-08-24 16:32:16 +0600

    pkp/pkp-lib#9295 composbale for issue assignment and added namespace(submission and publication) for status const

--- src/pages/workflow/composables/useWorkflowConfig/workflowConfigEditorialOPS.js
+++ src/pages/workflow/composables/useWorkflowConfig/workflowConfigEditorialOPS.js
@@ -28,7 +28,7 @@ export function getHeaderItems({
 	const {t} = useLocalize();
 	const actions = [];
 
-	if (submission.status === pkp.const.STATUS_PUBLISHED) {
+	if (submission.status === pkp.const.submission.STATUS_PUBLISHED) {
 		actions.push({
 			component: 'WorkflowActionButton',
 			props: {
@@ -39,7 +39,7 @@ export function getHeaderItems({
 	}
 
 	if (
-		submission.status !== pkp.const.STATUS_PUBLISHED &&
+		submission.status !== pkp.const.submission.STATUS_PUBLISHED &&
 		(submission.stageId === pkp.const.WORKFLOW_STAGE_ID_EDITING ||
 			submission.stageId === pkp.const.WORKFLOW_STAGE_ID_PRODUCTION)
 	) {
@@ -178,7 +178,7 @@ export const PublicationConfig = {
 			const items = [];
 
 			if (
-				submission.status !== pkp.const.STATUS_PUBLISHED &&
+				submission.status !== pkp.const.submission.STATUS_PUBLISHED &&
 				submission.publications.length < 2
 			) {
 				items.push({
@@ -220,7 +220,7 @@ export const PublicationConfig = {
 			if (!permissions.canPublish) {
 				return [];
 			}
-			if (selectedPublication.status === pkp.const.PUBLICATION_STATUS_QUEUED) {
+			if (selectedPublication.status === pkp.const.publication.STATUS_QUEUED) {
 				if (
 					hasSubmissionPassedStage(
 						submission,
@@ -248,7 +248,7 @@ export const PublicationConfig = {
 					},
 				});
 			} else if (
-				selectedPublication.status === pkp.const.PUBLICATION_STATUS_SCHEDULED
+				selectedPublication.status === pkp.const.publication.STATUS_SCHEDULED
 			) {
 				items.push({
 					component: 'WorkflowActionButton',
@@ -268,7 +268,7 @@ export const PublicationConfig = {
 					},
 				});
 			} else if (
-				selectedPublication.status === pkp.const.PUBLICATION_STATUS_PUBLISHED
+				selectedPublication.status === pkp.const.publication.STATUS_PUBLISHED
 			) {
 				items.push({
 					component: 'WorkflowActionButton',
