fcb0e117b9c9aa26615699f8989ce2fc979ba47d
ipula <ipularanasinghe007@gmail.com>
2025-08-27 16:43:43 +0200

    update reviewer invitation

--- src/pages/userInvitation/UserInvitationPage.stories.js
+++ src/pages/userInvitation/UserInvitationPage.stories.js
@@ -2,6 +2,7 @@ import UserInvitationPage from './UserInvitationPage.vue';
 import {http, HttpResponse} from 'msw';
 import userMock from './mocks/userMock.js';
 import PageInitConfigMock from './mocks/pageInitConfig';
+import ReviewerPageInitConfigMock from './mocks/reviewerPageInitConfig';
 
 export default {title: 'Pages/UserInvitation', component: UserInvitationPage};
 
@@ -117,4 +118,88 @@ export const Init = {
 		},
 	},
 	args: PageInitConfigMock,
+};
+
+export const Reviewer = {
+	render: (args) => ({
+		components: {UserInvitationPage},
+		setup() {
+			return {args};
+		},
+		template: '<UserInvitationPage v-bind="args" />',
+	}),
+	parameters: {
+		msw: {
+			handlers: [
+				http.post(
+					'https://mock/index.php/publicknowledge/api/v1/invitations/add/reviewerAccess',
+					() => {
+						return HttpResponse.json({invitationId: 15});
+					},
+				),
+				http.post(
+					'https://mock/index.php/publicknowledge/api/v1/invitations/15/populate',
+					async ({request}) => {
+						const data = await request.json();
+						let errors = {};
+
+						data.invitationData.userGroupsToAdd.forEach((element, index) => {
+							Object.keys(element).forEach((key) => {
+								if (element[key] === null) {
+									errors = {
+										...errors,
+										['userGroupsToAdd.' + index + '.' + key]: [
+											'This field is required',
+										],
+									};
+								}
+							});
+						});
+
+						if (data.invitationData.email === '') {
+							errors['email'] = ['This field is required'];
+						}
+						if (data.invitationData.familyName === '') {
+							errors['familyName'] = ['This field is required'];
+						}
+						if (data.invitationData.givenName === '') {
+							errors['givenName'] = ['This field is required'];
+						}
+
+						if (data.invitationData.emailComposer) {
+							Object.keys(data.invitationData.emailComposer).forEach(
+								(element) => {
+									if (data.invitationData.emailComposer[element] === '') {
+										errors['emailComposer'] = {
+											...errors['emailComposer'],
+											[element]: ['This field is required'],
+										};
+									}
+								},
+							);
+						}
+
+						if (Object.keys(errors).length > 0) {
+							return HttpResponse.json({errors: errors}, {status: 422});
+						}
+
+						return HttpResponse.json({status: 201});
+					},
+				),
+				http.post(
+					'https://mock/index.php/publicknowledge/api/v1/invitations/15/invite',
+					() => {
+						return HttpResponse.json({});
+					},
+				),
+				http.post(
+					'https://mock/index.php/publicknowledge/api/v1/user/_invite',
+					() => {
+						return HttpResponse.json('invitation send successfully');
+					},
+				),
+			],
+		},
+	},
+	args: ReviewerPageInitConfigMock,
 };
