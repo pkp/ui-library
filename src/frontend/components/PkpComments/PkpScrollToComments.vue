43efa13bdad853df9039f49c011da8f887217d5c
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2025-09-10 15:42:51 +0200

    wip

--- src/frontend/components/PkpComments/PkpScrollToComments.vue
+++ src/frontend/components/PkpComments/PkpScrollToComments.vue
@@ -1,62 +1,12 @@
 <template>
-	<div>
-		<a
-			id="scrollToComments"
-			style="cursor: pointer"
-			@click.prevent="scrollToComments"
-		>
-			{{
-				t('userComment.allComments', {
-					commentCount: commentsStore.allCommentsCount,
-				})
-			}}
-		</a>
-		<div style="margin-top: 1.5rem">
-			<a
-				v-if="!commentsStore.getCurrentUser()"
-				class="pkpScrollToComments__login"
-				:href="commentsStore.loginUrl"
-			>
-				{{ t('userComment.login') }}
-			</a>
-		</div>
-	</div>
+	<BaseScrollToComments>
+		<BaseScrollToCommentsAllComments />
+		<BaseScrollToCommentsLogInto />
+	</BaseScrollToComments>
 </template>
 
 <script setup>
-import {usePkpCommentsStore} from './usePkpCommentsStore';
-import {usePkpLocalize} from '@/frontend/composables/usePkpLocalize';
-
-const {t} = usePkpLocalize();
-const commentsStore = usePkpCommentsStore();
-
-const scrollToComments = () => {
-	// Element with ID 'pkpUserCommentsContainer' is defined in the pkpUserComment component.
-	const commentsContainer = document.getElementById('pkpUserCommentsContainer');
-	if (commentsContainer) {
-		commentsContainer.scrollIntoView({behavior: 'smooth'});
-	}
-};
+import BaseScrollToComments from './base/BaseScrollToComments.vue';
+import BaseScrollToCommentsAllComments from './base/BaseScrollToCommentsAllComments.vue';
+import BaseScrollToCommentsLogInto from './base/BaseScrollToCommentsLogInto.vue';
 </script>
-
-<style>
-.pkpScrollToComments__login {
-	display: inline-flex;
-	align-items: center;
-	justify-content: center;
-	padding: var(--pkp-spacing-2) var(--pkp-spacing-4);
-	font: var(--pkp-font-base-bold);
-	border-radius: var(--pkp-radius);
-	cursor: pointer;
-	border: 1px solid var(--pkp-color-transparent);
-	gap: var(--pkp-spacing-2);
-	background-color: var(--pkp-color-primary);
-	color: var(--pkp-text-color-on-dark);
-	text-decoration: none;
-}
-
-.pkpScrollToComments__login:hover {
-	background-color: var(--pkp-color-hover);
-	color: var(--pkp-text-color-on-dark);
-}
-</style>
