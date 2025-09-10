2df9e48ccc285d42ac2a0550cdc325b41f44b129
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2025-09-10 10:54:09 +0200

    wip

--- src/frontend/components/PkpIcon/PkpIcon.vue
+++ src/frontend/components/PkpIcon/PkpIcon.vue
@@ -1,38 +1,9 @@
-<script setup>
-import {computed} from 'vue';
-import Cancel from './icons/Cancel.vue';
-import MoreOptions from './icons/MoreOptions.vue';
-import Error from './icons/Error.vue';
-import Help from './icons/Help.vue';
-import User from './icons/User.vue';
-import Orcid from '@/frontend/components/PkpIcon/icons/Orcid.vue';
-import OrcidUnauthenticated from '@/frontend/components/PkpIcon/icons/OrcidUnauthenticated.vue';
-const svgIcons = {
-	Cancel,
-	MoreOptions,
-	Error,
-	Help,
-	User,
-	Orcid,
-	OrcidUnauthenticated,
-};
-
-const props = defineProps({
-	/** Which icon to use from our [Icon Gallery](?path=/story/components-icon--icon-gallery) */
-	icon: {type: String, required: true},
-});
-
-const classes = computed(() => {
-	return ['pkpIcon', props.icon === 'Error' ? 'pkpIcon--error' : ''];
-});
-</script>
-
 <template>
-	<component :is="svgIcons[icon]" :class="classes"></component>
+	<BaseIcon v-bind="$attrs" />
 </template>
 
 <style>
-[dir='rtl'] .pkpIcon {
+[dir='rtl'] .BaseIcon {
 	transform: scaleX(-1);
 }
 </style>
