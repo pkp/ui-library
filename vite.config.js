ced58a29331c98fea1dd2a0bd5e8114c380190e4
Blesilda Ramirez <ramirezblesilda@gmail.com>
2025-10-02 14:00:01 -0400

    pkp/pkp-lib#11788 Copy node_modules tinymce dir to public/styles dir via vite config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,24 @@ import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import {rolldownVersion} from 'vite';
 console.log('rolldownVersion:', rolldownVersion);
+import copy from 'rollup-plugin-copy';
 
 export default {
 	base: process.env.PKP_DOCS_VERSION
 		? '/dev/ui-library/' + process.env.PKP_DOCS_VERSION
 		: '/',
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		copy({
+			targets: [
+				{
+					src: 'node_modules/tinymce/skins/ui/tinymce-5/**/*.css',
+					dest: 'public/styles/tinymce',
+				},
+			],
+			hook: 'buildStart',
+		}),
+	],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
