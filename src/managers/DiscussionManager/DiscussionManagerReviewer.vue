05562b54b7ec47a155db2ee331a0e336839b436c
Blesilda Ramirez <ramirezblesilda@gmail.com>
2025-10-12 00:34:24 -0400

    pkp/pkp-lib#11825 Add DiscussionManagerReviewer component to use for reviwers page

---
+++ src/managers/DiscussionManager/DiscussionManagerReviewer.vue
@@ -0,0 +1,22 @@
+<template>
+	<template v-if="submission">
+		<DiscussionManager
+			:submission="submission"
+			:submission-stage-id="submissionStageId"
+		></DiscussionManager>
+	</template>
+</template>
+
+<script setup>
+import {useWorkflowDataSubmissionPublication} from '@/pages/workflow/composables/useWorkflowDataSubmissionPublication';
+import DiscussionManager from './DiscussionManager.vue';
+
+const props = defineProps({
+	submissionId: {type: String, required: true},
+	submissionStageId: {type: Number, required: true},
+});
+
+const {submission} = useWorkflowDataSubmissionPublication({
+	submissionId: props.submissionId,
+});
+</script>
