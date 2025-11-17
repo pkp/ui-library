bea8ebfe4d40d8caef22e36b077896a161c71de3
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2025-11-17 23:30:45 +0100

    pkp/pkp-lib#12027 Introduce useOrdering (#740)
    * pkp/pkp-lib#12027 Introduce useOrdering
    
    * pkp/pkp-lib#12027 Clean up the example

---
+++ src/components/Table/TableCellOrder.vue
@@ -0,0 +1,25 @@
+<template>
+	<TableCell>
+		<div class="flex items-center justify-center">
+			<button
+				class="inline-flex items-center justify-center rounded text-primary hover:bg-primary hover:text-on-dark"
+				@click="emit('up')"
+			>
+				<Icon class="h-6 w-6" icon="ChevronUp"></Icon>
+			</button>
+			<button
+				class="inline-flex items-center justify-center rounded text-primary hover:bg-primary hover:text-on-dark"
+				@click="emit('down')"
+			>
+				<Icon class="h-6 w-6" icon="ChevronDown"></Icon>
+			</button>
+		</div>
+	</TableCell>
+</template>
+
+<script setup>
+import Icon from '@/components/Icon/Icon.vue';
+import TableCell from './TableCell.vue';
+
+const emit = defineEmits(['up', 'down']);
+</script>
