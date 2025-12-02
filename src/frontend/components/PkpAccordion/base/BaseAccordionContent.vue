2059f1e1ff36495d0e954d8c879c756213a78b9e
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2025-12-02 21:06:28 +0100

    wip

---
+++ src/frontend/components/PkpAccordion/base/BaseAccordionContent.vue
@@ -0,0 +1,8 @@
+<template>
+	<AccordionContent class="BaseAccordionContent">
+		<slot />
+	</AccordionContent>
+</template>
+<script setup>
+import {AccordionContent} from 'reka-ui';
+</script>
---
+++ src/frontend/components/PkpAccordion/base/BaseAccordionHeader.vue
@@ -0,0 +1,8 @@
+<template>
+	<AccordionHeader class="BaseAccordionHeader" as-child>
+		<slot />
+	</AccordionHeader>
+</template>
+<script setup>
+import {AccordionHeader} from 'reka-ui';
+</script>
