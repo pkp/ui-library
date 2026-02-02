0cae91dcaadeee4f5070293a9c4d0ac1205634fe
Blesilda Ramirez <ramirezblesilda@gmail.com>
2026-02-02 03:01:32 -0500

    pkp/pkp-lib#12262 Add MediaFileManager component

---
+++ src/managers/MediaFileManager/MediaFileManager.vue
@@ -0,0 +1,84 @@
+<template>
+	<div>
+		<PkpTable>
+			<template #label>
+				<h3 class="text-xl-bold">
+					{{ t('publication.mediaFiles') }}
+					<Spinner
+						v-if="mediaFileManagerStore.isLoadingMediaFiles"
+						class="ms-2"
+					/>
+				</h3>
+			</template>
+			<template #description>
+				<p class="text-lg-normal">
+					{{ t('publication.mediaFiles.description') }}
+				</p>
+			</template>
+			<template #top-controls>
+				<div class="flex gap-x-2">
+					<component
+						:is="Components[action.component] || action.component"
+						v-bind="action.props || {}"
+						v-for="(action, i) in mediaFileManagerStore.topItems"
+						:key="i"
+					></component>
+				</div>
+			</template>
+			<TableHeader>
+				<TableColumn
+					v-for="(column, i) in mediaFileManagerStore.columns"
+					:key="i"
+					:no-text-wrap="false"
+				>
+					<span :class="column.headerSrOnly ? 'sr-only' : ''">
+						{{ column.header }}
+					</span>
+				</TableColumn>
+			</TableHeader>
+			<TableBody>
+				<TableRow
+					v-for="mediaFile in mediaFileManagerStore.mediaFilesList"
+					:key="mediaFile.id"
+					:striped="false"
+				>
+					<component
+						:is="Components[column.component] || column.component"
+						v-for="(column, i) in mediaFileManagerStore.columns"
+						:key="i"
+						:index="i"
+						:media-file="mediaFile"
+					></component>
+				</TableRow>
+			</TableBody>
+		</PkpTable>
+	</div>
+</template>
+<script setup>
+import {useMediaFileManagerStore} from './mediaFileManagerStore';
+
+import PkpTable from '@/components/Table/Table.vue';
+import TableHeader from '@/components/Table/TableHeader.vue';
+import TableBody from '@/components/Table/TableBody.vue';
+import TableColumn from '@/components/Table/TableColumn.vue';
+import TableRow from '@/components/Table/TableRow.vue';
+import MediaFileManagerActionButton from './MediaFileManagerActionButton.vue';
+import MediaFileManagerCellGroupId from './MediaFileManagerCellGroupId.vue';
+import MediaFileManagerCellName from './MediaFileManagerCellName.vue';
+import MediaFileManagerCellType from './MediaFileManagerCellType.vue';
+import MediaFileManagerCellSize from './MediaFileManagerCellSize.vue';
+import MediaFileManagerCellDateUploaded from './MediaFileManagerCellDateUploaded.vue';
+import MediaFileManagerCellActions from './MediaFileManagerCellActions.vue';
+import Spinner from '@/components/Spinner/Spinner.vue';
+
+const Components = {
+	MediaFileManagerActionButton,
+	MediaFileManagerCellGroupId,
+	MediaFileManagerCellName,
+	MediaFileManagerCellType,
+	MediaFileManagerCellSize,
+	MediaFileManagerCellDateUploaded,
+	MediaFileManagerCellActions,
+};
+const mediaFileManagerStore = useMediaFileManagerStore();
+</script>
