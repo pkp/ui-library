110b11502e15a0f79f9b520ff7ba4429cab2fe9b
Blesilda Ramirez <ramirezblesilda@gmail.com>
2026-02-20 01:30:48 -0500

    pkp/pkp-lib#12262 Update Metadata form based on genre's file metadata fields

--- src/managers/MediaFileManager/useMediaFileManagerMetadataFormModal.js
+++ src/managers/MediaFileManager/useMediaFileManagerMetadataFormModal.js
@@ -39,6 +39,7 @@ export function useMediaFileManagerMetadataFormModal(mediaFile = {}) {
 
 	addGroup('default');
 
+	// Name field is common to all metadata types
 	addFieldText('name', {
 		groupId: 'default',
 		label: t('publication.mediaFiles.metadataName'),
@@ -49,33 +50,97 @@ export function useMediaFileManagerMetadataFormModal(mediaFile = {}) {
 		isMultilingual: true,
 	});
 
-	addFieldText('caption', {
-		groupId: 'default',
-		label: t('grid.artworkFile.caption'),
-		size: 'large',
-		value: mediaFile.caption,
-	});
+	const metadataType = mediaFile.genreFileMetadataId;
 
-	addFieldText('credit', {
-		groupId: 'default',
-		label: t('grid.artworkFile.credit'),
-		size: 'large',
-		value: mediaFile.credit,
-	});
+	if (metadataType === 'artwork') {
+		addFieldText('caption', {
+			groupId: 'default',
+			label: t('grid.artworkFile.caption'),
+			size: 'large',
+			value: mediaFile.caption,
+		});
 
-	addFieldText('copyrightOwner', {
-		groupId: 'default',
-		label: t('grid.artworkFile.copyrightOwner'),
-		size: 'large',
-		value: mediaFile.copyrightOwner,
-	});
+		addFieldText('credit', {
+			groupId: 'default',
+			label: t('grid.artworkFile.credit'),
+			size: 'large',
+			value: mediaFile.credit,
+		});
 
-	addFieldText('terms', {
-		groupId: 'default',
-		label: t('grid.artworkFile.permissionTerms'),
-		size: 'large',
-		value: mediaFile.terms,
-	});
+		addFieldText('copyrightOwner', {
+			groupId: 'default',
+			label: t('grid.artworkFile.copyrightOwner'),
+			size: 'large',
+			value: mediaFile.copyrightOwner,
+		});
+
+		addFieldText('terms', {
+			groupId: 'default',
+			label: t('grid.artworkFile.permissionTerms'),
+			size: 'large',
+			value: mediaFile.terms,
+		});
+	}
+
+	if (metadataType === 'supplementary') {
+		addFieldText('description', {
+			groupId: 'default',
+			label: t('common.description'),
+			size: 'large',
+			value: mediaFile.description,
+		});
+
+		addFieldText('creator', {
+			groupId: 'default',
+			label: t('grid.supplementaryFile.creator'),
+			size: 'large',
+			value: mediaFile.creator,
+		});
+
+		addFieldText('publisher', {
+			groupId: 'default',
+			label: t('grid.supplementaryFile.publisher'),
+			size: 'large',
+			value: mediaFile.publisher,
+		});
+
+		addFieldText('source', {
+			groupId: 'default',
+			label: t('grid.supplementaryFile.source'),
+			size: 'large',
+			value: mediaFile.source,
+		});
+
+		addFieldText('subject', {
+			groupId: 'default',
+			label: t('grid.supplementaryFile.subject'),
+			size: 'large',
+			value: mediaFile.subject,
+		});
+
+		addFieldText('sponsor', {
+			groupId: 'default',
+			label: t('grid.supplementaryFile.sponsor'),
+			size: 'large',
+			value: mediaFile.sponsor,
+		});
+
+		addFieldText('dateCreated', {
+			groupId: 'default',
+			label: t('grid.supplementaryFile.date'),
+			size: 'large',
+			value: mediaFile.dateCreated,
+		});
+
+		addFieldText('language', {
+			groupId: 'default',
+			label: t('grid.supplementaryFile.language'),
+			size: 'large',
+			value: mediaFile.language,
+		});
+	}
+
+	// Document metadata type: only the name field (already added above)
 
 	const {setInitialState} = useFormChanged(form, null, {
 		warnOnClose: true,
