12b2f65f824482ad47c9b8addd320317892fee29
taslangraham <gtaslan1@gmail.com>
2026-03-06 12:19:57 -0500

    pkp/pkp-lib#12420 Fix missing 'Show More' button on public comments' front-office UI

---
+++ src/frontend/components/PkpComments/base/BaseCommentShowMore.vue
@@ -0,0 +1,27 @@
+<template>
+	<pkp-button
+		v-if="commentsStore.hasMoreComments(publication.id)"
+		:class="cn('showMore')"
+		@click="() => commentsStore.loadComments(publication.id)"
+	>
+		{{
+			t('userComment.showMore', {
+				count: commentsStore.getShowMoreCommentsCount(publication.id),
+			})
+		}}
+	</pkp-button>
+</template>
+
+<script setup>
+import {usePkpCommentsStore} from '../usePkpCommentsStore';
+import {usePkpLocalize} from '@/frontend/composables/usePkpLocalize';
+import {usePkpStyles} from '@/frontend/composables/usePkpStyles.js';
+
+const {t} = usePkpLocalize();
+const {cn} = usePkpStyles('PkpComments');
+const commentsStore = usePkpCommentsStore();
+
+defineProps({
+	publication: {type: Object, required: true},
+});
+</script>
