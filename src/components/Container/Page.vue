dafcb2c0bf0e3c1946a2af314355a40f259d99c1
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2026-03-26 11:11:51 +0100

    TurboSnap optimisation

--- src/components/Container/Page.vue
+++ src/components/Container/Page.vue
@@ -1,34 +1,13 @@
 <script type="text/javascript">
 import Container from '@/components/Container/Container.vue';
 import ModalManager from '@/components/Modal/ModalManager.vue';
-import DashboardPage from '@/pages/dashboard/DashboardPage.vue';
-
 import PkpAnnouncer from '@/components/Announcer/Announcer.vue';
-import ReviewerSubmissionPage from '@/pages/reviewerSubmission/ReviewerSubmissionPage.vue';
-import JobsPage from '@/pages/jobs/JobsPage.vue';
-import FailedJobsPage from '@/pages/jobs/FailedJobsPage.vue';
-import FailedJobDetailsPage from '@/pages/jobs/FailedJobDetailsPage.vue';
-import CounterReportsPage from '@/pages/counter/CounterReportsPage.vue';
-import UserInvitationPage from '@/pages/userInvitation/UserInvitationPage.vue';
-import AcceptInvitationPage from '@/pages/acceptInvitation/AcceptInvitationPage.vue';
-import UserCommentsPage from '@/pages/userComments/UserCommentsPage.vue';
-import RequestReviewRoundAuthorResponse from '@/pages/requestReviewRoundAuthorResponse/RequestReviewRoundAuthorResponse.vue';
 
 export default {
 	name: 'Page',
 	components: {
 		PkpAnnouncer,
 		ModalManager,
-		ReviewerSubmissionPage,
-		JobsPage,
-		FailedJobsPage,
-		FailedJobDetailsPage,
-		CounterReportsPage,
-		DashboardPage,
-		UserInvitationPage,
-		AcceptInvitationPage,
-		UserCommentsPage,
-		RequestReviewRoundAuthorResponse,
 	},
 	extends: Container,
 	data() {
