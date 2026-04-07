f9e9e180344d5f00eec3a0901bc98e88f839a3f5
Blesilda Biazon <ramirezblesilda@gmail.com>
2026-04-07 00:58:12 -0400

    pkp/pkp-lib#12531 Show task/discussion activity logs by categories

--- src/managers/DiscussionManager/DiscussionManagerCellActivity.vue
+++ src/managers/DiscussionManager/DiscussionManagerCellActivity.vue
@@ -1,15 +1,44 @@
 <template>
 	<TableCell>
 		<div class="flex flex-col items-start">
-			<span class="my-1 text-wrap text-base-normal">
+			<span v-if="!recentNote" class="my-1 text-wrap text-base-normal">
 				{{ workItem.latestActivities?.[0]?.message }}
 			</span>
+			<ol v-else class="list-outside list-decimal pl-5">
+				<li
+					v-for="activity in groupedActivities"
+					:key="activity"
+					class="my-1 text-wrap text-base-normal"
+				>
+					{{ activity }}
+				</li>
+			</ol>
 		</div>
 	</TableCell>
 </template>
 
 <script setup>
+import {computed} from 'vue';
+import {useDate} from '@/composables/useDate';
 import TableCell from '@/components/Table/TableCell.vue';
 
-defineProps({workItem: {type: Object, required: true}});
+const props = defineProps({workItem: {type: Object, required: true}});
+const {isWithin7Days} = useDate();
+
+const recentNote = computed(() => {
+	return props.workItem?.latestActivities?.find(
+		(activity) =>
+			activity.type === pkp.const.SUBMISSION_LOG_TASK_NOTE_POSTED &&
+			isWithin7Days(activity.date),
+	);
+});
+
+const groupedActivities = computed(() => {
+	return [
+		recentNote.value?.message,
+		props.workItem.latestActivities?.find(
+			(activity) => activity.type !== pkp.const.SUBMISSION_LOG_TASK_NOTE_POSTED,
+		)?.message,
+	].filter(Boolean);
+});
 </script>
