ec4d82df3fa136a7efac5f06d806ef5d21313357
Blesilda Biazon <ramirezblesilda@gmail.com>
2026-04-13 03:04:28 -0400

    pkp/pkp-lib#12534 Add reviewer step 3 form component

---
+++ src/pages/reviewerSubmission/forms/ReviewGuidelinesButton.vue
@@ -0,0 +1,34 @@
+<template>
+	<PkpButton is-link @click="openGuidelinesDialog">
+		{{ t('reviewer.submission.guidelines') }}
+	</PkpButton>
+</template>
+
+<script setup>
+import PkpButton from '@/components/Button/Button.vue';
+import {useLocalize} from '@/composables/useLocalize';
+import {useModal} from '@/composables/useModal';
+
+const {t} = useLocalize();
+const {openDialog} = useModal();
+
+const props = defineProps({
+	guidelines: {
+		type: String,
+		required: true,
+	},
+});
+
+function openGuidelinesDialog() {
+	openDialog({
+		title: t('reviewer.submission.guidelines'),
+		message: props.guidelines,
+		actions: [
+			{
+				label: t('common.ok'),
+				callback: (close) => close(),
+			},
+		],
+	});
+}
+</script>
---
+++ src/pages/reviewerSubmission/forms/ReviewSubmissionForm3.vue
@@ -0,0 +1,59 @@
+<template>
+	<div>
+		<!-- Form: Review Files, Guidelines, Comments/Form, Attachments, Discussions, Recommendation -->
+		<PkpForm
+			v-bind="form"
+			@set="set"
+			@cancel="goBack"
+			@save-for-later="saveForLater"
+		/>
+	</div>
+</template>
+
+<script setup>
+import {useReviewSubmissionForm3} from './useReviewSubmissionForm3';
+
+const props = defineProps({
+	reviewAssignment: {
+		type: Object,
+		required: true,
+	},
+	reviewForm: {
+		type: Object,
+		required: false,
+		default: null,
+	},
+	reviewFormElements: {
+		type: Array,
+		required: false,
+		default: null,
+	},
+	reviewFormResponses: {
+		type: Object,
+		required: false,
+		default: null,
+	},
+	reviewerRecommendationOptions: {
+		type: Array,
+		required: false,
+		default: () => [],
+	},
+	comments: {
+		type: String,
+		required: false,
+		default: '',
+	},
+	commentsPrivate: {
+		type: String,
+		required: false,
+		default: '',
+	},
+	reviewGuidelines: {
+		type: String,
+		required: false,
+		default: null,
+	},
+});
+
+const {form, set, saveForLater, goBack} = useReviewSubmissionForm3(props);
+</script>
