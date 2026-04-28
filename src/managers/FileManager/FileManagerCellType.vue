44da3337bb57286eae21997704c038e29ad6f398
Blesilda Biazon <ramirezblesilda@gmail.com>
2026-04-28 19:36:38 -0400

    pkp/pkp-lib#12584 Add Amendment Notice badge to FileManager type cell

--- src/managers/FileManager/FileManagerCellType.vue
+++ src/managers/FileManager/FileManagerCellType.vue
@@ -1,22 +1,39 @@
 <template>
 	<TableCell no-wrap>
-		<Badge
-			v-if="file.genreName"
-			:is-primary="!file.genreIsDependent && !file.genreIsSupplementary"
-		>
-			{{ localize(file.genreName) }}
-		</Badge>
+		<div class="flex flex-wrap gap-1">
+			<Badge
+				v-if="file.genreName"
+				:is-primary="!file.genreIsDependent && !file.genreIsSupplementary"
+			>
+				{{ localize(file.genreName) }}
+			</Badge>
+			<Badge v-if="hasAmendmentNotice">
+				{{ t('submission.files.amendmentNotice') }}
+			</Badge>
+		</div>
 	</TableCell>
 </template>
 <script setup>
+import {computed} from 'vue';
 import TableCell from '@/components/Table/TableCell.vue';
 import Badge from '@/components/Badge/Badge.vue';
 
 import {useLocalize} from '@/composables/useLocalize';
 
-const {localize} = useLocalize();
+const {t, localize} = useLocalize();
 
-defineProps({
+const props = defineProps({
 	file: {type: Object, required: true},
+});
+
+const hasAmendmentNotice = computed(() => {
+	if (props.file.fileStage !== pkp.const.SUBMISSION_FILE_REVIEW_REVISION) {
+		return false;
+	}
+
+	const summaryOfChanges = props.file.summaryOfChanges;
+	if (!summaryOfChanges || typeof summaryOfChanges !== 'object') return false;
+
+	return Object.values(summaryOfChanges).some((value) => !!value);
 });
 </script>
