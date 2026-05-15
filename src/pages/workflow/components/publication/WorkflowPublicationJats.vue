3662f69dc6639fff6fb3a18d013a6f89543c8a85
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2026-05-15 22:31:54 +0200

    pkp/pkp-lib#12754 Improve line breaking for jats rendering

--- src/pages/workflow/components/publication/WorkflowPublicationJats.vue
+++ src/pages/workflow/components/publication/WorkflowPublicationJats.vue
@@ -490,7 +490,8 @@ export default {
 		white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
 		white-space: -pre-wrap; /* Opera 4-6 */
 		white-space: -o-pre-wrap; /* Opera 7 */
-		word-wrap: break-word; /* Internet Explorer 5.5+ */
+
+		overflow-wrap: anywhere;
 	}
 }
 </style>
