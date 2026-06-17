74f96f925c956b53ea9a1ffbad8780d3974346f1
Blesilda Biazon <ramirezblesilda@gmail.com>
2026-06-17 17:13:47 +0800

    Add awaits on every triggerDataChange calls when reloading tables

--- src/pages/workflow/workflowStore.js
+++ src/pages/workflow/workflowStore.js
@@ -164,10 +164,10 @@ export const useWorkflowStore = defineComponentStore(
 						pageInitConfig: props.pageInitConfig,
 						store,
 					},
-					(finishedData) => {
-						triggerDataChange(finishedData);
+					async (finishedData) => {
+						await triggerDataChange(finishedData);
 						if (finishedCallback) {
-							finishedCallback(finishedData);
+							await finishedCallback(finishedData);
 						}
 					},
 				),
@@ -207,7 +207,7 @@ export const useWorkflowStore = defineComponentStore(
 					reviewRoundId: selectedReviewRound.value?.id,
 					...args,
 				},
-				(finishedData) => triggerDataChange(finishedData),
+				async (finishedData) => await triggerDataChange(finishedData),
 			);
 		}
 
