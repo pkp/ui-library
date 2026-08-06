a0b7faa81bd3188f101cff4cd5421e5e98fa8ee9
Blesilda Biazon <ramirezblesilda@gmail.com>
2026-08-05 21:45:27 -0400

    pkp/pkp-lib#13094 Add DataCitationViewModal component

---
+++ src/managers/DataCitationManager/modals/DataCitationViewModal.vue
@@ -0,0 +1,23 @@
+<template>
+	<SideModalBody>
+		<template #title>
+			{{ t('submission.dataCitations.viewModal.title') }}
+		</template>
+		<SideModalLayoutBasic>
+			<PkpForm v-bind="form" field-heading-element="h2" :display-only="true" />
+		</SideModalLayoutBasic>
+	</SideModalBody>
+</template>
+
+<script setup>
+import {useLocalize} from '@/composables/useLocalize';
+import SideModalBody from '@/components/Modal/SideModalBody.vue';
+import SideModalLayoutBasic from '@/components/Modal/SideModalLayoutBasic.vue';
+import PkpForm from '@/components/Form/Form.vue';
+
+defineProps({
+	form: {type: Object, required: true},
+});
+
+const {t} = useLocalize();
+</script>
